5c8206dc9ba78ba523b8aa72fc9d12606d55e9ee
Gino Miceli <gmiceli@google.com>
2018-09-05 00:38:41 -0400

    Formatting, cleanup
    Change-Id: Ibf531a0632dc1f1f01e44c6cc7a79f1cf63b5cd5

--- gnd/src/main/java/com/google/android/gnd/ui/startup/StartupFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/startup/StartupFragment.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.gnd.ui.startup;
+
+import static com.google.android.gnd.rx.RxAutoDispose.autoDisposable;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,20 +25,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.navigation.fragment.NavHostFragment;
+import butterknife.BindView;
+import butterknife.OnClick;
 import com.google.android.gnd.R;
 import com.google.android.gnd.system.AuthenticationManager;
 import com.google.android.gnd.system.GoogleApiManager;
 import com.google.android.gnd.ui.common.AbstractFragment;
 import com.google.android.gnd.ui.common.EphemeralPopups;
-
 import javax.inject.Inject;
-
-import androidx.navigation.fragment.NavHostFragment;
-import butterknife.BindView;
-import butterknife.OnClick;
-
-import static com.google.android.gnd.rx.RxAutoDispose.autoDisposable;
 
 public class StartupFragment extends AbstractFragment {
   private static final String TAG = StartupFragment.class.getSimpleName();
@@ -29,6 +42,12 @@ public class StartupFragment extends AbstractFragment {
 
   @BindView(R.id.sign_in_button)
   View signInButton;
+
+  @Override
+  public View onCreateView(
+    LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.startup_frag, container, false);
+  }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -90,11 +109,5 @@ public class StartupFragment extends AbstractFragment {
 
   private void quit() {
     getActivity().finish();
-  }
-
-  @Override
-  public View onCreateView(
-      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.startup_frag, container, false);
   }
 }
