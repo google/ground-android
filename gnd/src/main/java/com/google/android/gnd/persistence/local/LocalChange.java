2cc2c335488d1b2a2f4766b1e1c0bd2a25fa8216
Gino Miceli <gmiceli@google.com>
2019-04-27 11:50:56 -0400

    Model local change and deletion state

--- gnd/src/main/java/com/google/android/gnd/persistence/local/LocalChange.java
+++ gnd/src/main/java/com/google/android/gnd/persistence/local/LocalChange.java
@@ -16,12 +16,45 @@
 
 package com.google.android.gnd.persistence.local;
 
-interface LocalChange {
-  // TODO: Model the semantics of the following operations:
-  //  createFeature(Feature feature);
-  //  updateFeatureLocation(String featureId, Coordinates newLocation);
-  //  updateFeatureDeletionState(String featureId, DeletionState newDeletionState);
-  //  createRecord(Record record);
-  //  updateRecordResponses(String recordId, ValueMap modifiedResponses);
-  //  updateRecordDeletionState(String recordId, DeletionState newDeletionState);
+import java8.util.Optional;
+
+/**
+ * Represents the smallest unit of change to an entity made locally that can be merged with changes
+ * received from the remote data store.
+ *
+ * @param <T> the class of a builder for the type under mutation.
+ * @param <V> the class of the value being set or cleared.
+ */
+public interface LocalChange<T, V> {
+  /** Returns the builder class to which this change applies. */
+  Class<T> getEntityBuilderType();
+
+  /** Returns the value class to which this change applies. */
+  Class<V> getValueType();
+
+  /** Returns the locally unique id of this change. */
+  long getChangeId();
+
+  /** Returns to globally unique id of the entity being modified. */
+  String getEntityId();
+
+  /**
+   * Returns the value before this change is applied, if present. If the value was not specified
+   * (missing) empty is returned.
+   */
+  Optional<V> getOldValue();
+
+  /**
+   * Returns the value to be set after this change is applied. If empty, the change indicates the
+   * current value should be cleared.
+   */
+  Optional<V> getNewValue();
+
+  /** Applies the change to the provided builder, returning a reference to the same for chaining. */
+  T apply(T entityBuilder);
+
+  /**
+   * Rolls back the change on the provided builder, returning a reference to the same for chaining.
+   */
+  T rollBack(T entityBuilder);
 }
