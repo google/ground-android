2718bdbf97c5068392d94e5eab555b5874aff717
Scott Olsen <scg.olsen@gmail.com>
2019-07-17 15:38:16 -0400

    Tile: Add a value class for tiles
    This change adds a value class for tiles. Tiles represent some
    downloadable piece of imagery we can use for offline viewing.

---
+++ gnd/src/main/java/com/google/android/gnd/model/tile/Tile.java
@@ -0,0 +1,29 @@
+package com.google.android.gnd.model.tile;
+
+import com.google.auto.value.AutoValue;
+
+import java.net.URL;
+
+@AutoValue
+public abstract class Tile {
+    public enum State {
+        PENDING,
+        DOWNLOADED,
+        FAILED
+    }
+
+    public abstract String getId();
+    public abstract String getPath();
+    public abstract State getState();
+
+    public static Builder newBuilder() {return new AutoValue_Tile.Builder();}
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder setId(String id);
+        public abstract Builder setPath(String path);
+        public abstract Builder setState(State state);
+
+        public abstract Tile build();
+    }
+}
