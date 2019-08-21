1f818989cae52cef0cc14daafb07b7bc5114df39
Scott Olsen <scg.olsen@gmail.com>
2019-08-21 19:53:24 -0400

    Offline Imagery: Remove redundant file check
    Removed a redundant file existence check--this is already handled in a
    try/catch. Also removed periods from log messages.

--- gnd/src/main/java/com/google/android/gnd/ui/map/gms/GoogleMapsMapAdapter.java
+++ gnd/src/main/java/com/google/android/gnd/ui/map/gms/GoogleMapsMapAdapter.java
@@ -102,11 +102,6 @@ class GoogleMapsMapAdapter implements MapAdapter {
   public void renderJsonLayer() {
     File file = new File(context.getFilesDir(), GEO_JSON_FILE);
 
-    if (!file.exists()) {
-      Log.e(TAG, "GeoJSON file not found.");
-      return;
-    }
-
     try {
         InputStream is = new FileInputStream(file);
         BufferedReader buf = new BufferedReader(new InputStreamReader(is));
@@ -120,10 +115,10 @@ class GoogleMapsMapAdapter implements MapAdapter {
         JSONObject geoJson = new JSONObject(sb.toString());
         GeoJsonLayer layer = new GeoJsonLayer(map, geoJson);
         layer.addLayerToMap();
-        Log.d(TAG, "JSON layer successfully loaded.");
+        Log.d(TAG, "JSON layer successfully loaded");
 
     } catch (IOException | JSONException e) {
-        Log.e(TAG, "Unable to load JSON layer.", e);
+        Log.e(TAG, "Unable to load JSON layer", e);
     }
   }
 
