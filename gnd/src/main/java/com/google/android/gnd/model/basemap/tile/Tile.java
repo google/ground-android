20c0a3d33cc48320c5f9a57d098452f7b7249c46
Scott Olsen <scg.olsen@gmail.com>
2019-09-09 12:16:17 -0400

    Offline Imagery: Clarify Tile pathFromId TODO

--- gnd/src/main/java/com/google/android/gnd/model/basemap/tile/Tile.java
+++ gnd/src/main/java/com/google/android/gnd/model/basemap/tile/Tile.java
@@ -24,6 +24,9 @@ public abstract class Tile {
   public static String pathFromId(String tileId) {
     // Tile ids are stored as x-y-z. Paths must be z-x-y.mbtiles.
     // TODO: Convert tile ids to paths in a less restrictive and less hacky manner.
+    // TODO: Move this method to a more appropriate home? We need to perform (and possibly will no
+    // matter where the tiles are stored) translation between the tile ID and the file path of the
+    // corresponding tile source in remote storage/wherever we pull the source tile from.
     String[] fields = tileId.replaceAll("[()]", "").split(", ");
     String filename = fields[2] + "-" + fields[0] + "-" + fields[1];
 
