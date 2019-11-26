eac5db5a548dbb50a46edb410595bf81719a8401
Scott Olsen <scg.olsen@gmail.com>
2019-11-26 16:23:05 -0500

    Add license headers and javadoc to area model and db classes

--- gnd/src/main/java/com/google/android/gnd/model/basemap/Area.java
+++ gnd/src/main/java/com/google/android/gnd/model/basemap/Area.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.gnd.model.basemap;
 
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.auto.value.AutoValue;
 
+/**
+ * An area is a contiguous set of tiles that form a geodesic rectangle.
+ * They are uniquely identifiable by their NE and SW bounds (or NW and SE).
+ */
 @AutoValue
 public abstract class Area {
     public enum State {
