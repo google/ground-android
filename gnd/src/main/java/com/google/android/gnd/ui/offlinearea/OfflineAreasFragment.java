d4f333a9af5ddd26eca64b6eea8a118c9ed50aec
Gino Miceli <228050+gino-m@users.noreply.github.com>
2019-12-23 10:51:05 -0500

    Edit comment

--- gnd/src/main/java/com/google/android/gnd/ui/offlinearea/OfflineAreasFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/offlinearea/OfflineAreasFragment.java
@@ -33,8 +33,8 @@ import com.google.android.gnd.ui.common.TwoLineToolbar;
 
 /**
  * Fragment containing a list of downloaded areas on the device. An area is a set of offline
- * imagery. Users can manage their areas within this fragment. They can delete areas they no longer
- * need or initiate a flow to select and download a new area to the device.
+ * raster tiles. Users can manage their areas within this fragment. They can delete areas they no longer
+ * need or access the UI used to select and download a new area to the device.
  */
 @ActivityScoped
 public class OfflineAreasFragment extends AbstractFragment {
