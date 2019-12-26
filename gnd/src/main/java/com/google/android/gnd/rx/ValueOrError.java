4d7cdc2db757f1cbdb87d43a8462df14266a3f0c
Gino Miceli <228050+gino-m@users.noreply.github.com>
2019-12-26 00:59:17 -0500

    Remove unused wrap/unwrap methods

--- gnd/src/main/java/com/google/android/gnd/rx/ValueOrError.java
+++ gnd/src/main/java/com/google/android/gnd/rx/ValueOrError.java
@@ -16,7 +16,6 @@
 
 package com.google.android.gnd.rx;
 
-import io.reactivex.Single;
 import java8.util.Optional;
 import javax.annotation.Nullable;
 
@@ -54,36 +53,5 @@ public class ValueOrError<T> {
   @Override
   public String toString() {
     return error().map(t -> "Error: " + t).orElse("Value: " + value);
-  }
-
-  /**
-   * Returns a {@link Single} that emits a {@code ValueOrError} representing either the value
-   * emitted by the source on success, or the error which caused the operation to fail. The returned
-   * stream itself should never fail with an error.
-   *
-   * @param source the stream to be modified.
-   * @param <T> the type of value emitted on success.
-   */
-  public static <T> Single<ValueOrError<T>> wrap(Single<T> source) {
-    return source.map(ValueOrError::of).onErrorReturn(ValueOrError::error);
-  }
-
-  /**
-   * Returns a {@link Single} that emits a success value, or fails with an error depending on the
-   * contents of the {@code ValueOrError} emitted by the source stream.
-   *
-   * @param source the stream to be modified.
-   * @param <T> the type of value emitted on success.
-   */
-  public static <T> Single<T> unwrap(Single<ValueOrError<T>> source) {
-    return source.flatMap(ValueOrError::onSuccessOrError);
-  }
-
-  private static <T> Single<T> onSuccessOrError(ValueOrError<T> voe) {
-    return Single.create(
-        em -> {
-          voe.value().ifPresent(value -> em.onSuccess(value));
-          voe.error().ifPresent(error -> em.onError(error));
-        });
   }
 }
