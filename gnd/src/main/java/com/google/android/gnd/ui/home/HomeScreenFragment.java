5f3cc27d80400905afe45994f94de6f2e3947a33
Scott Olsen <scg.olsen@gmail.com>
2019-12-27 12:34:53 -0500

    Add a download offline area action button; update styling
    This change adds an action button to the offline area UI. Users can use the
    button to select a new offline area for download (note that, at this change,
    the button does not yet have any functionality). I've also made the following
    supporting updates:
    - Updated the offline areas fragment layout to use new primary color styling.
    - Removed a redundant call to set the offline area toolbar background color (in
      the home fragment).
    - Added a string for offline area selections.

--- gnd/src/main/java/com/google/android/gnd/ui/home/HomeScreenFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/home/HomeScreenFragment.java
@@ -229,7 +229,6 @@ public class HomeScreenFragment extends AbstractFragment
   //}
 
   private void showOfflineAreas() {
-    toolbar.setBackgroundColor(3);
     viewModel.showOfflineAreas();
   }
 
