1658541a6302c208033b31ba32010b6f5b202588
Shobhit Agarwal <ashobhit@google.com>
2020-01-18 14:27:22 +0530

    Add method to select images from storage

---
+++ gnd/src/main/java/com/google/android/gnd/system/StorageManager.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gnd.system;
+
+import android.Manifest.permission;
+import android.content.Intent;
+import android.util.Log;
+import com.google.android.gnd.rx.RxCompletable;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class StorageManager {
+
+  public static final String TAG = StorageManager.class.getName();
+
+  private static final int PICKFILE_REQUEST_CODE = StorageManager.class.hashCode() & 0xffff;
+  private final PermissionsManager permissionsManager;
+  private final ActivityStreams activityStreams;
+
+  @Inject
+  public StorageManager(PermissionsManager permissionsManager, ActivityStreams activityStreams) {
+    this.permissionsManager = permissionsManager;
+    this.activityStreams = activityStreams;
+  }
+
+  public void requestFilePicker() {
+    activityStreams
+        .getNextActivityResult(PICKFILE_REQUEST_CODE)
+        .doOnNext(
+            r -> {
+              Log.d(TAG, r.getRequestCode() + " : " + r.getData());
+            })
+        .subscribe();
+
+    permissionsManager
+        .obtainPermission(permission.READ_EXTERNAL_STORAGE)
+        .andThen(RxCompletable.completeIf(this::sendIntent))
+        .subscribe();
+  }
+
+  private boolean sendIntent() {
+    Log.d(TAG, "Sending file picker intent");
+    activityStreams.withActivity(
+        activity -> {
+          Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+          intent.setType("image/*");
+          activity.startActivityForResult(intent, PICKFILE_REQUEST_CODE);
+        });
+    return true;
+  }
+}
