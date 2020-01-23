aaa826be81ab1b581544897cfcbeb79a4d8723d9
Scott Olsen <scg.olsen@gmail.com>
2020-01-23 16:27:56 -0500

    Reapply changes from commit b838347
    Original commit message:
    Merge pull request #316 from gino-m/fix-proj-sel
    
    [Project selector] Fix reappearing frozen project selector

--- gnd/src/main/java/com/google/android/gnd/ui/projectselector/ProjectSelectorDialogFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/projectselector/ProjectSelectorDialogFragment.java
@@ -29,7 +29,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.FragmentManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.android.gnd.R;
@@ -54,10 +53,6 @@ public class ProjectSelectorDialogFragment extends AbstractDialogFragment {
 
   private ProjectSelectorViewModel viewModel;
   private ArrayAdapter listAdapter;
-
-  public static void show(FragmentManager fragmentManager) {
-    new ProjectSelectorDialogFragment().show(fragmentManager, TAG);
-  }
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -112,7 +107,7 @@ public class ProjectSelectorDialogFragment extends AbstractDialogFragment {
   }
 
   private void onItemSelected(AdapterView<?> parent, View view, int idx, long id) {
-    getDialog().hide();
+    dismiss();
     viewModel.activateProject(idx);
   }
 }
