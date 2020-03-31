38ce0c510516287940c6350e2d311ec36c9e3810
Shobhit Agarwal <ashobhit@google.com>
2020-03-31 17:38:25 +0530

    Move worker init to service

--- gnd/src/main/java/com/google/android/gnd/system/service/ForegroundService.java
+++ gnd/src/main/java/com/google/android/gnd/system/service/ForegroundService.java
@@ -3,6 +3,9 @@ package com.google.android.gnd.system.service;
 import android.content.Intent;
 import android.os.IBinder;
 import androidx.annotation.Nullable;
+import androidx.work.Configuration;
+import androidx.work.WorkManager;
+import com.google.android.gnd.inject.GndWorkerFactory;
 import com.google.android.gnd.system.NotificationManager;
 import dagger.android.DaggerService;
 import javax.inject.Inject;
@@ -10,6 +13,7 @@ import timber.log.Timber;
 
 public class ForegroundService extends DaggerService {
 
+  @Inject GndWorkerFactory workerFactory;
   @Inject NotificationManager notificationManager;
 
   @Override
@@ -19,7 +23,11 @@ public class ForegroundService extends DaggerService {
         NotificationManager.ALWAYS_ON_NOTIFICATION_ID,
         notificationManager.createForegroundServiceNotification());
 
-    // do heavy work on a background thread
+    // Set custom worker factory that allow Workers to use Dagger injection.
+    // TODO(github.com/google/dagger/issues/1183): Remove once Workers support injection.
+    WorkManager.initialize(
+        this, new Configuration.Builder().setWorkerFactory(workerFactory).build());
+
     // stopSelf();
     return START_NOT_STICKY;
   }
