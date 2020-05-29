059ef94b79b306bf2954a0358c0a802c8b42ca8c
Shobhit Agarwal <ashobhit@google.com>
2020-05-29 23:55:04 +0530

    Suppress PMD warning

--- gnd/src/main/java/com/google/android/gnd/persistence/local/room/entity/OptionEntity.java
+++ gnd/src/main/java/com/google/android/gnd/persistence/local/room/entity/OptionEntity.java
@@ -32,10 +32,10 @@ import com.google.auto.value.AutoValue.CopyAnnotations;
         @ForeignKey(
             entity = FieldEntity.class,
             parentColumns = "id",
-            childColumns = "field_id",
-            onDelete = ForeignKey.CASCADE),
-    indices = {@Index("field_id")},
-    primaryKeys = {"code", "field_id"})
+            childColumns = "field_id", // NOPMD
+          onDelete = ForeignKey.CASCADE),
+    indices = {@Index("field_id")}, // NOPMD
+    primaryKeys = {"code", "field_id"}) // NOPMD
 public abstract class OptionEntity {
 
   @CopyAnnotations
@@ -50,7 +50,7 @@ public abstract class OptionEntity {
 
   @CopyAnnotations
   @NonNull
-  @ColumnInfo(name = "field_id")
+  @ColumnInfo(name = "field_id") // NOPMD
   public abstract String getFieldId();
 
   public static OptionEntity fromOption(String fieldId, Option option) {
