429963b12874ca4e4a64124b205968b3cbe22645
Don Turner <donturner@google.com>
2020-06-11 12:27:50 +0100

    Rename AuthenticationManagerImpl to GoogleAuthenticationManager

--- gnd/src/main/java/com/google/android/gnd/system/auth/AuthenticationModule.java
+++ gnd/src/main/java/com/google/android/gnd/system/auth/AuthenticationModule.java
@@ -1,8 +1,6 @@
 package com.google.android.gnd.system.auth;
 
 import com.google.android.gnd.inject.ActivityScoped;
-import com.google.android.gnd.system.auth.AuthenticationManager;
-import com.google.android.gnd.system.auth.AuthenticationManagerImpl;
 import dagger.Binds;
 import dagger.Module;
 
@@ -11,6 +9,6 @@ public abstract class AuthenticationModule {
 
   @ActivityScoped
   @Binds
-  abstract AuthenticationManager bind(AuthenticationManagerImpl implementation);
+  abstract AuthenticationManager bind(GoogleAuthenticationManager implementation);
 
 }
