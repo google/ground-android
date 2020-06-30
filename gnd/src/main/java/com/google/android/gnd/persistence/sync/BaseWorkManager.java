1b543afbc915cf34767ec1f2757250cfcb4c5478
Shobhit Agarwal <ashobhit@google.com>
2020-06-30 23:51:28 +0530

    Fix styling issues

--- gnd/src/main/java/com/google/android/gnd/persistence/sync/BaseWorkManager.java
+++ gnd/src/main/java/com/google/android/gnd/persistence/sync/BaseWorkManager.java
@@ -39,11 +39,9 @@ import javax.inject.Provider;
  */
 public abstract class BaseWorkManager {
 
-  /** Backoff time should increase linearly.
-   *
-   * TODO: Check if it is possible to wake the worker as soon as the connection becomes available.
-   *  If yes, then switch to EXPONENTIAL backoff policy.
-   */
+  /** Backoff time should increase linearly. */
+  // TODO: Check if it is possible to wake the worker as soon as the connection becomes available.
+  //  If yes, then switch to EXPONENTIAL backoff policy.
   private static final BackoffPolicy BACKOFF_POLICY = BackoffPolicy.LINEAR;
 
   /** Number of milliseconds to wait before retrying failed sync tasks. */
