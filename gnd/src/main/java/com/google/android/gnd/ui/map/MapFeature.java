de5570dcd31b93dfce72c1e5d57b48e61b10c543
Shobhit Agarwal <ashobhit@google.com>
2020-10-22 17:18:29 +0530

    Create abstract class for objects that need to be rendered

---
+++ gnd/src/main/java/com/google/android/gnd/ui/map/MapFeature.java
@@ -0,0 +1,9 @@
+package com.google.android.gnd.ui.map;
+
+/** Abstract class defining objects that need to be rendered on map. */
+public abstract class MapFeature {
+
+  // TODO: Pull common attributes from concrete classes to here.
+  // TODO: Define enum for supported MapFeatures and replace all instanceOf checks
+
+}
