d3ae3144e3f6f5466b5e33446d35308c68bfa080
Heman Parbhakar <hemanp@google.com>
2021-03-25 10:59:54 +0530

    Some minor fixes

--- gnd/src/main/java/com/google/android/gnd/ui/terms/TermsFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/terms/TermsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,8 +46,6 @@ public class TermsFragment extends AbstractFragment implements BackPressListener
 
   @Override
   public boolean onBack() {
-    // Workaround to exit on back from sign-in screen since for some reason
-    // popUpTo is not working on signOut action.
     getActivity().finish();
     return false;
   }
