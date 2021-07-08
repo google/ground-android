394bfca9d9acc8a8ff4d4f781f85c79b7cfc7805
Heman Parbhakar <hemanp@google.com>
2021-07-08 17:09:35 +0530

    Fixed unit test case failure and code clean up.

--- gnd/src/test/java/com/google/android/gnd/ui/MainViewModelTest.java
+++ gnd/src/test/java/com/google/android/gnd/ui/MainViewModelTest.java
@@ -16,12 +16,17 @@
 
 package com.google.android.gnd.ui;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import com.google.android.gnd.MainViewModel;
+import com.google.android.gnd.model.Project;
 import com.google.android.gnd.model.TermsOfService;
+import com.google.android.gnd.model.form.Element;
+import com.google.android.gnd.model.form.Field;
+import com.google.android.gnd.model.form.Form;
+import com.google.android.gnd.model.layer.Layer;
+import com.google.android.gnd.model.layer.Style;
 import com.google.android.gnd.persistence.local.LocalDatabaseModule;
 import com.google.android.gnd.persistence.local.LocalValueStore;
 import com.google.android.gnd.persistence.remote.RemoteDataStore;
@@ -32,18 +37,20 @@ import com.google.android.gnd.repository.UserRepository;
 import com.google.android.gnd.rx.Schedulers;
 import com.google.android.gnd.rx.SchedulersModule;
 import com.google.android.gnd.system.auth.AuthenticationManager;
-import com.google.android.gnd.system.auth.AuthenticationModule;
 import com.google.android.gnd.system.auth.SignInState;
 import com.google.android.gnd.system.auth.SignInState.State;
 import com.google.android.gnd.ui.common.Navigator;
 import com.google.android.gnd.ui.home.HomeScreenFragmentDirections;
 import com.google.android.gnd.ui.signin.SignInFragmentDirections;
+import com.google.common.collect.ImmutableList;
 import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
 import dagger.hilt.android.testing.HiltTestApplication;
 import dagger.hilt.android.testing.UninstallModules;
+import io.reactivex.Flowable;
 import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import java8.util.Optional;
 import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Rule;
@@ -68,23 +75,66 @@ public class MainViewModelTest {
   private static final Observable<SignInState> SIGN_IN_STATE =
       Observable.just(new SignInState(State.SIGNED_IN));
 
+  private static final Field TEST_FIELD =
+      Field.newBuilder()
+          .setId("field id")
+          .setIndex(1)
+          .setLabel("field label")
+          .setRequired(false)
+          .setType(Field.Type.TEXT_FIELD)
+          .build();
+
+  private static final Form TEST_FORM =
+      Form.newBuilder()
+          .setId("form id")
+          .setElements(ImmutableList.of(Element.ofField(TEST_FIELD)))
+          .build();
+
+  private static final Layer TEST_LAYER =
+      Layer.newBuilder()
+          .setId("layer id")
+          .setName("heading title")
+          .setDefaultStyle(Style.builder().setColor("000").build())
+          .setForm(TEST_FORM)
+          .build();
+
+  private static final Project TEST_PROJECT =
+      Project.newBuilder()
+          .setId("project id")
+          .setTitle("project 1")
+          .setDescription("foo description")
+          .putLayer("layer id", TEST_LAYER)
+          .build();
+
+
+  private static final Flowable<Optional<Project>> TEST_ACTIVE_PROJECT  =
+      Flowable.just(Optional.of(TEST_PROJECT));
+
   @Rule
   public MockitoRule rule = MockitoJUnit.rule();
   @Rule
   public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
   @Rule
   public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
   public MainViewModel mainViewModel;
+
+  @Mock
   RemoteDataStore mockRemoteDataStore;
-  AuthenticationManager authenticationManager;
+
+  @Mock
+  AuthenticationManager mockAuthenticationManager;
+
   @Mock
   ProjectRepository mockProjectRepository;
+
   @Mock
   FeatureRepository mockFeatureRepository;
   @Mock
   UserRepository mockUserRepository;
   @Mock
   TermsOfServiceRepository mockTermsOfServiceRepository;
+
   @Inject
   Navigator navigator;
   @Inject
@@ -95,20 +145,21 @@ public class MainViewModelTest {
   @Before
   public void setup() {
     hiltRule.inject();
-    mockProjectRepository = new ProjectRepository(null, mockRemoteDataStore, null, localValueStore);
-    mockRemoteDataStore = mock(RemoteDataStore.class);
-    authenticationManager = mock(AuthenticationManager.class);
 
-    when(authenticationManager.getSignInState()).thenReturn(SIGN_IN_STATE);
+    when(mockAuthenticationManager.getSignInState()).thenReturn(SIGN_IN_STATE);
+    when(mockProjectRepository.getActiveProject()).thenReturn(TEST_ACTIVE_PROJECT);
+
     // returning empty terms of service by default.
     when(mockRemoteDataStore.loadTermsOfService()).thenReturn(Maybe.empty());
+    mockProjectRepository = new ProjectRepository(null, null, mockRemoteDataStore, localValueStore);
+
     mockTermsOfServiceRepository =
         new TermsOfServiceRepository(
             mockRemoteDataStore,
             localValueStore);
     mainViewModel = new MainViewModel(mockProjectRepository, mockFeatureRepository,
         mockUserRepository,
-        mockTermsOfServiceRepository, navigator, authenticationManager, null, schedulers);
+        mockTermsOfServiceRepository, navigator, mockAuthenticationManager, null, schedulers);
   }
 
   @Test
