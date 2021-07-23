5c02bea03092a7aedf453475f58f644d72465fac
Shobhit Agarwal <ashobhit@google.com>
2021-07-23 10:09:55 +0530

    Fix code styling issues

--- gnd/src/test/java/com/google/android/gnd/ui/home/featuredetails/FeatureDetailsViewModelTest.java
+++ gnd/src/test/java/com/google/android/gnd/ui/home/featuredetails/FeatureDetailsViewModelTest.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.gnd.ui.home.featuredetails;
 
+import static com.google.android.gnd.model.feature.FeatureType.POINT;
 import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
@@ -102,7 +119,7 @@ public class FeatureDetailsViewModelTest {
   }
 
   void setSelectedFeature(FeatureType featureType) {
-    Feature feature = featureType == FeatureType.POINT ? TEST_POINT_FEATURE : TEST_POLYGON_FEATURE;
+    Feature feature = featureType == POINT ? TEST_POINT_FEATURE : TEST_POLYGON_FEATURE;
     viewModel.onSelectedFeature(Optional.of(feature));
   }
 }
