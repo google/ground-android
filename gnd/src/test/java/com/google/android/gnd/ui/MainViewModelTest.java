5755dec271bb7097871a09642c75066265097eb4
Shobhit Agarwal <ashobhit@google.com>
2021-07-28 00:42:08 +0530

    Add more tests to MainViewModelTest

--- gnd/src/test/java/com/google/android/gnd/ui/MainViewModelTest.java
+++ gnd/src/test/java/com/google/android/gnd/ui/MainViewModelTest.java
@@ -16,40 +16,47 @@
 
 package com.google.android.gnd.ui;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.navigation.NavDirections;
+import com.google.android.gnd.FakeData;
 import com.google.android.gnd.MainViewModel;
+import com.google.android.gnd.R;
+import com.google.android.gnd.TestObservers;
 import com.google.android.gnd.model.Project;
 import com.google.android.gnd.model.TermsOfService;
-import com.google.android.gnd.model.form.Element;
-import com.google.android.gnd.model.form.Field;
-import com.google.android.gnd.model.form.Form;
-import com.google.android.gnd.model.layer.Layer;
-import com.google.android.gnd.model.layer.Style;
+import com.google.android.gnd.model.User;
 import com.google.android.gnd.persistence.local.LocalDatabaseModule;
-import com.google.android.gnd.persistence.local.LocalValueStore;
-import com.google.android.gnd.persistence.remote.RemoteDataStore;
 import com.google.android.gnd.repository.FeatureRepository;
 import com.google.android.gnd.repository.ProjectRepository;
 import com.google.android.gnd.repository.TermsOfServiceRepository;
 import com.google.android.gnd.repository.UserRepository;
 import com.google.android.gnd.rx.Schedulers;
 import com.google.android.gnd.rx.SchedulersModule;
+import com.google.android.gnd.rx.annotations.Hot;
 import com.google.android.gnd.system.auth.AuthenticationManager;
 import com.google.android.gnd.system.auth.SignInState;
 import com.google.android.gnd.system.auth.SignInState.State;
+import com.google.android.gnd.ui.common.EphemeralPopups;
 import com.google.android.gnd.ui.common.Navigator;
 import com.google.android.gnd.ui.home.HomeScreenFragmentDirections;
 import com.google.android.gnd.ui.signin.SignInFragmentDirections;
-import com.google.common.collect.ImmutableList;
 import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
 import dagger.hilt.android.testing.HiltTestApplication;
 import dagger.hilt.android.testing.UninstallModules;
+import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
 import java8.util.Optional;
 import javax.inject.Inject;
 import org.junit.Before;
@@ -57,122 +64,184 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @HiltAndroidTest
-@UninstallModules({SchedulersModule.class,
-    LocalDatabaseModule.class})
+@UninstallModules({SchedulersModule.class, LocalDatabaseModule.class})
 @Config(application = HiltTestApplication.class)
 @RunWith(RobolectricTestRunner.class)
 public class MainViewModelTest {
 
-  private static final TermsOfService TOS = TermsOfService.builder().setId("1")
-      .setText("Test Terms").build();
+  private static final TermsOfService TEST_TERMS_OF_SERVICE =
+      TermsOfService.builder().setId("1").setText("Test Terms").build();
 
-  private static final Observable<SignInState> SIGN_IN_STATE =
-      Observable.just(new SignInState(State.SIGNED_IN));
+  private static final User TEST_USER = FakeData.TEST_USER;
 
-  private static final Field TEST_FIELD =
-      Field.newBuilder()
-          .setId("field id")
-          .setIndex(1)
-          .setLabel("field label")
-          .setRequired(false)
-          .setType(Field.Type.TEXT_FIELD)
-          .build();
+  private static final Flowable<Optional<Project>> TEST_ACTIVE_PROJECT =
+      Flowable.just(Optional.of(FakeData.TEST_PROJECT));
 
-  private static final Form TEST_FORM =
-      Form.newBuilder()
-          .setId("form id")
-          .setElements(ImmutableList.of(Element.ofField(TEST_FIELD)))
-          .build();
+  @Rule public MockitoRule rule = MockitoJUnit.rule();
+  @Rule public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+  @Rule public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
-  private static final Layer TEST_LAYER =
-      Layer.newBuilder()
-          .setId("layer id")
-          .setName("heading title")
-          .setDefaultStyle(Style.builder().setColor("000").build())
-          .setForm(TEST_FORM)
-          .build();
+  @Mock ProjectRepository mockProjectRepository;
+  @Mock FeatureRepository mockFeatureRepository;
+  @Mock UserRepository mockUserRepository;
+  @Mock TermsOfServiceRepository mockTosRepository;
+  @Mock EphemeralPopups mockPopups;
+  @Mock Navigator mockNavigator;
 
-  private static final Project TEST_PROJECT =
-      Project.newBuilder()
-          .setId("project id")
-          .setTitle("project 1")
-          .setDescription("foo description")
-          .putLayer("layer id", TEST_LAYER)
-          .build();
+  @Inject Schedulers schedulers;
 
-
-  private static final Flowable<Optional<Project>> TEST_ACTIVE_PROJECT  =
-      Flowable.just(Optional.of(TEST_PROJECT));
-
-  @Rule
-  public MockitoRule rule = MockitoJUnit.rule();
-  @Rule
-  public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
-  @Rule
-  public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
-
-  public MainViewModel mainViewModel;
-
-  @Mock
-  RemoteDataStore mockRemoteDataStore;
-
-  @Mock
-  AuthenticationManager mockAuthenticationManager;
-
-  @Mock
-  ProjectRepository mockProjectRepository;
-
-  @Mock
-  FeatureRepository mockFeatureRepository;
-  @Mock
-  UserRepository mockUserRepository;
-  @Mock
-  TermsOfServiceRepository mockTermsOfServiceRepository;
-
-  @Inject
-  Navigator navigator;
-  @Inject
-  LocalValueStore localValueStore;
-  @Inject
-  Schedulers schedulers;
+  private FakeAuthenticationManager authenticationManager;
+  private MainViewModel viewModel;
 
   @Before
   public void setup() {
     hiltRule.inject();
 
-    when(mockAuthenticationManager.getSignInState()).thenReturn(SIGN_IN_STATE);
+    // TODO: Add a test for syncFeatures
     when(mockProjectRepository.getActiveProject()).thenReturn(TEST_ACTIVE_PROJECT);
 
-    // returning empty terms of service by default.
-    when(mockRemoteDataStore.loadTermsOfService()).thenReturn(Maybe.empty());
-    mockProjectRepository = new ProjectRepository(null, null, mockRemoteDataStore, localValueStore);
+    authenticationManager = new FakeAuthenticationManager();
+    viewModel =
+        new MainViewModel(
+            mockProjectRepository,
+            mockFeatureRepository,
+            mockUserRepository,
+            mockTosRepository,
+            mockNavigator,
+            authenticationManager,
+            mockPopups,
+            schedulers);
+  }
 
-    mockTermsOfServiceRepository =
-        new TermsOfServiceRepository(
-            mockRemoteDataStore,
-            localValueStore);
-    mainViewModel = new MainViewModel(mockProjectRepository, mockFeatureRepository,
-        mockUserRepository,
-        mockTermsOfServiceRepository, navigator, mockAuthenticationManager, null, schedulers);
+  private void assertProgressDialogVisible(boolean visible) {
+    TestObservers.observeUntilFirstChange(viewModel.getSignInProgressDialogVisibility());
+    assertThat(viewModel.getSignInProgressDialogVisibility().getValue()).isEqualTo(visible);
+  }
+
+  private void assertNavigate(NavDirections navDirections) {
+    Mockito.verify(mockNavigator, times(1)).navigate(navDirections);
   }
 
   @Test
-  public void testSignedIn_emptyTermsOfService() {
-    mainViewModel.onSignedIn().test().assertValue(HomeScreenFragmentDirections.showHomeScreen());
+  public void testSignInStateChanged_onSignedOut() {
+    authenticationManager.signOut();
+
+    assertProgressDialogVisible(false);
+    assertNavigate(SignInFragmentDirections.showSignInScreen());
+    Mockito.verify(mockProjectRepository, times(1)).clearActiveProject();
+    Mockito.verify(mockUserRepository, times(1)).clearUserPreferences();
+    Mockito.verify(mockTosRepository, times(1)).setTermsOfServiceAccepted(false);
   }
 
   @Test
-  public void testSignedIn_filledTermsOfService() {
-    when(mockRemoteDataStore.loadTermsOfService()).thenReturn(Maybe.just(TOS));
-    mainViewModel.onSignedIn().test().assertValue(SignInFragmentDirections.showTermsOfService()
-        .setTermsOfServiceText(TOS.getText()));
+  public void testSignInStateChanged_onSigningIn() {
+    authenticationManager.signingIn();
+
+    assertProgressDialogVisible(true);
+    Mockito.verify(mockNavigator, times(0)).navigate(any());
+    Mockito.verify(mockTosRepository, times(1)).setTermsOfServiceAccepted(false);
   }
 
+  @Test
+  public void testSignInStateChanged_onSignedIn_whenTosAccepted() {
+    when(mockTosRepository.isTermsOfServiceAccepted()).thenReturn(true);
+    when(mockUserRepository.saveUser(any(User.class))).thenReturn(Completable.complete());
+
+    authenticationManager.signIn();
+
+    assertProgressDialogVisible(false);
+    assertNavigate(HomeScreenFragmentDirections.showHomeScreen());
+    Mockito.verify(mockUserRepository, times(1)).saveUser(TEST_USER);
+    Mockito.verify(mockTosRepository, times(0)).setTermsOfServiceAccepted(anyBoolean());
+  }
+
+  @Test
+  public void testSignInStateChanged_onSignedIn_whenTosNotAccepted() {
+    when(mockTosRepository.isTermsOfServiceAccepted()).thenReturn(false);
+    when(mockUserRepository.saveUser(any(User.class))).thenReturn(Completable.complete());
+    when(mockTosRepository.getTermsOfService()).thenReturn(Maybe.just(TEST_TERMS_OF_SERVICE));
+
+    authenticationManager.signIn();
+
+    assertProgressDialogVisible(false);
+    assertNavigate(
+        SignInFragmentDirections.showTermsOfService()
+            .setTermsOfServiceText(TEST_TERMS_OF_SERVICE.getText()));
+    Mockito.verify(mockUserRepository, times(1)).saveUser(TEST_USER);
+    Mockito.verify(mockTosRepository, times(0)).setTermsOfServiceAccepted(anyBoolean());
+  }
+
+  @Test
+  public void testSignInStateChanged_onSignedIn_whenTosNotAcceptedAndFailedToGetRemoteTos() {
+    when(mockTosRepository.isTermsOfServiceAccepted()).thenReturn(false);
+    when(mockUserRepository.saveUser(any(User.class))).thenReturn(Completable.complete());
+    when(mockTosRepository.getTermsOfService()).thenReturn(Maybe.error(new Exception("foo_error")));
+    TestObserver<Integer> testErrorObserver = viewModel.getUnrecoverableErrors().test();
+
+    authenticationManager.signIn();
+
+    assertProgressDialogVisible(false);
+    Mockito.verify(mockNavigator, times(0)).navigate(any());
+    Mockito.verify(mockUserRepository, times(1)).saveUser(TEST_USER);
+    testErrorObserver.assertValue(R.string.config_load_error);
+    Mockito.verify(mockTosRepository, times(0)).setTermsOfServiceAccepted(anyBoolean());
+  }
+
+  @Test
+  public void testSignInStateChanged_onSignInError() {
+    authenticationManager.error();
+
+    Mockito.verify(mockPopups, times(1)).showError(R.string.sign_in_unsuccessful);
+    assertProgressDialogVisible(false);
+    assertNavigate(SignInFragmentDirections.showSignInScreen());
+    Mockito.verify(mockProjectRepository, times(1)).clearActiveProject();
+    Mockito.verify(mockUserRepository, times(1)).clearUserPreferences();
+    Mockito.verify(mockTosRepository, times(1)).setTermsOfServiceAccepted(false);
+  }
+
+  private static class FakeAuthenticationManager implements AuthenticationManager {
+
+    @Hot(replays = true)
+    private final Subject<SignInState> behaviourSubject = BehaviorSubject.create();
+
+    @Override
+    public Observable<SignInState> getSignInState() {
+      return behaviourSubject;
+    }
+
+    @Override
+    public User getCurrentUser() {
+      return TEST_USER;
+    }
+
+    @Override
+    public void init() {
+      // do nothing
+    }
+
+    public void error() {
+      behaviourSubject.onNext(new SignInState(new Exception("sign-in error")));
+    }
+
+    public void signingIn() {
+      behaviourSubject.onNext(new SignInState(State.SIGNING_IN));
+    }
+
+    @Override
+    public void signIn() {
+      behaviourSubject.onNext(new SignInState(TEST_USER));
+    }
+
+    @Override
+    public void signOut() {
+      behaviourSubject.onNext(new SignInState(State.SIGNED_OUT));
+    }
+  }
 }
-
