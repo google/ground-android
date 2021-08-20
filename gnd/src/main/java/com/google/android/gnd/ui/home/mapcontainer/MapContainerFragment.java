1fc627a9c5a0a6a0acfa483e562a7fce2d84a524
scottolsen <scg.olsen@gmail.com>
2021-08-20 12:57:19 -0400

    Render remote tiles in the map adapter

--- gnd/src/main/java/com/google/android/gnd/ui/home/mapcontainer/MapContainerFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/home/mapcontainer/MapContainerFragment.java
@@ -166,7 +166,7 @@ public class MapContainerFragment extends AbstractFragment {
     homeScreenViewModel
         .getBottomSheetState()
         .observe(this, state -> onBottomSheetStateChange(state, map));
-    mapContainerViewModel.getMbtilesFilePaths().observe(this, map::addTileOverlays);
+    mapContainerViewModel.getMbtilesFilePaths().observe(this, map::addLocalTileOverlays);
 
     // TODO: Do this the RxJava way
     map.moveCamera(mapContainerViewModel.getCameraPosition().getValue());
