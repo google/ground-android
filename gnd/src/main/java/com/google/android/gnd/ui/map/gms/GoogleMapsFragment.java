d316edde8965c1857de902257b30d11fc1d6d1b4
Rachel Prince <rachelprince@google.com>
2021-11-17 18:27:10 -0500

    Anchor points so they custom icon is centered in crosshairs

--- gnd/src/main/java/com/google/android/gnd/ui/map/gms/GoogleMapsFragment.java
+++ gnd/src/main/java/com/google/android/gnd/ui/map/gms/GoogleMapsFragment.java
@@ -364,8 +364,9 @@ public class GoogleMapsFragment extends SupportMapFragment implements MapFragmen
     LatLng position = toLatLng(mapPin.getPosition());
     String color = mapPin.getStyle().getColor();
     BitmapDescriptor icon = markerIconFactory.getMarkerIcon(parseColor(color));
+    // TODO: add the anchor values into the resource dimensions file
     Marker marker =
-        getMap().addMarker(new MarkerOptions().position(position).icon(icon).alpha(1.0f));
+        getMap().addMarker(new MarkerOptions().position(position).icon(icon).anchor(0.5f, 0.85f).alpha(1.0f));
     markers.add(marker);
     marker.setTag(mapPin);
   }
