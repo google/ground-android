ad2d645f3a6776cfc7fd2b746af730ad04115725
Shobhit Agarwal <ashobhit@google.com>
2021-11-18 12:33:19 +0530

    Fix styling issues

--- gnd/src/test/java/com/google/android/gnd/ui/home/mapcontainer/PolygonDrawingViewModelTest.java
+++ gnd/src/test/java/com/google/android/gnd/ui/home/mapcontainer/PolygonDrawingViewModelTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.gnd.ui.home.mapcontainer;
 
 import static com.google.android.gnd.TestObservers.observeUntilFirstChange;
@@ -7,7 +23,6 @@ import static org.junit.Assert.assertThrows;
 import android.view.View;
 import com.google.android.gnd.BaseHiltTest;
 import com.google.android.gnd.FakeData;
-import com.google.android.gnd.TestObservers;
 import com.google.android.gnd.model.feature.Point;
 import com.google.android.gnd.rx.Nil;
 import dagger.hilt.android.testing.HiltAndroidTest;
@@ -176,7 +191,7 @@ public class PolygonDrawingViewModelTest extends BaseHiltTest {
   }
 
   private void assertCompleteButtonVisible(int visibility) {
-    TestObservers.observeUntilFirstChange(viewModel.getPolygonDrawingCompletedVisibility());
+    observeUntilFirstChange(viewModel.getPolygonDrawingCompletedVisibility());
     assertThat(viewModel.getPolygonDrawingCompletedVisibility().getValue()).isEqualTo(visibility);
   }
 
