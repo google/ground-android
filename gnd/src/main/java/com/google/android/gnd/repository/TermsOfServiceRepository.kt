c0b619cdd0ed847d867359eb59bf6976ab88054f
Shobhit Agarwal <ashobhit@google.com>
2022-04-11 20:23:24 +0530

    Convert TermsOfServiceRepository to Kotlin

--- gnd/src/main/java/com/google/android/gnd/repository/TermsOfServiceRepository.kt
+++ gnd/src/main/java/com/google/android/gnd/repository/TermsOfServiceRepository.kt
@@ -14,42 +14,29 @@
  * limitations under the License.
  */
 
-package com.google.android.gnd.repository;
+package com.google.android.gnd.repository
 
-import com.google.android.gnd.model.TermsOfService;
-import com.google.android.gnd.persistence.local.LocalValueStore;
-import com.google.android.gnd.persistence.remote.RemoteDataStore;
-import com.google.android.gnd.rx.annotations.Cold;
-import io.reactivex.Maybe;
-import java.util.concurrent.TimeUnit;
-import javax.inject.Inject;
+import com.google.android.gnd.model.TermsOfService
+import com.google.android.gnd.persistence.local.LocalValueStore
+import com.google.android.gnd.persistence.remote.RemoteDataStore
+import com.google.android.gnd.rx.annotations.Cold
+import io.reactivex.Maybe
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
-public class TermsOfServiceRepository {
+private const val LOAD_REMOTE_PROJECT_TERMS_OF_SERVICE_TIMEOUT_SECS: Long = 30
 
-  private static final long LOAD_REMOTE_PROJECT_TERMS_OF_SERVICE_TIMEOUT_SECS = 30;
+class TermsOfServiceRepository @Inject constructor(
+    private val remoteDataStore: RemoteDataStore, private val localValueStore: LocalValueStore) {
 
-  private final RemoteDataStore remoteDataStore;
-  private final LocalValueStore localValueStore;
+    val termsOfService: @Cold Maybe<TermsOfService>
+        get() = remoteDataStore
+            .loadTermsOfService()
+            .timeout(LOAD_REMOTE_PROJECT_TERMS_OF_SERVICE_TIMEOUT_SECS, TimeUnit.SECONDS)
 
-  @Inject
-  public TermsOfServiceRepository(
-      RemoteDataStore remoteDataStore, LocalValueStore localValueStore) {
-    this.remoteDataStore = remoteDataStore;
-    this.localValueStore = localValueStore;
-  }
-
-  @Cold
-  public Maybe<TermsOfService> getTermsOfService() {
-    return remoteDataStore
-        .loadTermsOfService()
-        .timeout(LOAD_REMOTE_PROJECT_TERMS_OF_SERVICE_TIMEOUT_SECS, TimeUnit.SECONDS);
-  }
-
-  public boolean isTermsOfServiceAccepted() {
-    return localValueStore.isTermsOfServiceAccepted();
-  }
-
-  public void setTermsOfServiceAccepted(boolean value) {
-    localValueStore.setTermsOfServiceAccepted(value);
-  }
+    var isTermsOfServiceAccepted: Boolean
+        get() = localValueStore.isTermsOfServiceAccepted
+        set(value) {
+            localValueStore.isTermsOfServiceAccepted = value
+        }
 }
