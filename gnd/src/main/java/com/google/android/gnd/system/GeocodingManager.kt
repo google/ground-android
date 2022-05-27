63016b7a64d82fbd8fb8458e96b57da1744870c9
Shobhit Agarwal <ashobhit@google.com>
2022-05-28 01:06:03 +0530

    Migrate GeocodingManager.java

--- gnd/src/main/java/com/google/android/gnd/system/GeocodingManager.kt
+++ gnd/src/main/java/com/google/android/gnd/system/GeocodingManager.kt
@@ -13,85 +13,60 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.gnd.system
 
-package com.google.android.gnd.system;
+import android.content.res.Resources
+import android.location.Geocoder
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gnd.R
+import com.google.android.gnd.rx.Schedulers
+import com.google.android.gnd.rx.annotations.Cold
+import io.reactivex.Single
+import java8.util.Optional
+import timber.log.Timber
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
 
-import static java8.util.stream.StreamSupport.stream;
-
-import android.content.res.Resources;
-import android.location.Address;
-import android.location.Geocoder;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gnd.R;
-import com.google.android.gnd.rx.Schedulers;
-import com.google.android.gnd.rx.annotations.Cold;
-import io.reactivex.Single;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java8.util.Optional;
-import java8.util.stream.Collectors;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import timber.log.Timber;
-
-/** Abstracts native geocoding facilities. */
+/** Abstracts native geocoding facilities.  */
 @Singleton
-public class GeocodingManager {
-  static class AddressNotFoundException extends Exception {
-    public AddressNotFoundException(String message) {
-      super(message);
-    }
-  }
+class GeocodingManager @Inject constructor(
+    private val geocoder: Geocoder,
+    private val schedulers: Schedulers,
+    resources: Resources
+) {
+    private val defaultAreaName: String = resources.getString(R.string.unnamed_area)
 
-  private final Geocoder geocoder;
-  private final Schedulers schedulers;
-  private final String defaultAreaName;
-
-  @Inject
-  public GeocodingManager(Geocoder geocoder, Schedulers schedulers, Resources resources) {
-    this.geocoder = geocoder;
-    this.schedulers = schedulers;
-    this.defaultAreaName = resources.getString(R.string.unnamed_area);
-  }
-
-  /**
-   * Retrieve a human readable name for the region bounded by the provided {@param bounds}.
-   *
-   * <p>If no area name is found for the given area, returns a default value.
-   */
-  @Cold
-  public Single<String> getAreaName(LatLngBounds bounds) {
-    return Single.fromCallable(() -> getAreaNameInternal(bounds))
-        .doOnError(throwable -> Timber.e(throwable, "Couldn't get address for bounds: %s", bounds))
-        .subscribeOn(schedulers.io());
-  }
-
-  private String getAreaNameInternal(LatLngBounds bounds)
-      throws AddressNotFoundException, IOException {
-    LatLng center = bounds.getCenter();
-
-    List<Address> addresses = geocoder.getFromLocation(center.latitude, center.longitude, 1);
-    if (addresses.isEmpty()) {
-      throw new AddressNotFoundException("No address found for area.");
+    /**
+     * Retrieve a human readable name for the region bounded by the provided {@param bounds}.
+     *
+     *
+     * If no area name is found for the given area, returns a default value.
+     */
+    fun getAreaName(bounds: LatLngBounds): @Cold Single<String> {
+        return Single.fromCallable { getAreaNameInternal(bounds) }
+            .doOnError { Timber.e(it, "Couldn't get address for bounds: $bounds") }
+            .subscribeOn(schedulers.io())
     }
 
-    Address address = addresses.get(0);
+    @Throws(AddressNotFoundException::class, IOException::class)
+    private fun getAreaNameInternal(bounds: LatLngBounds): String {
+        val center = bounds.center
+        val addresses = geocoder.getFromLocation(center.latitude, center.longitude, 1)
+        if (addresses.isEmpty()) {
+            throw AddressNotFoundException("No address found for area.")
+        }
+        val address = addresses[0]
 
-    // TODO(#613): Decide exactly what set of address parts we want to show the user.
-    String country = Optional.ofNullable(address.getCountryName()).orElse("");
-    String locality = Optional.ofNullable(address.getLocality()).orElse("");
-    String admin = Optional.ofNullable(address.getAdminArea()).orElse("");
-    String subAdmin = Optional.ofNullable(address.getSubAdminArea()).orElse("");
-    Collection<String> components =
-        new ArrayList<>(Arrays.asList(country, locality, admin, subAdmin));
-
-    String fullLocationName =
-        stream(components).filter(x -> !x.isEmpty()).collect(Collectors.joining(", "));
-
-    return fullLocationName.isEmpty() ? defaultAreaName : fullLocationName;
-  }
+        // TODO(#613): Decide exactly what set of address parts we want to show the user.
+        val country = Optional.ofNullable(address.countryName).orElse("")
+        val locality = Optional.ofNullable(address.locality).orElse("")
+        val admin = Optional.ofNullable(address.adminArea).orElse("")
+        val subAdmin = Optional.ofNullable(address.subAdminArea).orElse("")
+        val components: Collection<String> = ArrayList(listOf(country, locality, admin, subAdmin))
+        val fullLocationName = components.filter { it.isNotEmpty() }.joinToString()
+        return fullLocationName.ifEmpty { defaultAreaName }
+    }
 }
+
+internal class AddressNotFoundException(message: String) : Exception(message)
