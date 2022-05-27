17601e1390285b3699ffb85144e2e5c5ae61f795
Shobhit Agarwal <ashobhit@google.com>
2022-05-28 01:01:02 +0530

    Migrate GoogleApiManager.java

--- gnd/src/main/java/com/google/android/gnd/system/GoogleApiManager.kt
+++ gnd/src/main/java/com/google/android/gnd/system/GoogleApiManager.kt
@@ -13,73 +13,62 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.gnd.system
 
-package com.google.android.gnd.system;
+import android.content.Context
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gnd.rx.RxCompletable
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
+import io.reactivex.Completable
+import io.reactivex.CompletableEmitter
+import javax.inject.Inject
 
-import static com.google.android.gnd.rx.RxCompletable.completeOrError;
-
-import android.app.Application;
-import android.content.Context;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import dagger.hilt.android.scopes.ActivityScoped;
-import io.reactivex.Completable;
-import javax.inject.Inject;
+private val INSTALL_API_REQUEST_CODE = GoogleApiAvailability::class.java.hashCode() and 0xffff
 
 @ActivityScoped
-public class GoogleApiManager {
-  private static final int INSTALL_API_REQUEST_CODE =
-      GoogleApiAvailability.class.hashCode() & 0xffff;
-  private final GoogleApiAvailability googleApiAvailability;
-  private final Context context;
-  private final ActivityStreams activityStreams;
+class GoogleApiManager @Inject constructor(
+    @param:ApplicationContext private val context: Context,
+    private val googleApiAvailability: GoogleApiAvailability,
+    private val activityStreams: ActivityStreams
+) {
 
-  @Inject
-  public GoogleApiManager(
-      Application app,
-      GoogleApiAvailability googleApiAvailability,
-      ActivityStreams activityStreams) {
-    this.context = app.getApplicationContext();
-    this.googleApiAvailability = googleApiAvailability;
-    this.activityStreams = activityStreams;
-  }
+    /**
+     * Returns a stream that either completes immediately if Google Play Services are already
+     * installed, otherwise shows install dialog. Terminates with error if install not possible or
+     * cancelled.
+     */
+    fun installGooglePlayServices(): Completable =
+        requestInstallOrComplete().ambWith(getNextInstallApiResult())
 
-  /**
-   * Returns a stream that either completes immediately if Google Play Services are already
-   * installed, otherwise shows install dialog. Terminates with error if install not possible or
-   * cancelled.
-   */
-  public Completable installGooglePlayServices() {
-    return requestInstallOrComplete().ambWith(getNextInstallApiResult());
-  }
-
-  private Completable requestInstallOrComplete() {
-    return Completable.create(
-        em -> {
-          int status = googleApiAvailability.isGooglePlayServicesAvailable(context);
-          if (status == ConnectionResult.SUCCESS) {
-            em.onComplete();
-          } else if (googleApiAvailability.isUserResolvableError(status)) {
-            // TODO: Throw appropriate Exception.
-            activityStreams.withActivity(
-                activity ->
+    private fun requestInstallOrComplete(): Completable {
+        return Completable.create { emitter: CompletableEmitter ->
+            val status = googleApiAvailability.isGooglePlayServicesAvailable(context)
+            if (status == ConnectionResult.SUCCESS) {
+                emitter.onComplete()
+            } else if (googleApiAvailability.isUserResolvableError(status)) {
+                activityStreams.withActivity {
                     googleApiAvailability.showErrorDialogFragment(
-                        activity,
+                        it,
                         status,
-                        INSTALL_API_REQUEST_CODE,
-                        dialogInterface -> em.onError(new Exception())));
+                        INSTALL_API_REQUEST_CODE
+                    ) { emitter.onError(Exception()) } // TODO: Throw appropriate Exception.
+                }
+            } else {
+                emitter.onError(Exception()) // TODO: Throw appropriate Exception.
+            }
+        }
+    }
 
-          } else {
-            // TODO: Throw appropriate Exception.
-            em.onError(new Exception());
-          }
-        });
-  }
 
-  private Completable getNextInstallApiResult() {
-    // TODO: Throw appropriate Exception.
-    return activityStreams
-        .getNextActivityResult(INSTALL_API_REQUEST_CODE)
-        .flatMapCompletable(r -> completeOrError(r::isOk, Exception.class));
-  }
+    private fun getNextInstallApiResult(): Completable =
+        activityStreams
+            .getNextActivityResult(INSTALL_API_REQUEST_CODE)
+            .flatMapCompletable {
+                RxCompletable.completeOrError(
+                    { it.isOk() },
+                    Exception::class.java // TODO: Throw appropriate Exception.
+                )
+            }
 }
