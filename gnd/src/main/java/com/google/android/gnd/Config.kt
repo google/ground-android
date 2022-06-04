de41e6be1c2ef9a60f322ee9b11096dcc84ae637
Shobhit Agarwal <ashobhit@google.com>
2022-06-04 20:28:03 +0530

    Bump db schema version

--- gnd/src/main/java/com/google/android/gnd/Config.kt
+++ gnd/src/main/java/com/google/android/gnd/Config.kt
@@ -25,7 +25,7 @@ object Config {
 
     // Local db settings.
     // TODO(#128): Reset version to 1 before releasing.
-    const val DB_VERSION = 90
+    const val DB_VERSION = 91
     const val DB_NAME = "gnd.db"
 
     // Firebase Cloud Firestore settings.
