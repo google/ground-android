4fc856399f40fc0281cc7811b0a7215decf9c189
Jakob Sunde <jsunde@google.com>
2022-06-06 15:18:47 +0000

    Add unintentionally removed deps back to AddFeatureTest

--- gnd/src/androidTest/java/com/google/android/gnd/AddFeatureTest.java
+++ gnd/src/androidTest/java/com/google/android/gnd/AddFeatureTest.java
@@ -22,6 +22,8 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
