08dee2a9e8f52f0d366be8253f1039593d8bee38
Shobhit Agarwal <ashobhit@google.com>
2022-06-24 23:47:23 +0530

    Add missing import

--- gnd/src/androidTest/java/com/google/android/ground/AcceptTermsOfServiceTest.java
+++ gnd/src/androidTest/java/com/google/android/ground/AcceptTermsOfServiceTest.java
@@ -21,6 +21,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
 import com.google.android.ground.persistence.remote.FakeRemoteDataStore;
