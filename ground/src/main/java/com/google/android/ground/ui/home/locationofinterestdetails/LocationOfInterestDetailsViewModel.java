d5c1e1e55a672bd83ff182a24bb359859281a0d3
Shobhit Agarwal <ashobhit@google.com>
2022-08-23 22:42:17 +0530

    Directly get survey from id in SubmissionRepository

--- ground/src/main/java/com/google/android/ground/ui/home/locationofinterestdetails/LocationOfInterestDetailsViewModel.java
+++ ground/src/main/java/com/google/android/ground/ui/home/locationofinterestdetails/LocationOfInterestDetailsViewModel.java
@@ -141,7 +141,7 @@ public class LocationOfInterestDetailsViewModel extends ViewModel {
         .map(
             locationOfInterest ->
                 submissionRepository.getIncompleteSubmissionMutationsOnceAndStream(
-                    locationOfInterest.getSurvey(), locationOfInterest.getId()))
+                    locationOfInterest.getSurveyId(), locationOfInterest.getId()))
         .orElse(Flowable.just(ImmutableList.of()));
   }
 
