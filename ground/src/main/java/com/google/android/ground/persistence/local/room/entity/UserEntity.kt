33eead234430eeabbecfe0452c6d3bbdb210cf1f
Shobhit Agarwal <ashobhit@google.com>
2022-09-01 23:26:57 +0530

    Convert UserEntity.java to kotlin

--- ground/src/main/java/com/google/android/ground/persistence/local/room/entity/UserEntity.kt
+++ ground/src/main/java/com/google/android/ground/persistence/local/room/entity/UserEntity.kt
@@ -13,81 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground.persistence.local.room.entity
 
-package com.google.android.ground.persistence.local.room.entity;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.android.ground.model.User
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-import com.google.android.ground.model.User;
-import com.google.auto.value.AutoValue;
-import com.google.auto.value.AutoValue.CopyAnnotations;
-
-@AutoValue
 @Entity(tableName = "user")
-public abstract class UserEntity {
-
-  @CopyAnnotations
-  @NonNull
-  @PrimaryKey
-  @ColumnInfo(name = "id")
-  public abstract String getId();
-
-  @CopyAnnotations
-  @NonNull
-  @ColumnInfo(name = "email")
-  public abstract String getEmail();
-
-  @CopyAnnotations
-  @NonNull
-  @ColumnInfo(name = "display_name")
-  public abstract String getDisplayName();
-
+data class UserEntity(
+  @ColumnInfo(name = "id") @PrimaryKey val id: String,
+  @ColumnInfo(name = "email") val email: String,
+  @ColumnInfo(name = "display_name") val displayName: String,
   // TODO(https://github.com/google/ground-android/issues/964): Save to remote db
-  @CopyAnnotations
-  @Nullable
-  @ColumnInfo(name = "photo_url")
-  public abstract String getPhotoUrl();
+  @ColumnInfo(name = "photo_url") val photoUrl: String?
+) {
 
-  public static UserEntity fromUser(User user) {
-    return UserEntity.builder()
-        .setId(user.getId())
-        .setEmail(user.getEmail())
-        .setDisplayName(user.getDisplayName())
-        .setPhotoUrl(user.getPhotoUrl())
-        .build();
-  }
+  companion object {
+    fun fromUser(user: User): UserEntity =
+      UserEntity(
+        id = user.id,
+        email = user.email,
+        displayName = user.displayName,
+        photoUrl = user.photoUrl
+      )
 
-  public static User toUser(UserEntity u) {
-    return new User(u.getId(), u.getEmail(), u.getDisplayName(), u.getPhotoUrl());
-  }
-
-  public static UserEntity create(String id, String email, String displayName, String photoUrl) {
-    return builder()
-        .setId(id)
-        .setEmail(email)
-        .setDisplayName(displayName)
-        .setPhotoUrl(photoUrl)
-        .build();
-  }
-
-  public static Builder builder() {
-    return new AutoValue_UserEntity.Builder();
-  }
-
-  @AutoValue.Builder
-  public abstract static class Builder {
-
-    public abstract Builder setId(String id);
-
-    public abstract Builder setEmail(String email);
-
-    public abstract Builder setDisplayName(String displayName);
-
-    public abstract Builder setPhotoUrl(@Nullable String photoUrl);
-
-    public abstract UserEntity build();
+    fun toUser(u: UserEntity): User = User(u.id, u.email, u.displayName, u.photoUrl)
   }
 }
