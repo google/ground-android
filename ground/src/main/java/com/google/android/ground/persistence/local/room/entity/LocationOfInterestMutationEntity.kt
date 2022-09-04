a3f3a39a606bc3415bdc9ad5ae1feb0ba8fc7c95
Shobhit Agarwal <ashobhit@google.com>
2022-09-04 13:00:52 +0530

    Migrate mutation entities to kotlin

--- ground/src/main/java/com/google/android/ground/persistence/local/room/entity/LocationOfInterestMutationEntity.kt
+++ ground/src/main/java/com/google/android/ground/persistence/local/room/entity/LocationOfInterestMutationEntity.kt
@@ -13,148 +13,85 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground.persistence.local.room.entity
 
-package com.google.android.ground.persistence.local.room.entity;
-
-import static androidx.room.ForeignKey.CASCADE;
-import static com.google.android.ground.persistence.local.room.entity.LocationOfInterestEntity.formatVertices;
-import static com.google.android.ground.persistence.local.room.entity.LocationOfInterestEntity.parseVertices;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.room.ColumnInfo;
-import androidx.room.Embedded;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.Index;
-import com.google.android.ground.model.mutation.LocationOfInterestMutation;
-import com.google.android.ground.persistence.local.room.models.Coordinates;
-import com.google.android.ground.persistence.local.room.models.MutationEntitySyncStatus;
-import com.google.android.ground.persistence.local.room.models.MutationEntityType;
-import com.google.auto.value.AutoValue;
-import com.google.auto.value.AutoValue.CopyAnnotations;
-import java.util.Date;
-import java8.util.Optional;
+import androidx.room.*
+import com.google.android.ground.model.geometry.Point
+import com.google.android.ground.model.mutation.LocationOfInterestMutation
+import com.google.android.ground.persistence.local.room.entity.LocationOfInterestEntity.Companion.formatVertices
+import com.google.android.ground.persistence.local.room.entity.LocationOfInterestEntity.Companion.parseVertices
+import com.google.android.ground.persistence.local.room.models.Coordinates
+import com.google.android.ground.persistence.local.room.models.MutationEntitySyncStatus
+import com.google.android.ground.persistence.local.room.models.MutationEntityType
+import java.util.*
+import java8.util.Optional
 
 /**
  * Defines how Room persists LOI mutations for remote sync in the local db. By default, Room uses
  * the name of object fields and their respective types to determine database column names and
  * types.
  */
-@AutoValue
 @Entity(
-    tableName = "location_of_interest_mutation",
-    foreignKeys =
-        @ForeignKey(
-            entity = LocationOfInterestEntity.class,
-            parentColumns = "id",
-            childColumns = "location_of_interest_id",
-            onDelete = CASCADE),
-    indices = {@Index("location_of_interest_id")})
-public abstract class LocationOfInterestMutationEntity extends MutationEntity {
-
-  @CopyAnnotations
-  @NonNull
-  @ColumnInfo(name = "location_of_interest_id")
-  public abstract String getLocationOfInterestId();
-
-  @CopyAnnotations
-  @NonNull
-  @ColumnInfo(name = "job_id")
-  public abstract String getJobId();
-
+  tableName = "location_of_interest_mutation",
+  foreignKeys =
+    [
+      ForeignKey(
+        entity = LocationOfInterestEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["location_of_interest_id"],
+        onDelete = ForeignKey.CASCADE
+      )],
+  indices = [Index("location_of_interest_id")]
+)
+data class LocationOfInterestMutationEntity(
+  @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long? = 0,
+  @ColumnInfo(name = "survey_id") val surveyId: String,
+  @ColumnInfo(name = "type") val type: MutationEntityType,
+  @ColumnInfo(name = "state") val syncStatus: MutationEntitySyncStatus,
+  @ColumnInfo(name = "retry_count") val retryCount: Long,
+  @ColumnInfo(name = "last_error") val lastError: String,
+  @ColumnInfo(name = "user_id") val userId: String,
+  @ColumnInfo(name = "client_timestamp") val clientTimestamp: Long,
+  @ColumnInfo(name = "location_of_interest_id") val locationOfInterestId: String,
+  @ColumnInfo(name = "job_id") val jobId: String,
   /** Non-null if the LOI's location was updated, null if unchanged. */
-  @CopyAnnotations
-  @Nullable
-  @Embedded
-  public abstract Coordinates getNewLocation();
-
-  // Boilerplate generated using Android Studio AutoValue plugin:
-
+  @Embedded val newLocation: Coordinates?,
   /** Non-empty if a polygon's vertices were updated, null if unchanged. */
-  @CopyAnnotations
-  @Nullable
-  @ColumnInfo(name = "polygon_vertices")
-  public abstract String getNewPolygonVertices();
+  @ColumnInfo(name = "polygon_vertices") val newPolygonVertices: String?
+) {
 
-  public static LocationOfInterestMutationEntity fromMutation(LocationOfInterestMutation m) {
-    return LocationOfInterestMutationEntity.builder()
-        .setId(m.getId())
-        .setSurveyId(m.getSurveyId())
-        .setLocationOfInterestId(m.getLocationOfInterestId())
-        .setJobId(m.getJobId())
-        .setNewLocation(m.getLocation().map(Coordinates::fromPoint).orElse(null))
-        .setNewPolygonVertices(formatVertices(m.getPolygonVertices()))
-        .setType(MutationEntityType.fromMutationType(m.getType()))
-        .setSyncStatus(MutationEntitySyncStatus.fromMutationSyncStatus(m.getSyncStatus()))
-        .setRetryCount(m.getRetryCount())
-        .setLastError(m.getLastError())
-        .setUserId(m.getUserId())
-        .setClientTimestamp(m.getClientTimestamp().getTime())
-        .build();
-  }
-
-  public LocationOfInterestMutation toMutation() {
+  fun toMutation(): LocationOfInterestMutation {
     return LocationOfInterestMutation.builder()
-        .setJobId(getJobId())
-        .setLocation(Optional.ofNullable(getNewLocation()).map(Coordinates::toPoint))
-        .setPolygonVertices(parseVertices(getNewPolygonVertices()))
-        .setId(getId())
-        .setSurveyId(getSurveyId())
-        .setLocationOfInterestId(getLocationOfInterestId())
-        .setType(getType().toMutationType())
-        .setSyncStatus(getSyncStatus().toMutationSyncStatus())
-        .setRetryCount(getRetryCount())
-        .setLastError(getLastError())
-        .setUserId(getUserId())
-        .setClientTimestamp(new Date(getClientTimestamp()))
-        .build();
+      .setJobId(jobId)
+      .setLocation(Optional.ofNullable(newLocation).map { obj: Coordinates? -> obj!!.toPoint() })
+      .setPolygonVertices(parseVertices(newPolygonVertices))
+      .setId(id)
+      .setSurveyId(surveyId)
+      .setLocationOfInterestId(locationOfInterestId)
+      .setType(type.toMutationType())
+      .setSyncStatus(syncStatus.toMutationSyncStatus())
+      .setRetryCount(retryCount)
+      .setLastError(lastError)
+      .setUserId(userId)
+      .setClientTimestamp(Date(clientTimestamp))
+      .build()
   }
 
-  public static LocationOfInterestMutationEntity create(
-      @Nullable Long id,
-      String surveyId,
-      String locationOfInterestId,
-      String jobId,
-      MutationEntityType type,
-      MutationEntitySyncStatus syncStatus,
-      @Nullable Coordinates newLocation,
-      @Nullable String newPolygonVertices,
-      long retryCount,
-      @Nullable String lastError,
-      String userId,
-      long clientTimestamp) {
-    return builder()
-        .setId(id)
-        .setSurveyId(surveyId)
-        .setLocationOfInterestId(locationOfInterestId)
-        .setJobId(jobId)
-        .setType(type)
-        .setSyncStatus(syncStatus)
-        .setNewLocation(newLocation)
-        .setNewPolygonVertices(newPolygonVertices)
-        .setRetryCount(retryCount)
-        .setLastError(lastError)
-        .setUserId(userId)
-        .setClientTimestamp(clientTimestamp)
-        .build();
-  }
-
-  public static Builder builder() {
-    return new AutoValue_LocationOfInterestMutationEntity.Builder();
-  }
-
-  @AutoValue.Builder
-  public abstract static class Builder extends MutationEntity.Builder<Builder> {
-
-    public abstract Builder setLocationOfInterestId(String newLocationOfInterestId);
-
-    public abstract Builder setJobId(String newJobId);
-
-    public abstract Builder setNewLocation(@Nullable Coordinates newNewLocation);
-
-    public abstract Builder setNewPolygonVertices(@Nullable String newPolygonVertices);
-
-    public abstract LocationOfInterestMutationEntity build();
+  companion object {
+    fun fromMutation(m: LocationOfInterestMutation): LocationOfInterestMutationEntity =
+      LocationOfInterestMutationEntity(
+        id = m.id,
+        surveyId = m.surveyId,
+        locationOfInterestId = m.locationOfInterestId,
+        jobId = m.jobId,
+        newLocation = m.location.map { point: Point? -> Coordinates.fromPoint(point) }.orElse(null),
+        newPolygonVertices = formatVertices(m.polygonVertices),
+        type = MutationEntityType.fromMutationType(m.type),
+        syncStatus = MutationEntitySyncStatus.fromMutationSyncStatus(m.syncStatus),
+        retryCount = m.retryCount,
+        lastError = m.lastError,
+        userId = m.userId,
+        clientTimestamp = m.clientTimestamp.time
+      )
   }
 }
--- ground/src/main/java/com/google/android/ground/persistence/local/room/entity/SubmissionMutationEntity.kt
+++ ground/src/main/java/com/google/android/ground/persistence/local/room/entity/SubmissionMutationEntity.kt
@@ -13,158 +13,97 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground.persistence.local.room.entity
 
-package com.google.android.ground.persistence.local.room.entity;
+import androidx.room.*
+import com.google.android.ground.model.Survey
+import com.google.android.ground.model.mutation.SubmissionMutation
+import com.google.android.ground.persistence.local.LocalDataConsistencyException
+import com.google.android.ground.persistence.local.room.converter.ResponseDeltasConverter.fromString
+import com.google.android.ground.persistence.local.room.converter.ResponseDeltasConverter.toString
+import com.google.android.ground.persistence.local.room.models.MutationEntitySyncStatus
+import com.google.android.ground.persistence.local.room.models.MutationEntityType
+import java.util.*
 
-import static androidx.room.ForeignKey.CASCADE;
-
-import androidx.annotation.Nullable;
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.Index;
-import com.google.android.ground.model.Survey;
-import com.google.android.ground.model.job.Job;
-import com.google.android.ground.model.mutation.SubmissionMutation;
-import com.google.android.ground.persistence.local.LocalDataConsistencyException;
-import com.google.android.ground.persistence.local.room.converter.ResponseDeltasConverter;
-import com.google.android.ground.persistence.local.room.models.MutationEntitySyncStatus;
-import com.google.android.ground.persistence.local.room.models.MutationEntityType;
-import com.google.auto.value.AutoValue;
-import com.google.auto.value.AutoValue.CopyAnnotations;
-import java.util.Date;
-import org.json.JSONObject;
-
-/** Representation of a {@link SubmissionMutation} in local data store. */
-@AutoValue
+/** Representation of a [SubmissionMutation] in local data store. */
 @Entity(
-    tableName = "submission_mutation",
-    foreignKeys = {
-      @ForeignKey(
-          entity = LocationOfInterestEntity.class,
-          parentColumns = "id",
-          childColumns = "location_of_interest_id",
-          onDelete = CASCADE),
-      @ForeignKey(
-          entity = SubmissionEntity.class,
-          parentColumns = "id",
-          childColumns = "submission_id",
-          onDelete = CASCADE)
-    },
-    indices = {@Index("location_of_interest_id"), @Index("submission_id")})
-public abstract class SubmissionMutationEntity extends MutationEntity {
-
-  @CopyAnnotations
-  @ColumnInfo(name = "location_of_interest_id")
-  public abstract String getLocationOfInterestId();
-
-  @CopyAnnotations
-  @ColumnInfo(name = "job_id")
-  public abstract String getJobId();
-
-  @CopyAnnotations
-  @ColumnInfo(name = "submission_id")
-  public abstract String getSubmissionId();
-
+  tableName = "submission_mutation",
+  foreignKeys =
+    [
+      ForeignKey(
+        entity = LocationOfInterestEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["location_of_interest_id"],
+        onDelete = ForeignKey.CASCADE
+      ),
+      ForeignKey(
+        entity = SubmissionEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["submission_id"],
+        onDelete = ForeignKey.CASCADE
+      )],
+  indices = [Index("location_of_interest_id"), Index("submission_id")]
+)
+data class SubmissionMutationEntity(
+  @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long? = 0,
+  @ColumnInfo(name = "survey_id") val surveyId: String,
+  @ColumnInfo(name = "type") val type: MutationEntityType,
+  @ColumnInfo(name = "state") val syncStatus: MutationEntitySyncStatus,
+  @ColumnInfo(name = "retry_count") val retryCount: Long,
+  @ColumnInfo(name = "last_error") val lastError: String,
+  @ColumnInfo(name = "user_id") val userId: String,
+  @ColumnInfo(name = "client_timestamp") val clientTimestamp: Long,
+  @ColumnInfo(name = "location_of_interest_id") val locationOfInterestId: String,
+  @ColumnInfo(name = "job_id") val jobId: String,
+  @ColumnInfo(name = "submission_id") val submissionId: String,
   /**
-   * For mutations of type {@link MutationEntityType#CREATE} and {@link MutationEntityType#UPDATE},
-   * returns a {@link JSONObject} with the new values of modified task responses, with {@code null}
-   * values representing responses that were removed/cleared.
+   * For mutations of type [MutationEntityType.CREATE] and [MutationEntityType.UPDATE], returns a
+   * [JSONObject] with the new values of modified task responses, with `null` values representing
+   * responses that were removed/cleared.
    *
-   * <p>This method returns {@code null} for mutation type {@link MutationEntityType#DELETE}.
+   * This method returns `null` for mutation type [MutationEntityType.DELETE].
    */
-  @CopyAnnotations
-  @Nullable
-  @ColumnInfo(name = "response_deltas")
-  public abstract String getResponseDeltas();
+  @ColumnInfo(name = "response_deltas") val responseDeltas: String?
+) {
 
-  public static SubmissionMutationEntity create(
-      long id,
-      String surveyId,
-      String locationOfInterestId,
-      String jobId,
-      String submissionId,
-      MutationEntityType type,
-      MutationEntitySyncStatus syncStatus,
-      String responseDeltas,
-      long retryCount,
-      @Nullable String lastError,
-      @Nullable String userId,
-      long clientTimestamp) {
-    return builder()
-        .setId(id)
-        .setSurveyId(surveyId)
-        .setLocationOfInterestId(locationOfInterestId)
-        .setJobId(jobId)
-        .setSubmissionId(submissionId)
-        .setType(type)
-        .setSyncStatus(syncStatus)
-        .setResponseDeltas(responseDeltas)
-        .setRetryCount(retryCount)
-        .setLastError(lastError)
-        .setUserId(userId)
-        .setClientTimestamp(clientTimestamp)
-        .build();
-  }
-
-  public static SubmissionMutationEntity fromMutation(SubmissionMutation m) {
-    return SubmissionMutationEntity.builder()
-        .setId(m.getId())
-        .setSurveyId(m.getSurveyId())
-        .setLocationOfInterestId(m.getLocationOfInterestId())
-        .setJobId(m.getJob().getId())
-        .setSubmissionId(m.getSubmissionId())
-        .setType(MutationEntityType.fromMutationType(m.getType()))
-        .setSyncStatus(MutationEntitySyncStatus.fromMutationSyncStatus(m.getSyncStatus()))
-        .setResponseDeltas(ResponseDeltasConverter.toString(m.getResponseDeltas()))
-        .setRetryCount(m.getRetryCount())
-        .setLastError(m.getLastError())
-        .setUserId(m.getUserId())
-        .setClientTimestamp(m.getClientTimestamp().getTime())
-        .build();
-  }
-
-  // Boilerplate generated using Android Studio AutoValue plugin:
-
-  public static Builder builder() {
-    return new AutoValue_SubmissionMutationEntity.Builder();
-  }
-
-  public SubmissionMutation toMutation(Survey survey) throws LocalDataConsistencyException {
-    Job job =
-        survey
-            .getJob(getJobId())
-            .orElseThrow(
-                () ->
-                    new LocalDataConsistencyException(
-                        "Unknown jobId in submission mutation " + getId()));
+  @Throws(LocalDataConsistencyException::class)
+  fun toMutation(survey: Survey): SubmissionMutation {
+    val job =
+      survey.getJob(jobId).orElseThrow {
+        LocalDataConsistencyException("Unknown jobId in submission mutation $id")
+      }
     return SubmissionMutation.builder()
-        .setJob(job)
-        .setSubmissionId(getSubmissionId())
-        .setResponseDeltas(ResponseDeltasConverter.fromString(job, getResponseDeltas()))
-        .setId(getId())
-        .setSurveyId(getSurveyId())
-        .setLocationOfInterestId(getLocationOfInterestId())
-        .setType(getType().toMutationType())
-        .setSyncStatus(getSyncStatus().toMutationSyncStatus())
-        .setRetryCount(getRetryCount())
-        .setLastError(getLastError())
-        .setUserId(getUserId())
-        .setClientTimestamp(new Date(getClientTimestamp()))
-        .build();
+      .setJob(job)
+      .setSubmissionId(submissionId)
+      .setResponseDeltas(fromString(job, responseDeltas))
+      .setId(id)
+      .setSurveyId(surveyId)
+      .setLocationOfInterestId(locationOfInterestId)
+      .setType(type.toMutationType())
+      .setSyncStatus(syncStatus.toMutationSyncStatus())
+      .setRetryCount(retryCount)
+      .setLastError(lastError)
+      .setUserId(userId)
+      .setClientTimestamp(Date(clientTimestamp))
+      .build()
   }
 
-  @AutoValue.Builder
-  public abstract static class Builder extends MutationEntity.Builder<Builder> {
+  companion object {
 
-    public abstract Builder setLocationOfInterestId(String newLocationOfInterestId);
-
-    public abstract Builder setJobId(String newJobId);
-
-    public abstract Builder setSubmissionId(String newSubmissionId);
-
-    public abstract Builder setResponseDeltas(String newResponseDeltas);
-
-    public abstract SubmissionMutationEntity build();
+    fun fromMutation(m: SubmissionMutation): SubmissionMutationEntity =
+      SubmissionMutationEntity(
+        id = m.id,
+        surveyId = m.surveyId,
+        locationOfInterestId = m.locationOfInterestId,
+        jobId = m.job!!.id,
+        submissionId = m.submissionId,
+        type = MutationEntityType.fromMutationType(m.type),
+        syncStatus = MutationEntitySyncStatus.fromMutationSyncStatus(m.syncStatus),
+        responseDeltas = toString(m.responseDeltas),
+        retryCount = m.retryCount,
+        lastError = m.lastError,
+        userId = m.userId,
+        clientTimestamp = m.clientTimestamp.time
+      )
   }
 }
