30eb91e1910fe7d25d45c27c3eca40d73b558387
Shobhit Agarwal <ashobhit@google.com>
2022-09-28 21:10:57 +0530

    Fix value of survey organizer role

--- ground/src/main/java/com/google/android/ground/model/Constants.kt
+++ ground/src/main/java/com/google/android/ground/model/Constants.kt
@@ -17,7 +17,7 @@ package com.google.android.ground.model
 
 object Constants {
   const val OWNER = "owner"
-  const val SURVEY_ORGANIZER = "survey-organizer"
-  const val DATA_COLLECTOR = "data-collector"
+  const val SURVEY_ORGANIZER = "survey_organizer"
+  const val DATA_COLLECTOR = "data_collector"
   const val VIEWER_ROLE = "viewer"
 }
