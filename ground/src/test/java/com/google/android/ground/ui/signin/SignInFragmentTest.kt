bad603858e5a2c5dac1a3355b116fbeddda534c2
Shobhit Agarwal <ashobhit@google.com>
2022-10-09 13:02:42 +0530

    Create a simple test for checking button click for SignInFragment

---
+++ ground/src/test/java/com/google/android/ground/ui/signin/SignInFragmentTest.kt
@@ -0,0 +1,44 @@
+package com.google.android.ground.ui.signin
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.google.android.ground.BaseHiltTest
+import com.google.android.ground.R
+import com.google.android.ground.launchFragmentInHiltContainer
+import com.google.android.ground.system.auth.SignInState
+import com.sharedtest.FakeData
+import com.sharedtest.system.auth.FakeAuthenticationManager
+import dagger.hilt.android.testing.HiltAndroidTest
+import javax.inject.Inject
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@HiltAndroidTest
+@RunWith(RobolectricTestRunner::class)
+class SignInFragmentTest : BaseHiltTest() {
+
+  @Inject lateinit var fakeAuthenticationManager: FakeAuthenticationManager
+
+  override fun setUp() {
+    super.setUp()
+    fakeAuthenticationManager.setState(SignInState.signedOut())
+  }
+
+  @Test
+  fun buttonClick_shouldSignIn() {
+    launchFragmentInHiltContainer<SignInFragment>()
+    fakeAuthenticationManager.setUser(TEST_USER)
+
+    onView(withId(R.id.sign_in_button)).perform(click())
+
+    fakeAuthenticationManager.signInState
+      .test()
+      .assertValue(SignInState(SignInState.State.SIGNED_IN, Result.success(TEST_USER)))
+  }
+
+  companion object {
+    private val TEST_USER = FakeData.USER
+  }
+}
