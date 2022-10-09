3465194884945b0adebfa2a09b86d09fcab705d3
Shobhit Agarwal <ashobhit@google.com>
2022-10-09 13:02:19 +0530

    Configure fragment testing in isolation

---
+++ ground/src/debug/java/com/google/android/ground/HiltTestActivity.kt
@@ -0,0 +1,6 @@
+package com.google.android.ground
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint class HiltTestActivity : AppCompatActivity()
