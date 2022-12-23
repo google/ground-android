da52f7933e9ac220239678b6423da5e926cd89ed
Shobhit Agarwal <ashobhit@google.com>
2022-12-23 15:59:17 +0530

    Convert TaskViewFactory to kotlin

--- ground/src/main/java/com/google/android/ground/ui/editsubmission/TaskViewFactory.kt
+++ ground/src/main/java/com/google/android/ground/ui/editsubmission/TaskViewFactory.kt
@@ -13,83 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground.ui.editsubmission
 
-package com.google.android.ground.ui.editsubmission;
+import android.widget.LinearLayout
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.google.android.ground.BR
+import com.google.android.ground.R
+import com.google.android.ground.model.task.Task
+import com.google.android.ground.ui.common.ViewModelFactory
+import com.google.android.ground.ui.util.ViewUtil.assignGeneratedId
+import javax.inject.Inject
 
-import static com.google.android.ground.ui.util.ViewUtil.assignGeneratedId;
+/** Inflates a new view and generates a view model for a given [Task.Type]. */
+class TaskViewFactory @Inject internal constructor() {
 
-import android.widget.LinearLayout;
-import androidx.annotation.LayoutRes;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
-import androidx.fragment.app.Fragment;
-import com.google.android.ground.BR;
-import com.google.android.ground.R;
-import com.google.android.ground.model.task.Task;
-import com.google.android.ground.ui.common.ViewModelFactory;
-import javax.inject.Inject;
-
-/** Inflates a new view and generates a view model for a given {@link Task.Type}. */
-public class TaskViewFactory {
-
-  @Inject Fragment fragment;
-  @Inject ViewModelFactory viewModelFactory;
-
-  @Inject
-  TaskViewFactory() {}
-
-  public static Class<? extends AbstractTaskViewModel> getViewModelClass(Task.Type taskType) {
-    switch (taskType) {
-      case TEXT:
-        return TextTaskViewModel.class;
-      case MULTIPLE_CHOICE:
-        return MultipleChoiceTaskViewModel.class;
-      case PHOTO:
-        return PhotoTaskViewModel.class;
-      case NUMBER:
-        return NumberTaskViewModel.class;
-      case DATE:
-        return DateTaskViewModel.class;
-      case TIME:
-        return TimeTaskViewModel.class;
-      default:
-        throw new IllegalArgumentException("Unsupported task type: " + taskType);
-    }
-  }
-
-  @LayoutRes
-  private static int getLayoutId(Task.Type taskType) {
-    switch (taskType) {
-      case TEXT:
-        return R.layout.text_input_task;
-      case MULTIPLE_CHOICE:
-        return R.layout.multiple_choice_input_task;
-      case PHOTO:
-        return R.layout.photo_input_task;
-      case NUMBER:
-        return R.layout.number_input_task;
-      case DATE:
-        return R.layout.date_input_task;
-      case TIME:
-        return R.layout.time_input_task;
-      default:
-        throw new IllegalArgumentException("Unsupported task type: " + taskType);
-    }
-  }
+  @Inject lateinit var fragment: Fragment
+  @Inject lateinit var viewModelFactory: ViewModelFactory
 
   /**
-   * Inflates the view, generates a new view model and binds to the {@link ViewDataBinding}.
+   * Inflates the view, generates a new view model and binds to the [ViewDataBinding].
    *
    * @param taskType Type of the task
    * @param root Parent layout
-   * @return {@link ViewDataBinding}
+   * @return [ViewDataBinding]
    */
-  ViewDataBinding addTaskView(Task.Type taskType, LinearLayout root) {
-    ViewDataBinding binding =
-        DataBindingUtil.inflate(fragment.getLayoutInflater(), getLayoutId(taskType), root, true);
-    binding.setLifecycleOwner(fragment);
-    binding.setVariable(BR.viewModel, viewModelFactory.create(getViewModelClass(taskType)));
-    assignGeneratedId(binding.getRoot());
-    return binding;
+  fun addTaskView(taskType: Task.Type, root: LinearLayout): ViewDataBinding {
+    val binding =
+      DataBindingUtil.inflate<ViewDataBinding>(
+        fragment.layoutInflater,
+        getLayoutId(taskType),
+        root,
+        true
+      )
+    binding.lifecycleOwner = fragment
+    binding.setVariable(BR.viewModel, viewModelFactory.create(getViewModelClass(taskType)))
+    assignGeneratedId(binding.root)
+    return binding
+  }
+
+  companion object {
+    fun getViewModelClass(taskType: Task.Type): Class<out AbstractTaskViewModel> {
+      return when (taskType) {
+        Task.Type.TEXT -> TextTaskViewModel::class.java
+        Task.Type.MULTIPLE_CHOICE -> MultipleChoiceTaskViewModel::class.java
+        Task.Type.PHOTO -> PhotoTaskViewModel::class.java
+        Task.Type.NUMBER -> NumberTaskViewModel::class.java
+        Task.Type.DATE -> DateTaskViewModel::class.java
+        Task.Type.TIME -> TimeTaskViewModel::class.java
+        else -> throw IllegalArgumentException("Unsupported task type: $taskType")
+      }
+    }
+
+    @LayoutRes
+    private fun getLayoutId(taskType: Task.Type): Int {
+      return when (taskType) {
+        Task.Type.TEXT -> R.layout.text_input_task
+        Task.Type.MULTIPLE_CHOICE -> R.layout.multiple_choice_input_task
+        Task.Type.PHOTO -> R.layout.photo_input_task
+        Task.Type.NUMBER -> R.layout.number_input_task
+        Task.Type.DATE -> R.layout.date_input_task
+        Task.Type.TIME -> R.layout.time_input_task
+        else -> throw IllegalArgumentException("Unsupported task type: $taskType")
+      }
+    }
   }
 }
