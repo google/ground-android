1923575e40d275b50d25f0c3899d34de48c42698
Shobhit Agarwal <ashobhit@google.com>
2023-01-11 00:29:45 +0530

    Add a marker at the center of the screen and refresh whenever the map is repositioned

--- ground/src/main/java/com/google/android/ground/ui/datacollection/DropAPinTaskFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/datacollection/DropAPinTaskFragment.kt
@@ -15,24 +15,30 @@
  */
 package com.google.android.ground.ui.datacollection
 
+import android.graphics.Color.parseColor
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.ground.databinding.BasemapLayoutBinding
+import com.google.android.ground.databinding.DropAPinTaskBinding
+import com.google.android.ground.model.job.Style
 import com.google.android.ground.model.task.Task
+import com.google.android.ground.ui.MarkerIconFactory
 import com.google.android.ground.ui.common.AbstractMapContainerFragment
 import com.google.android.ground.ui.common.BaseMapViewModel
 import com.google.android.ground.ui.map.CameraPosition
 import com.google.android.ground.ui.map.MapFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DropAPinTaskFragment(task: Task, private val viewModel: DropAPinTaskViewModel) :
   AbstractMapContainerFragment() {
 
+  @Inject lateinit var markerIconFactory: MarkerIconFactory
+
   private lateinit var mapViewModel: BaseMapViewModel
-  private lateinit var binding: BasemapLayoutBinding
+  private lateinit var binding: DropAPinTaskBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -44,7 +50,7 @@ class DropAPinTaskFragment(task: Task, private val viewModel: DropAPinTaskViewMo
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    binding = BasemapLayoutBinding.inflate(inflater, container, false)
+    binding = DropAPinTaskBinding.inflate(inflater, container, false)
     binding.viewModel = mapViewModel
     binding.lifecycleOwner = this
     return binding.root
@@ -57,5 +63,16 @@ class DropAPinTaskFragment(task: Task, private val viewModel: DropAPinTaskViewMo
   override fun onMapCameraMoved(position: CameraPosition) {
     super.onMapCameraMoved(position)
     viewModel.updateResponse(position)
+    refreshMarker()
+  }
+
+  private fun refreshMarker() {
+    val markerBitmap =
+      markerIconFactory.getMarkerBitmap(
+        parseColor(Style().color),
+        mapFragment.currentZoomLevel,
+        false
+      )
+    binding.markerIcon.setImageBitmap(markerBitmap)
   }
 }
