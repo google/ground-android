31d89ebfa69dfd961bf221d7a4b34c9242eb21f4
Shobhit Agarwal <ashobhit@google.com>
2023-01-22 13:23:21 +0530

    Add unit tests

--- ground/src/test/java/com/google/android/ground/ui/surveyselector/SurveySelectorFragmentTest.kt
+++ ground/src/test/java/com/google/android/ground/ui/surveyselector/SurveySelectorFragmentTest.kt
@@ -15,103 +15,169 @@
  */
 package com.google.android.ground.ui.surveyselector
 
-import android.os.Looper
 import android.view.View
-import android.widget.ListView
-import com.google.android.ground.BaseHiltTest
-import com.google.android.ground.MainActivity
-import com.google.android.ground.R
-import com.google.android.ground.persistence.local.LocalDataStore
-import com.google.android.ground.persistence.local.LocalDataStoreModule
-import com.google.android.ground.persistence.local.room.RoomLocalDataStore
-import com.google.android.ground.persistence.local.stores.LocalLocationOfInterestMutationStore
-import com.google.android.ground.persistence.local.stores.LocalSurveyStore
-import com.google.android.ground.persistence.local.stores.LocalUserStore
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.google.android.ground.*
+import com.google.android.ground.model.Survey
 import com.google.android.ground.repository.SurveyRepository
-import com.google.android.ground.safeEq
+import com.google.android.ground.ui.common.Navigator
+import com.google.android.ground.ui.home.HomeScreenFragmentDirections
 import com.google.common.collect.ImmutableList
-import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth
 import com.sharedtest.FakeData
-import com.sharedtest.persistence.remote.FakeRemoteDataStore
 import com.sharedtest.system.auth.FakeAuthenticationManager
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import io.reactivex.Maybe
+import io.reactivex.*
 import java8.util.Optional
 import javax.inject.Inject
+import org.hamcrest.Matchers.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
-import org.robolectric.Robolectric
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.Shadows.shadowOf
 
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-@UninstallModules(LocalDataStoreModule::class)
 class SurveySelectorFragmentTest : BaseHiltTest() {
+
+  @BindValue @Mock lateinit var navigator: Navigator
+  @BindValue @Mock lateinit var surveyRepository: SurveyRepository
   @Inject lateinit var fakeAuthenticationManager: FakeAuthenticationManager
-  @Inject lateinit var fakeRemoteDataStore: FakeRemoteDataStore
-  @Inject lateinit var surveyRepository: SurveyRepository
 
-  @BindValue @InjectMocks var mockLocalDataStore: LocalDataStore = RoomLocalDataStore()
-
-  @BindValue @Mock lateinit var mockSurveyStore: LocalSurveyStore
-  @BindValue @Mock lateinit var mockUserStore: LocalUserStore
-  @BindValue @Mock lateinit var localLocationOfInterestStore: LocalLocationOfInterestMutationStore
-
-  private lateinit var surveySelectorFragment: SurveySelectorFragment
+  private lateinit var fragment: SurveySelectorFragment
+  private lateinit var activeSurveyFlowableEmitter: FlowableEmitter<Optional<Survey>>
 
   @Before
   override fun setUp() {
     super.setUp()
-    fakeRemoteDataStore.setTestSurveys(ImmutableList.of(TEST_SURVEY_1, TEST_SURVEY_2))
-    fakeAuthenticationManager.setUser(FakeData.USER)
+    fakeAuthenticationManager.setUser(TEST_USER)
+
+    whenever(surveyRepository.activeSurvey)
+      .thenReturn(
+        Flowable.create({ activeSurveyFlowableEmitter = it }, BackpressureStrategy.LATEST)
+      )
+  }
+
+  @Test
+  fun created_surveysAvailable_whenNoSurveySynced() {
+    setAllSurveys(listOf(TEST_SURVEY_1, TEST_SURVEY_2))
+    setOfflineSurveys(ImmutableList.of())
     setUpFragment()
+
+    // Assert that 2 surveys are displayed
+    onView(withId(R.id.recycler_view)).check(matches(allOf(isDisplayed(), hasChildCount(2))))
+
+    var viewHolder = getViewHolder(0)
+    Truth.assertThat(viewHolder.binding.title.text).isEqualTo(TEST_SURVEY_1.title)
+    Truth.assertThat(viewHolder.binding.description.text).isEqualTo(TEST_SURVEY_1.description)
+    Truth.assertThat(viewHolder.binding.offlineIcon.visibility).isEqualTo(View.GONE)
+
+    viewHolder = getViewHolder(1)
+    Truth.assertThat(viewHolder.binding.title.text).isEqualTo(TEST_SURVEY_2.title)
+    Truth.assertThat(viewHolder.binding.description.text).isEqualTo(TEST_SURVEY_2.description)
+    Truth.assertThat(viewHolder.binding.offlineIcon.visibility).isEqualTo(View.GONE)
+  }
+
+  @Test
+  fun created_surveysAvailable_whenOneSurveySynced() {
+    setAllSurveys(listOf(TEST_SURVEY_1, TEST_SURVEY_2))
+    setOfflineSurveys(ImmutableList.of(TEST_SURVEY_2))
+    setUpFragment()
+
+    // Assert that 2 surveys are displayed
+    onView(withId(R.id.recycler_view)).check(matches(allOf(isDisplayed(), hasChildCount(2))))
+
+    var viewHolder = getViewHolder(0)
+    Truth.assertThat(viewHolder.binding.title.text).isEqualTo(TEST_SURVEY_1.title)
+    Truth.assertThat(viewHolder.binding.description.text).isEqualTo(TEST_SURVEY_1.description)
+    Truth.assertThat(viewHolder.binding.offlineIcon.visibility).isEqualTo(View.GONE)
+
+    viewHolder = getViewHolder(1)
+    Truth.assertThat(viewHolder.binding.title.text).isEqualTo(TEST_SURVEY_2.title)
+    Truth.assertThat(viewHolder.binding.description.text).isEqualTo(TEST_SURVEY_2.description)
+    Truth.assertThat(viewHolder.binding.offlineIcon.visibility).isEqualTo(View.VISIBLE)
+  }
+
+  @Test
+  fun click_activatesSurvey() {
+    setAllSurveys(listOf(TEST_SURVEY_1, TEST_SURVEY_2))
+    setOfflineSurveys(ImmutableList.of())
+    setUpFragment()
+
+    // Click second item
+    onView(withId(R.id.recycler_view))
+      .perform(RecyclerViewActions.actionOnItemAtPosition<SurveyListAdapter.ViewHolder>(1, click()))
+
+    // Assert that survey was activated
+    verify(surveyRepository).activateSurvey(eq(TEST_SURVEY_2.id))
+  }
+
+  @Test
+  fun surveyActivated_whenNothingClicked() {
+    setAllSurveys(listOf(TEST_SURVEY_1, TEST_SURVEY_2))
+    setOfflineSurveys(ImmutableList.of(TEST_SURVEY_2))
+    setUpFragment()
+
+    // Activate survey
+    activeSurveyFlowableEmitter.onNext(Optional.of(TEST_SURVEY_2))
+
+    // Assert that no navigation was requested
+    verifyNoInteractions(navigator)
+  }
+
+  @Test
+  fun surveyActivated_whenSurveyClicked() {
+    setAllSurveys(listOf(TEST_SURVEY_1, TEST_SURVEY_2))
+    setOfflineSurveys(ImmutableList.of(TEST_SURVEY_2))
+    setUpFragment()
+
+    // Click second item
+    onView(withId(R.id.recycler_view))
+      .perform(RecyclerViewActions.actionOnItemAtPosition<SurveyListAdapter.ViewHolder>(1, click()))
+
+    // Activate survey
+    activeSurveyFlowableEmitter.onNext(Optional.of(TEST_SURVEY_2))
+
+    // Assert that navigation to home screen was requested
+    verify(navigator).navigate(HomeScreenFragmentDirections.showHomeScreen())
   }
 
   private fun setUpFragment() {
-    val activityController = Robolectric.buildActivity(MainActivity::class.java)
-    val activity = activityController.setup().get()
-
-    surveySelectorFragment = SurveySelectorFragment()
-
-    surveySelectorFragment.showNow(
-      activity.supportFragmentManager,
-      SurveySelectorFragment::class.java.simpleName
-    )
-    shadowOf(Looper.getMainLooper()).idle()
+    launchFragmentInHiltContainer<SurveySelectorFragment> {
+      fragment = this as SurveySelectorFragment
+    }
   }
 
-  @Test
-  fun show_surveyDialogIsShown() {
-    val listView = surveySelectorFragment.dialog!!.currentFocus as ListView
-
-    assertThat(listView.visibility).isEqualTo(View.VISIBLE)
-    assertThat(listView.findViewById<View>(R.id.survey_name)?.visibility).isEqualTo(View.VISIBLE)
+  private fun setAllSurveys(surveys: List<Survey>) {
+    whenever(surveyRepository.getSurveySummaries(FakeData.USER)).thenReturn(Single.just(surveys))
   }
 
-  @Test
-  fun show_surveySelected_surveyIsActivated() {
-    val listView = surveySelectorFragment.dialog!!.currentFocus as ListView
+  private fun setOfflineSurveys(surveys: ImmutableList<Survey>) {
+    whenever(surveyRepository.offlineSurveys).thenReturn(Single.just(surveys))
+  }
 
-    // TODO: Replace mocks with inserting the survey in local db
-    Mockito.`when`(mockLocalDataStore.surveyStore.getSurveyById(safeEq(TEST_SURVEY_2.id)))
-      .thenReturn(Maybe.just(TEST_SURVEY_2))
-    shadowOf(listView).performItemClick(1)
-    shadowOf(Looper.getMainLooper()).idle()
-
-    // Verify Dialog is dismissed
-    assertThat(surveySelectorFragment.dialog).isNull()
-    surveyRepository.activeSurvey.test().assertValue(Optional.of(TEST_SURVEY_2))
+  private fun getViewHolder(index: Int): SurveyListAdapter.ViewHolder {
+    val recyclerView = fragment.view?.findViewById<RecyclerView>(R.id.recycler_view)
+    val viewHolder = recyclerView?.findViewHolderForAdapterPosition(index)
+    return viewHolder as SurveyListAdapter.ViewHolder
   }
 
   companion object {
-    private val TEST_SURVEY_1 = FakeData.SURVEY.copy(id = "some id 1")
-    private val TEST_SURVEY_2 = FakeData.SURVEY.copy(id = "some id 2")
+    private val TEST_USER = FakeData.USER
+    private val TEST_SURVEY_1 =
+      FakeData.SURVEY.copy(id = "1", title = "survey 1", description = "description 1")
+    private val TEST_SURVEY_2 =
+      FakeData.SURVEY.copy(id = "2", title = "survey 2", description = "description 2")
   }
 }
