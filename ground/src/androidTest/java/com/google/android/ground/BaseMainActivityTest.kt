61a9ca657ea4c08a91328a327c380167397928f9
Shobhit Agarwal <ashobhit@google.com>
2023-02-04 01:16:39 +0530

    Convert BaseMainActivityTest.java to kotlin

--- ground/src/androidTest/java/com/google/android/ground/BaseMainActivityTest.kt
+++ ground/src/androidTest/java/com/google/android/ground/BaseMainActivityTest.kt
@@ -13,52 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground
 
-package com.google.android.ground;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import javax.annotation.OverridingMethodsMustInvokeSuper
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import dagger.hilt.android.testing.HiltAndroidRule;
-import javax.annotation.OverridingMethodsMustInvokeSuper;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-
-/**
- * Configures all necessary rules and {@link MainActivity} needed for running instrumentation tests.
- */
-public abstract class BaseMainActivityTest {
-
+/** Configures all necessary rules and [MainActivity] needed for running instrumentation tests. */
+abstract class BaseMainActivityTest {
   // Create an idling resource which can be used to wait for data-bindings to complete.
-  public final DataBindingIdlingResource dataBindingIdlingResource =
-      new DataBindingIdlingResource();
+  val dataBindingIdlingResource = DataBindingIdlingResource()
 
   // Ensures that the Hilt component is initialized before running the ActivityScenarioRule.
-  @Rule(order = 0)
-  public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+  @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
 
   // Swaps the background executor in Architecture Components with one which executes synchronously.
-  @Rule(order = 1)
-  public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+  @get:Rule(order = 1) var instantTaskExecutorRule = InstantTaskExecutorRule()
 
   // Sets the preferences so no login is required and an active survey is selected.
-  @Rule(order = 2)
-  public SetPreferencesRule preferencesRule = new SetPreferencesRule();
+  @get:Rule(order = 2) var preferencesRule = SetPreferencesRule()
 
   // Load the MainActivity for each test.
-  @Rule(order = 3)
-  public ActivityScenarioRule<MainActivity> scenarioRule =
-      new ActivityScenarioRule<>(MainActivity.class);
+  @get:Rule(order = 3) var scenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
   @Before
   @OverridingMethodsMustInvokeSuper
-  public void setUp() {
-    hiltRule.inject();
+  open fun setUp() {
+    hiltRule.inject()
   }
 
   /**
@@ -66,30 +55,30 @@ public abstract class BaseMainActivityTest {
    * not scheduled in the main Looper (for example when executed on a different thread).
    */
   @Before
-  public void registerIdlingResource() {
+  fun registerIdlingResource() {
     // Register the data-binding idling resource. If a test is dependent on a data-binding then it
     // MUST monitor the activity, otherwise it will timeout waiting for the data-binding to
     // complete. See tests below for examples.
-    IdlingRegistry.getInstance().register(dataBindingIdlingResource);
+    IdlingRegistry.getInstance().register(dataBindingIdlingResource)
   }
 
   /**
    * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
    */
   @After
-  public void unregisterIdlingResource() {
-    IdlingRegistry.getInstance().unregister(dataBindingIdlingResource);
+  fun unregisterIdlingResource() {
+    IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
   }
 
-  protected void skipTermsOfServiceFragment() {
+  protected fun skipTermsOfServiceFragment() {
     // Tap on the checkbox
-    onView(withId(R.id.agreeCheckBox)).perform(click());
+    onView(withId(R.id.agreeCheckBox)).perform(click())
     // Tap on Submit on Terms Fragment
-    onView(withId(R.id.agreeButton)).perform(click());
+    onView(withId(R.id.agreeButton)).perform(click())
   }
 
-  protected void skipSurveySelectorFragment() {
+  protected fun skipSurveySelectorFragment() {
     // Tap on the survey list
-    onView(withId(R.id.recycler_view)).perform(click());
+    onView(withId(R.id.recycler_view)).perform(click())
   }
 }
