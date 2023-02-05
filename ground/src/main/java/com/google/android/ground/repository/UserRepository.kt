9aa68ca1788ae200a506a9d5bad3b47adf7d077d
Gino Miceli <gmiceli@google.com>
2023-02-04 20:05:48 -0500

    chore: ktdoc formatting nit

--- ground/src/main/java/com/google/android/ground/repository/UserRepository.kt
+++ ground/src/main/java/com/google/android/ground/repository/UserRepository.kt
@@ -24,11 +24,10 @@ import com.google.android.ground.system.auth.AuthenticationManager
 import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
-import javax.inject.Singleton
 
 /**
  * Coordinates persistence of [User] instance in local data store. For more details on this pattern
- * and overall architecture, see * https://developer.android.com/jetpack/docs/guide.
+ * and overall architecture, see https://developer.android.com/jetpack/docs/guide.
  */
 @Singleton
 class UserRepository
