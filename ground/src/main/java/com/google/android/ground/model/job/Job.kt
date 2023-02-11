6afeedd9c70a7db6c74c4939a7eb253846717f7c
Shobhit Agarwal <ashobhit@google.com>
2023-02-11 21:07:58 +0530

    Sort tasks by "index" and not "id"

--- ground/src/main/java/com/google/android/ground/model/job/Job.kt
+++ ground/src/main/java/com/google/android/ground/model/job/Job.kt
@@ -20,7 +20,7 @@ import java8.util.Optional
 
 data class Job(val id: String, val name: String? = null, val tasks: Map<String, Task> = mapOf()) {
   val tasksSorted: List<Task>
-    get() = tasks.values.sortedBy { it.id }
+    get() = tasks.values.sortedBy { it.index }
 
   fun getTask(id: String): Optional<Task> = Optional.ofNullable(tasks[id])
 
