6ce9359c0f703106c92cb51496f440c608a3fae4
Shobhit Agarwal <ashobhit@google.com>
2023-02-19 10:23:31 +0530

    Convert SubmissionDetailsViewModel

--- ground/src/main/java/com/google/android/ground/ui/submissiondetails/SubmissionDetailsViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/submissiondetails/SubmissionDetailsViewModel.kt
@@ -13,104 +13,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.android.ground.ui.submissiondetails
 
-package com.google.android.ground.ui.submissiondetails;
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import com.google.android.ground.model.locationofinterest.LocationOfInterest
+import com.google.android.ground.model.submission.Submission
+import com.google.android.ground.repository.SubmissionRepository
+import com.google.android.ground.rx.Loadable
+import com.google.android.ground.rx.annotations.Hot
+import com.google.android.ground.ui.common.AbstractViewModel
+import com.google.android.ground.ui.common.LocationOfInterestHelper
+import io.reactivex.Completable
+import io.reactivex.processors.BehaviorProcessor
+import io.reactivex.processors.FlowableProcessor
+import java8.util.Optional
+import javax.inject.Inject
 
-import android.view.View;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.LiveDataReactiveStreams;
-import com.google.android.ground.model.locationofinterest.LocationOfInterest;
-import com.google.android.ground.model.submission.Submission;
-import com.google.android.ground.repository.SubmissionRepository;
-import com.google.android.ground.rx.Loadable;
-import com.google.android.ground.rx.annotations.Hot;
-import com.google.android.ground.ui.common.AbstractViewModel;
-import com.google.android.ground.ui.common.LocationOfInterestHelper;
-import io.reactivex.Completable;
-import io.reactivex.Flowable;
-import io.reactivex.processors.BehaviorProcessor;
-import io.reactivex.processors.FlowableProcessor;
-import java8.util.Optional;
-import javax.inject.Inject;
+class SubmissionDetailsViewModel
+@Inject
+internal constructor(
+  private val submissionRepository: SubmissionRepository,
+  locationOfInterestHelper: LocationOfInterestHelper
+) : AbstractViewModel() {
 
-public class SubmissionDetailsViewModel extends AbstractViewModel {
+  val progressBarVisibility: @Hot(replays = true) LiveData<Int>
+  val submission: @Hot(replays = true) LiveData<Loadable<Submission>>
+  val subtitle: @Hot(replays = true) LiveData<String>
+  val title: @Hot(replays = true) LiveData<String>
 
-  @Hot(replays = true)
-  public final LiveData<Loadable<Submission>> submission;
+  private val argsProcessor: @Hot(replays = true) FlowableProcessor<SubmissionDetailsFragmentArgs> =
+    BehaviorProcessor.create()
 
-  @Hot(replays = true)
-  public final LiveData<Integer> progressBarVisibility;
-
-  @Hot(replays = true)
-  public final LiveData<String> title;
-
-  @Hot(replays = true)
-  public final LiveData<String> subtitle;
-
-  private final SubmissionRepository submissionRepository;
-
-  @Hot(replays = true)
-  private final FlowableProcessor<SubmissionDetailsFragmentArgs> argsProcessor =
-      BehaviorProcessor.create();
-
-  @Inject
-  SubmissionDetailsViewModel(
-      SubmissionRepository submissionRepository,
-      LocationOfInterestHelper locationOfInterestHelper) {
-    this.submissionRepository = submissionRepository;
-
-    Flowable<Loadable<Submission>> submissionStream =
-        argsProcessor.switchMapSingle(
-            args ->
-                submissionRepository
-                    .getSubmission(
-                        args.getSurveyId(), args.getLocationOfInterestId(), args.getSubmissionId())
-                    .map(Loadable::loaded)
-                    .onErrorReturn(Loadable::error));
+  init {
+    val submissionStream =
+      argsProcessor.switchMapSingle { args: SubmissionDetailsFragmentArgs ->
+        submissionRepository
+          .getSubmission(args.surveyId, args.locationOfInterestId, args.submissionId)
+          .map { Loadable.loaded(it) }
+          .onErrorReturn { Loadable.error(it) }
+      }
 
     // TODO: Refactor to expose the fetched submission directly.
-    this.submission = LiveDataReactiveStreams.fromPublisher(submissionStream);
-
-    this.progressBarVisibility =
-        LiveDataReactiveStreams.fromPublisher(
-            submissionStream.map(SubmissionDetailsViewModel::getProgressBarVisibility));
-
-    this.title =
-        LiveDataReactiveStreams.fromPublisher(
-            submissionStream
-                .map(SubmissionDetailsViewModel::getLocationOfInterest)
-                .map(locationOfInterestHelper::getLabel));
-
-    this.subtitle =
-        LiveDataReactiveStreams.fromPublisher(
-            submissionStream
-                .map(SubmissionDetailsViewModel::getLocationOfInterest)
-                .map(locationOfInterestHelper::getCreatedBy));
+    submission = LiveDataReactiveStreams.fromPublisher(submissionStream)
+    progressBarVisibility =
+      LiveDataReactiveStreams.fromPublisher(
+        submissionStream.map { submission: Loadable<Submission> ->
+          getProgressBarVisibility(submission)
+        }
+      )
+    title =
+      LiveDataReactiveStreams.fromPublisher(
+        submissionStream
+          .map { submission: Loadable<Submission> -> getLocationOfInterest(submission) }
+          .map { locationOfInterest: Optional<LocationOfInterest> ->
+            locationOfInterestHelper.getLabel(locationOfInterest)
+          }
+      )
+    subtitle =
+      LiveDataReactiveStreams.fromPublisher(
+        submissionStream
+          .map { submission: Loadable<Submission> -> getLocationOfInterest(submission) }
+          .map { locationOfInterest: Optional<LocationOfInterest> ->
+            locationOfInterestHelper.getCreatedBy(locationOfInterest)
+          }
+      )
   }
 
-  private static Integer getProgressBarVisibility(Loadable<Submission> submission) {
-    return submission.isLoaded() ? View.GONE : View.VISIBLE;
-  }
-
-  private static Optional<LocationOfInterest> getLocationOfInterest(
-      Loadable<Submission> submission) {
-    return submission.value().map(Submission::getLocationOfInterest);
-  }
-
-  public void loadSubmissionDetails(SubmissionDetailsFragmentArgs args) {
-    this.argsProcessor.onNext(args);
+  fun loadSubmissionDetails(args: SubmissionDetailsFragmentArgs) {
+    argsProcessor.onNext(args)
   }
 
   /**
-   * Creates an {@link com.google.android.ground.model.mutation.SubmissionMutation}, marks the
-   * locally stored {@link Submission} as DELETED and enqueues a worker to remove the submission
-   * from remote {@link com.google.android.ground.persistence.remote.firebase.FirestoreDataStore}.
+   * Creates an [com.google.android.ground.model.mutation.SubmissionMutation], marks the locally
+   * stored [Submission] as DELETED and enqueues a worker to remove the submission from remote
+   * [com.google.android.ground.persistence.remote.firebase.FirestoreDataStore].
    */
-  @Hot
-  public Completable deleteCurrentSubmission(
-      String surveyId, String locationOfInterestId, String submissionId) {
+  fun deleteCurrentSubmission(
+    surveyId: String,
+    locationOfInterestId: String,
+    submissionId: String
+  ): @Hot Completable {
     return submissionRepository
-        .getSubmission(surveyId, locationOfInterestId, submissionId)
-        .flatMapCompletable(submissionRepository::deleteSubmission);
+      .getSubmission(surveyId, locationOfInterestId, submissionId)
+      .flatMapCompletable { submissionRepository.deleteSubmission(it) }
+  }
+
+  companion object {
+    private fun getProgressBarVisibility(submission: Loadable<Submission>): Int {
+      return if (submission.isLoaded) View.GONE else View.VISIBLE
+    }
+
+    private fun getLocationOfInterest(
+      submission: Loadable<Submission>
+    ): Optional<LocationOfInterest> {
+      return submission.value().map(Submission::locationOfInterest)
+    }
   }
 }
