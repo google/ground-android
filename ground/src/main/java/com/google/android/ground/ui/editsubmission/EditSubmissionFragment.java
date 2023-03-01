caa7747fbb2f9956ea5773b68029ac81b63c5c8d
Jakob Sunde <jsunde@google.com>
2023-03-01 04:12:53 +0000

    Remove old logic from EditSubmissionFragment

--- ground/src/main/java/com/google/android/ground/ui/editsubmission/EditSubmissionFragment.java
+++ ground/src/main/java/com/google/android/ground/ui/editsubmission/EditSubmissionFragment.java
@@ -17,7 +17,6 @@
 package com.google.android.ground.ui.editsubmission;
 
 import static com.google.android.ground.rx.RxAutoDispose.autoDisposable;
-import static java.util.Objects.requireNonNull;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
@@ -32,23 +31,13 @@ import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.ViewDataBinding;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.ground.MainActivity;
 import com.google.android.ground.R;
 import com.google.android.ground.databinding.DateInputTaskBinding;
-import com.google.android.ground.databinding.EditSubmissionBottomSheetBinding;
 import com.google.android.ground.databinding.EditSubmissionFragBinding;
-import com.google.android.ground.databinding.MultipleChoiceInputTaskBinding;
 import com.google.android.ground.databinding.NumberInputTaskBinding;
-import com.google.android.ground.databinding.PhotoInputTaskBinding;
-import com.google.android.ground.databinding.PhotoInputTaskBindingImpl;
-import com.google.android.ground.databinding.TextInputTaskBinding;
 import com.google.android.ground.databinding.TimeInputTaskBinding;
 import com.google.android.ground.model.job.Job;
-import com.google.android.ground.model.submission.MultipleChoiceTaskData;
-import com.google.android.ground.model.task.MultipleChoice;
-import com.google.android.ground.model.task.Option;
 import com.google.android.ground.model.task.Task;
 import com.google.android.ground.repository.UserMediaRepository;
 import com.google.android.ground.rx.Schedulers;
@@ -57,17 +46,13 @@ import com.google.android.ground.ui.common.BackPressListener;
 import com.google.android.ground.ui.common.EphemeralPopups;
 import com.google.android.ground.ui.common.Navigator;
 import com.google.android.ground.ui.common.TwoLineToolbar;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 import dagger.hilt.android.AndroidEntryPoint;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java8.util.Optional;
-import java8.util.function.Consumer;
 import javax.inject.Inject;
-import timber.log.Timber;
 
 @AndroidEntryPoint
 public class EditSubmissionFragment extends AbstractFragment implements BackPressListener {
@@ -77,12 +62,6 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
 
     /** Key used to store unsaved responses across activity re-creation. */
     private static final String RESTORED_RESPONSES = "restoredResponses";
-
-    /** Key used to store field ID waiting for photo taskData across activity re-creation. */
-    private static final String TASK_WAITING_FOR_PHOTO = "photoFieldId";
-
-    /** Key used to store captured photo Uri across activity re-creation. */
-    private static final String CAPTURED_PHOTO_PATH = "capturedPhotoPath";
   }
 
   private final List<AbstractTaskViewModel> taskViewModels = new ArrayList<>();
@@ -97,14 +76,8 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
   private EditSubmissionFragBinding binding;
 
   private static AbstractTaskViewModel getViewModel(ViewDataBinding binding) {
-    if (binding instanceof TextInputTaskBinding) {
-      return ((TextInputTaskBinding) binding).getViewModel();
-    } else if (binding instanceof MultipleChoiceInputTaskBinding) {
-      return ((MultipleChoiceInputTaskBinding) binding).getViewModel();
-    } else if (binding instanceof NumberInputTaskBinding) {
+    if (binding instanceof NumberInputTaskBinding) {
       return ((NumberInputTaskBinding) binding).getViewModel();
-    } else if (binding instanceof PhotoInputTaskBinding) {
-      return ((PhotoInputTaskBinding) binding).getViewModel();
     } else if (binding instanceof DateInputTaskBinding) {
       return ((DateInputTaskBinding) binding).getViewModel();
     } else if (binding instanceof TimeInputTaskBinding) {
@@ -139,11 +112,6 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
     toolbar.setNavigationOnClickListener(__ -> onCloseButtonClick());
     // Observe state changes.
     viewModel.getJob().observe(getViewLifecycleOwner(), this::rebuildForm);
-    viewModel
-        .getSaveResults()
-        .observeOn(schedulers.ui())
-        .as(autoDisposable(getViewLifecycleOwner()))
-        .subscribe(this::handleSaveResult);
 
     // Initialize view model.
     Bundle args = getArguments();
@@ -151,9 +119,6 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
       args.putSerializable(
           BundleKeys.RESTORED_RESPONSES,
           savedInstanceState.getSerializable(BundleKeys.RESTORED_RESPONSES));
-      viewModel.setTaskWaitingForPhoto(
-          savedInstanceState.getString(BundleKeys.TASK_WAITING_FOR_PHOTO));
-      viewModel.setCapturedPhotoPath(savedInstanceState.getString(BundleKeys.CAPTURED_PHOTO_PATH));
     }
     viewModel.initialize(EditSubmissionFragmentArgs.fromBundle(args));
   }
@@ -162,42 +127,13 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
   public void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     outState.putSerializable(BundleKeys.RESTORED_RESPONSES, viewModel.getDraftResponses());
-    outState.putString(BundleKeys.TASK_WAITING_FOR_PHOTO, viewModel.getTaskWaitingForPhoto());
-    outState.putString(BundleKeys.CAPTURED_PHOTO_PATH, viewModel.getCapturedPhotoPath());
-  }
-
-  private void handleSaveResult(EditSubmissionViewModel.SaveResult saveResult) {
-    switch (saveResult) {
-      case HAS_VALIDATION_ERRORS:
-        showValidationErrorsAlert();
-        break;
-      case NO_CHANGES_TO_SAVE:
-        popups.showFyi(R.string.no_changes_to_save);
-        navigator.navigateUp();
-        break;
-      case SAVED:
-        popups.showSuccess(R.string.saved);
-        navigator.navigateUp();
-        break;
-      default:
-        Timber.e("Unknown save result type: %s", saveResult);
-        break;
-    }
   }
 
   private void addTaskViewModel(Task task, ViewDataBinding binding) {
-    if (binding instanceof PhotoInputTaskBindingImpl) {
-      ((PhotoInputTaskBindingImpl) binding).setEditSubmissionViewModel(viewModel);
-    }
-
     AbstractTaskViewModel taskViewModel = getViewModel(binding);
     taskViewModel.initialize(task, viewModel.getResponse(task.getId()));
 
-    if (taskViewModel instanceof PhotoTaskViewModel) {
-      initPhotoTask((PhotoTaskViewModel) taskViewModel);
-    } else if (taskViewModel instanceof MultipleChoiceTaskViewModel) {
-      observeSelectChoiceClicks((MultipleChoiceTaskViewModel) taskViewModel);
-    } else if (taskViewModel instanceof DateTaskViewModel) {
+    if (taskViewModel instanceof DateTaskViewModel) {
       observeDateDialogClicks((DateTaskViewModel) taskViewModel);
     } else if (taskViewModel instanceof TimeTaskViewModel) {
       observeTimeDialogClicks((TimeTaskViewModel) taskViewModel);
@@ -254,76 +190,6 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
       ViewDataBinding binding = taskViewFactory.addTaskView(task.getType(), formLayout);
       addTaskViewModel(task, binding);
     }
-  }
-
-  private void observeSelectChoiceClicks(MultipleChoiceTaskViewModel viewModel) {
-    viewModel
-        .getShowDialogClicks()
-        .as(autoDisposable(this))
-        .subscribe(
-            __ ->
-                createMultipleChoiceDialog(
-                        viewModel.getTask(),
-                        viewModel.getCurrentResponse(),
-                        viewModel::updateResponse)
-                    .show());
-  }
-
-  private AlertDialog createMultipleChoiceDialog(
-      Task task, Optional<MultipleChoiceTaskData> response, Consumer<List<Option>> consumer) {
-    MultipleChoice multipleChoice = requireNonNull(task.getMultipleChoice());
-    switch (multipleChoice.getCardinality()) {
-      case SELECT_MULTIPLE:
-        return new MultiSelectDialogFactory(
-                requireContext(), task.getLabel(), multipleChoice, response, consumer)
-            .createDialog();
-      case SELECT_ONE:
-        return new SingleSelectDialogFactory(
-                requireContext(), task.getLabel(), multipleChoice, response, consumer)
-            .createDialog();
-      default:
-        throw new IllegalArgumentException(
-            "Unknown cardinality: " + multipleChoice.getCardinality());
-    }
-  }
-
-  private void initPhotoTask(PhotoTaskViewModel photoTaskViewModel) {
-    photoTaskViewModel.setEditable(true);
-    photoTaskViewModel.setSurveyId(viewModel.getSurveyId());
-    photoTaskViewModel.setSubmissionId(viewModel.getSubmissionId());
-    observeSelectPhotoClicks(photoTaskViewModel);
-    observePhotoResults(photoTaskViewModel);
-  }
-
-  private void observeSelectPhotoClicks(PhotoTaskViewModel fieldViewModel) {
-    fieldViewModel
-        .getTakePhotoClicks()
-        .as(autoDisposable(getViewLifecycleOwner()))
-        .subscribe(__ -> onShowPhotoSelectorDialog(fieldViewModel.getTask(), fieldViewModel));
-  }
-
-  private void observePhotoResults(PhotoTaskViewModel fieldViewModel) {
-    viewModel
-        .getLastPhotoResult()
-        .as(autoDisposable(getViewLifecycleOwner()))
-        .subscribe(fieldViewModel::onPhotoResult);
-  }
-
-  private void onShowPhotoSelectorDialog(Task task, PhotoTaskViewModel fieldViewModel) {
-    EditSubmissionBottomSheetBinding addPhotoBottomSheetBinding =
-        EditSubmissionBottomSheetBinding.inflate(getLayoutInflater());
-    addPhotoBottomSheetBinding.setViewModel(viewModel);
-    addPhotoBottomSheetBinding.setTask(task);
-
-    BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(requireContext());
-    bottomSheetDialog.setContentView(addPhotoBottomSheetBinding.getRoot());
-    bottomSheetDialog.setCancelable(true);
-    bottomSheetDialog.show();
-
-    RecyclerView recyclerView = addPhotoBottomSheetBinding.recyclerView;
-    recyclerView.setHasFixedSize(true);
-    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-    recyclerView.setAdapter(new AddPhotoDialogAdapter(type -> bottomSheetDialog.dismiss()));
   }
 
   private void showDateDialog(DateTaskViewModel fieldViewModel) {
@@ -388,14 +254,6 @@ public class EditSubmissionFragment extends AbstractFragment implements BackPres
         .setMessage(R.string.unsaved_changes)
         .setPositiveButton(R.string.discard_changes, (d, i) -> navigator.navigateUp())
         .setNegativeButton(R.string.continue_editing, (d, i) -> {})
-        .create()
-        .show();
-  }
-
-  private void showValidationErrorsAlert() {
-    new AlertDialog.Builder(requireContext())
-        .setMessage(R.string.invalid_data_warning)
-        .setPositiveButton(R.string.invalid_data_confirm, (a, b) -> {})
         .create()
         .show();
   }
