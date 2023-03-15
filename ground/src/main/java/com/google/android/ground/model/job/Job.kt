60d3af639a26aa1bad96794a8c5e116d878d479e
Jakob Sunde <jsunde@google.com>
2023-03-15 14:24:57 +0000

    Format Job

--- ground/src/main/java/com/google/android/ground/model/job/Job.kt
+++ ground/src/main/java/com/google/android/ground/model/job/Job.kt
@@ -20,18 +20,16 @@ import java8.util.Optional
 
 /**
  * @param suggestLoiTaskType the type of task used to suggest the LOI for this Job. Null if the job
- *   is already associated with an LOI.
+ * is already associated with an LOI.
  */
 data class Job(
   val id: String,
   val name: String? = null,
   val tasks: Map<String, Task> = mapOf(),
-  val suggestLoiTaskType: Task.Type? = null
+  val suggestLoiTaskType: Task.Type? = null,
 ) {
   val tasksSorted: List<Task>
     get() = tasks.values.sortedBy { it.index }
-
   fun getTask(id: String): Optional<Task> = Optional.ofNullable(tasks[id])
-
   fun hasData(): Boolean = tasks.isNotEmpty()
 }
