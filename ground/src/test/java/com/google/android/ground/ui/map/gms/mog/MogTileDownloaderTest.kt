ca07cb9abebdda32c65367eecf888f13271c27c9
Gino Miceli <gmiceli@google.com>
2023-05-21 16:57:25 -0400

    Add back downloader

--- ground/src/test/java/com/google/android/ground/ui/map/gms/mog/MogTileDownloaderTest.kt
+++ ground/src/test/java/com/google/android/ground/ui/map/gms/mog/MogTileDownloaderTest.kt
@@ -16,6 +16,9 @@
 
 package com.google.android.ground.ui.map.gms.mog
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -23,7 +26,18 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class MogTileDownloaderTest {
   @Test
-  fun downloadTiles() {
-    // TODO
+  fun downloadTiles() = runBlocking {
+    // Indonesia: LngLatBbox(west=95.00933042066814, south=-11.007616494507769,
+    // east=141.01944540877238, north=5.904411619513165)
+    val southwest = LatLng(-11.007616494507769, 95.00933042066814)
+    val northeast = LatLng(5.904411619513165, 141.01944540877238)
+    //    val southwest = LatLng(4.089672, 95.546853)
+    //    val northeast = LatLng(5.435577, 96.278013)
+    val mogBaseUrl = "https://storage.googleapis.com/ground-raster-basemaps/2022/cog/8"
+    val mogCollection = MogCollection("$mogBaseUrl/world.tif", "$mogBaseUrl/{x}/{y}.tif", 8, 14)
+    val downloader = MogTileDownloader(mogCollection, "/tmp/tiles")
+    var startTimeMillis = System.currentTimeMillis()
+    println("Fetching headers...")
+    downloader.downloadTiles(LatLngBounds(southwest, northeast), 12..14)
   }
 }
