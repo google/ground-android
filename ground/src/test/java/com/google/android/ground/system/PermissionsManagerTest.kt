3338f59ed95195d3323caf82c274e4a9ce525079
Shobhit Agarwal <ashobhit@google.com>
2023-07-15 01:51:38 +0530

    Update unit tests

--- ground/src/test/java/com/google/android/ground/system/PermissionsManagerTest.kt
+++ ground/src/test/java/com/google/android/ground/system/PermissionsManagerTest.kt
@@ -16,14 +16,21 @@
 package com.google.android.ground.system
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.google.android.ground.BaseHiltTest
+import com.google.android.ground.system.PermissionsManager.Companion.PERMISSIONS_REQUEST_CODE
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.reactivex.Observable
 import javax.inject.Inject
+import kotlin.test.assertFailsWith
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.whenever
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadow.api.Shadow.extract
 import org.robolectric.shadows.ShadowApplication
@@ -35,6 +42,7 @@ class PermissionsManagerTest : BaseHiltTest() {
 
   private val testPermission = Manifest.permission.ACCESS_COARSE_LOCATION
 
+  @BindValue @Mock lateinit var activityStreamsMock: ActivityStreams
   @Inject lateinit var permissionsManager: PermissionsManager
 
   @Test
@@ -47,6 +55,32 @@ class PermissionsManagerTest : BaseHiltTest() {
 
   @Test
   fun permissionNotAvailable_granted() = runWithTestDispatcher {
+    setupPermissionResult(true)
+
     permissionsManager.obtainPermission(testPermission)
+  }
+
+  @Test
+  fun permissionNotAvailable_notGranted_fails() = runWithTestDispatcher {
+    setupPermissionResult(false)
+
+    assertFailsWith<PermissionDeniedException> {
+      permissionsManager.obtainPermission(testPermission)
+    }
+  }
+
+  private fun setupPermissionResult(granted: Boolean) {
+    whenever(activityStreamsMock.getNextRequestPermissionsResult(PERMISSIONS_REQUEST_CODE))
+      .thenReturn(
+        Observable.just(
+          RequestPermissionsResult(
+            PERMISSIONS_REQUEST_CODE,
+            arrayOf(testPermission),
+            intArrayOf(
+              if (granted) PackageManager.PERMISSION_GRANTED else PackageManager.PERMISSION_DENIED
+            )
+          )
+        )
+      )
   }
 }
