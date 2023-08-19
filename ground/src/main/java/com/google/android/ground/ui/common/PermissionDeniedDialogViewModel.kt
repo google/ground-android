6b6d72e3780fbb6bb8ea81ac5812a4e50bb02202
Shobhit Agarwal <ashobhit@google.com>
2023-08-20 01:10:05 +0530

    Signout using user repository to clear authentication details as well

--- ground/src/main/java/com/google/android/ground/ui/common/PermissionDeniedDialogViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/common/PermissionDeniedDialogViewModel.kt
@@ -1,18 +1,20 @@
 package com.google.android.ground.ui.common
 
+import com.google.android.ground.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PermissionDeniedDialogViewModel
 @Inject
-internal constructor(private val navigator: Navigator) : AbstractViewModel() {
+internal constructor(private val navigator: Navigator, private val userRepository: UserRepository) :
+  AbstractViewModel() {
 
   fun closeApp() {
     navigator.finishApp()
   }
 
   fun signOut() {
-    navigator.navigate(PermissionDeniedDialogFragmentDirections.showSignInScreen())
+    userRepository.signOut()
   }
 }
