9f0cf074786c743ff26a4ac96ea7179497f9225a
Shobhit Agarwal <ashobhit@google.com>
2023-08-20 01:01:56 +0530

    Bind the dialog to action buttons

--- ground/src/main/java/com/google/android/ground/ui/common/PermissionDeniedDialogViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/common/PermissionDeniedDialogViewModel.kt
@@ -4,4 +4,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PermissionDeniedDialogViewModel @Inject internal constructor() : AbstractViewModel() {}
+class PermissionDeniedDialogViewModel
+@Inject
+internal constructor(private val navigator: Navigator) : AbstractViewModel() {
+
+  fun closeApp() {
+    navigator.finishApp()
+  }
+
+  fun signOut() {
+    navigator.navigate(PermissionDeniedDialogFragmentDirections.showSignInScreen())
+  }
+}
