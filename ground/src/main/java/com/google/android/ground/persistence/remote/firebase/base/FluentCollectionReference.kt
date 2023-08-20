041ade5af89865ca5cd634940bf2525b0d7d52b0
Shobhit Agarwal <ashobhit@google.com>
2023-08-20 12:26:00 +0530

    Update ktdoc and directly use the util method for network check

--- ground/src/main/java/com/google/android/ground/persistence/remote/firebase/base/FluentCollectionReference.kt
+++ ground/src/main/java/com/google/android/ground/persistence/remote/firebase/base/FluentCollectionReference.kt
@@ -32,12 +32,7 @@ protected constructor(
   protected val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
-  /**
-   * Returns a Completable that completes immediately on subscribe if network is available, or fails
-   * in error if not.
-   */
-  private fun requireActiveNetwork() =
-    requireActiveNetwork(reference.firestore.app.applicationContext)
+  private val context = reference.firestore.app.applicationContext
 
   /**
    * Runs the specified query, returning a Single containing a List of values created by applying
@@ -48,7 +43,7 @@ protected constructor(
     query: Query,
     mappingFunction: Function<DocumentSnapshot, T>
   ): List<T> {
-    requireActiveNetwork()
+    requireActiveNetwork(context)
     val querySnapshot = query.get().await()
     return querySnapshot.documents.filter { it.exists() }.map { mappingFunction.apply(it) }
   }
