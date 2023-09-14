96d294f42b762ad9fe49164eb20db4ee4b38df38
Gino Miceli <228050+gino-m@users.noreply.github.com>
2023-09-14 17:04:15 +0300

    Resolve some StrictMode policy violations (#1884)
    * Stop blocking when getting local file path
    
    * Upgrade Firebase libs to try to remove StrictMode violation
    
    * Get Firebase auth off main thread
    
    * Instantiate Firebase async
    
    * ktdoc and naming
    
    * ktfmt
    
    * detekt fix
    
    * Remove stale TODOs

--- ground/src/main/java/com/google/android/ground/ui/util/FileUtil.kt
+++ ground/src/main/java/com/google/android/ground/ui/util/FileUtil.kt
@@ -16,12 +16,21 @@
 package com.google.android.ground.ui.util
 
 import android.content.Context
+import com.google.android.ground.coroutines.IoDispatcher
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.io.File
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
-class FileUtil @Inject constructor(@ApplicationContext private val context: Context) {
-  val filesDir = context.filesDir
+class FileUtil
+@Inject
+constructor(
+  @ApplicationContext private val context: Context,
+  @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+
+  suspend fun getFilesDir(): File = withContext(ioDispatcher) { context.filesDir }
 
   /**
    * Get a file by name relative to the app's file directory
