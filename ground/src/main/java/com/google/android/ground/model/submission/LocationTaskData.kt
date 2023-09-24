9db6d8803b020963c865770bcd00716d11420d2a
Shobhit Agarwal <ashobhit@google.com>
2023-09-24 20:12:29 +0530

    Create a separate class for location task data

---
+++ ground/src/main/java/com/google/android/ground/model/submission/LocationTaskData.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.ground.model.submission
+
+import com.google.android.ground.model.geometry.Geometry
+import com.google.android.ground.model.geometry.Point
+import com.google.android.ground.ui.datacollection.tasks.point.LatLngConverter
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+data class LocationTaskData(
+  val geometry: Geometry,
+  val altitude: Double?, // in metres
+  val accuracy: Float? // in metres
+) : TaskData {
+  override fun getDetailsText(): String {
+    if (geometry !is Point) {
+      return "Invalid geometry type: Expected POINT, Found ${geometry.javaClass.name}"
+    }
+
+    val df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.DOWN
+    return "${LatLngConverter.processCoordinates(geometry.coordinates)}\n\n" +
+      "Altitude: ${df.format(altitude)}m\n" +
+      "Accuracy: ${df.format(accuracy)}m"
+  }
+
+  override fun isEmpty(): Boolean {
+    return false
+  }
+}
--- ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/location/CaptureLocationTaskViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/location/CaptureLocationTaskViewModel.kt
@@ -21,14 +21,13 @@ import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.google.android.ground.model.geometry.Coordinates
+import com.google.android.ground.model.geometry.Point
+import com.google.android.ground.model.submission.LocationTaskData
 import com.google.android.ground.system.FINE_LOCATION_UPDATES_REQUEST
 import com.google.android.ground.system.LocationManager
 import com.google.android.ground.system.PermissionsManager
 import com.google.android.ground.system.SettingsManager
 import com.google.android.ground.ui.datacollection.tasks.AbstractTaskViewModel
-import com.google.android.ground.ui.datacollection.tasks.point.LatLngConverter.processCoordinates
-import java.math.RoundingMode
-import java.text.DecimalFormat
 import javax.inject.Inject
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
@@ -44,7 +43,7 @@ constructor(
 
   val locationUpdates: LiveData<String> =
     locationManager.locationUpdates
-      .map { it.toTaskData().displayText() }
+      .map { it.toTaskData().getDetailsText() }
       .distinctUntilChanged()
       .asLiveData()
 
@@ -58,25 +57,11 @@ constructor(
     locationManager.disableLocationUpdates()
   }
 
-  data class TaskData(
-    val coordinates: Coordinates,
-    val altitude: Double?, // in metres
-    val accuracy: Float? // in metres
-  )
-
   companion object {
-    private fun Location.toTaskData(): TaskData {
+    private fun Location.toTaskData(): LocationTaskData {
       val altitude = if (hasAltitude()) altitude else null
       val accuracy = if (hasAccuracy()) accuracy else null
-      return TaskData(Coordinates(latitude, longitude), altitude, accuracy)
-    }
-
-    private fun TaskData.displayText(): String {
-      val df = DecimalFormat("#.##")
-      df.roundingMode = RoundingMode.DOWN
-      return "${processCoordinates(coordinates)}\n\n" +
-        "Altitude: ${df.format(altitude)}m\n" +
-        "Accuracy: ${df.format(accuracy)}m"
+      return LocationTaskData(Point(Coordinates(latitude, longitude)), altitude, accuracy)
     }
   }
 }
