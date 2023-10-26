1bcd90069f946e33c59de2a789f60d64b6b4e889
Shobhit Agarwal <ashobhit@google.com>
2023-10-26 08:13:00 +0530

    Show undo button at the beginning for photo task fragment

--- ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/photo/PhotoTaskFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/photo/PhotoTaskFragment.kt
@@ -89,8 +89,9 @@ class PhotoTaskFragment : Hilt_PhotoTaskFragment<PhotoTaskViewModel>() {
   }
 
   override fun onCreateActionButtons() {
-    super.onCreateActionButtons()
     addUndoButton()
+    addSkipButton()
+    addContinueButton()
   }
 
   override fun onResume() {
