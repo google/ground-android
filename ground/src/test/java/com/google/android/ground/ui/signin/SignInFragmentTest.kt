6855117f7789be9cafc5cfb00ec716afdb59fc47
Shobhit Agarwal <ashobhit@google.com>
2023-11-13 10:59:35 +0530

    Check snackbar visibility

--- ground/src/test/java/com/google/android/ground/ui/signin/SignInFragmentTest.kt
+++ ground/src/test/java/com/google/android/ground/ui/signin/SignInFragmentTest.kt
@@ -17,9 +17,11 @@ package com.google.android.ground.ui.signin
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.google.android.ground.BaseHiltTest
 import com.google.android.ground.R
 import com.google.android.ground.launchFragmentInHiltContainer
@@ -78,6 +80,9 @@ class SignInFragmentTest : BaseHiltTest() {
     fakeAuthenticationManager.signInState
       .test()
       .assertValue(SignInState(SignInState.State.SIGNED_OUT, Result.success(null)))
+
+    onView(withId(com.google.android.material.R.id.snackbar_text))
+      .check(matches(withText(R.string.network_error_when_signing_in)))
   }
 
   @Test
