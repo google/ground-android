3cc36a0fe8369464b33783ec2ae35d2f746aae13
Shobhit Agarwal <ashobhit@google.com>
2024-02-24 23:43:09 +0530

    Update ktfmt and detekt plugins

--- ground/src/main/java/com/google/android/ground/persistence/local/room/LocalDatabase.kt
+++ ground/src/main/java/com/google/android/ground/persistence/local/room/LocalDatabase.kt
@@ -70,15 +70,26 @@ import com.google.android.ground.persistence.local.room.fields.*
 )
 abstract class LocalDatabase : RoomDatabase() {
   abstract fun locationOfInterestDao(): LocationOfInterestDao
+
   abstract fun locationOfInterestMutationDao(): LocationOfInterestMutationDao
+
   abstract fun taskDao(): TaskDao
+
   abstract fun jobDao(): JobDao
+
   abstract fun multipleChoiceDao(): MultipleChoiceDao
+
   abstract fun optionDao(): OptionDao
+
   abstract fun surveyDao(): SurveyDao
+
   abstract fun tileSourceDao(): TileSourceDao
+
   abstract fun submissionDao(): SubmissionDao
+
   abstract fun submissionMutationDao(): SubmissionMutationDao
+
   abstract fun offlineAreaDao(): OfflineAreaDao
+
   abstract fun userDao(): UserDao
 }
