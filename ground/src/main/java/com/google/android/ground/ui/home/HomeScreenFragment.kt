395e51e8dea06dea8e024c6aebd88a58d27c7fdb
Shobhit Agarwal <ashobhit@google.com>
2024-04-27 16:12:05 +0530

    Remove licenses entrypoint from app drawer

--- ground/src/main/java/com/google/android/ground/ui/home/HomeScreenFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/home/HomeScreenFragment.kt
@@ -15,7 +15,6 @@
  */
 package com.google.android.ground.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -28,7 +27,6 @@ import androidx.core.view.GravityCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.ground.BuildConfig
 import com.google.android.ground.MainViewModel
 import com.google.android.ground.R
@@ -102,7 +100,6 @@ class HomeScreenFragment :
       homeScreenViewModel.showSurveySelector()
     }
     updateNavHeader()
-    binding.navView.findViewById<TextView>(R.id.view_licenses).setOnClickListener { showLicenses() }
     // Re-open data collection screen if any drafts are present
     viewLifecycleOwner.lifecycleScope.launch {
       homeScreenViewModel.maybeNavigateToDraftSubmission()
@@ -168,10 +165,5 @@ class HomeScreenFragment :
         AppTheme { SignOutConfirmationDialog(openDialog) { userRepository.signOut() } }
       }
     }
-  }
-
-  private fun showLicenses() {
-    OssLicensesMenuActivity.setActivityTitle(getString(R.string.view_licenses_title))
-    startActivity(Intent(activity, OssLicensesMenuActivity::class.java))
   }
 }
