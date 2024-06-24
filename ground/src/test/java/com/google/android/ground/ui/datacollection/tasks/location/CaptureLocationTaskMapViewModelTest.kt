8d50e46b477e748e9bc5d06dd98d7a2c944f2b71
anandwana001 <akshaynandwana001@gmail.com>
2024-06-24 20:26:03 +0530

    updated test and added license

--- ground/src/test/java/com/google/android/ground/ui/datacollection/tasks/location/CaptureLocationTaskMapViewModelTest.kt
+++ ground/src/test/java/com/google/android/ground/ui/datacollection/tasks/location/CaptureLocationTaskMapViewModelTest.kt
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.ground.ui.datacollection.tasks.location
 
 import com.google.android.ground.BaseHiltTest
-import com.google.android.ground.ui.common.BaseMapViewModel
+import com.google.android.ground.ui.common.MapConfig
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Test
@@ -14,11 +29,16 @@ import javax.inject.Inject
 class CaptureLocationTaskMapViewModelTest : BaseHiltTest() {
 
   @Inject lateinit var captureLocationTaskMapViewModel: CaptureLocationTaskMapViewModel
-  @Inject lateinit var baseMapViewModel: BaseMapViewModel
 
   @Test
   fun testGetMapConfig() {
     assertThat(captureLocationTaskMapViewModel.mapConfig)
-      .isEqualTo(baseMapViewModel.mapConfig.copy(allowGestures = false))
+      .isEqualTo(MapConfig(showOfflineImagery = true, allowGestures = false))
+  }
+
+  @Test
+  fun testGetMapConfigWithNoOfflineImagery() {
+    assertThat(captureLocationTaskMapViewModel.mapConfig)
+      .isNotEqualTo(MapConfig(showOfflineImagery = false, allowGestures = false))
   }
 }
