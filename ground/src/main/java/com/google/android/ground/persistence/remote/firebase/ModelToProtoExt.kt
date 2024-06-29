1c200b656fccb991434b1d2506b4aafdd8c16019
Shobhit Agarwal <ashobhit@google.com>
2024-06-29 19:40:52 +0530

    Add Properties and LocationOfInterest

--- ground/src/main/java/com/google/android/ground/persistence/remote/firebase/ModelToProtoExt.kt
+++ ground/src/main/java/com/google/android/ground/persistence/remote/firebase/ModelToProtoExt.kt
@@ -23,15 +23,37 @@ import com.google.android.ground.model.geometry.LinearRing
 import com.google.android.ground.model.geometry.MultiPolygon
 import com.google.android.ground.model.geometry.Point
 import com.google.android.ground.model.geometry.Polygon
+import com.google.android.ground.model.locationofinterest.LocationOfInterest
+import com.google.android.ground.model.locationofinterest.LoiProperties
+import com.google.protobuf.Timestamp
+import java.util.Date
 import com.google.android.ground.proto.AuditInfo as AuditInfoProto
 import com.google.android.ground.proto.Coordinates as CoordinatesProto
 import com.google.android.ground.proto.Geometry as GeometryProto
 import com.google.android.ground.proto.LinearRing as LinearRingProto
+import com.google.android.ground.proto.LocationOfInterest as LocationOfInterestProto
+import com.google.android.ground.proto.LocationOfInterest.Property as PropertyProto
+import com.google.android.ground.proto.LocationOfInterest.Source as SourceProto
 import com.google.android.ground.proto.MultiPolygon as MultiPolygonProto
 import com.google.android.ground.proto.Point as PointProto
 import com.google.android.ground.proto.Polygon as PolygonProto
-import com.google.protobuf.Timestamp
-import java.util.Date
+
+fun LocationOfInterest.toProtoBuf(): LocationOfInterestProto =
+  LocationOfInterestProto.newBuilder()
+    .setId(id)
+    .setJobId(job.id)
+    .setGeometry(geometry.toProtoBuf())
+    .setSubmissionCount(submissionCount)
+    // TODO(#2499): Add ownerId
+    .setCreated(created.toProtoBuf())
+    .setLastModified(lastModified.toProtoBuf())
+    // TODO(#2499): Add customTag
+    .setSource(
+      if (isPredefined == null) SourceProto.SOURCE_UNSPECIFIED
+      else if (isPredefined) SourceProto.FIELD_DATA else SourceProto.IMPORTED
+    )
+    .putAllProperties(properties.toProtoBuf())
+    .build()
 
 private fun AuditInfo.toProtoBuf(): AuditInfoProto {
   val builder =
@@ -77,3 +99,17 @@ private fun Polygon.toProtoBuf(): PolygonProto =
 
 private fun MultiPolygon.toProtoBuf(): MultiPolygonProto =
   MultiPolygonProto.newBuilder().addAllPolygons(polygons.map { it.toProtoBuf() }).build()
+
+private fun LoiProperties.toProtoBuf(): Map<String, PropertyProto> {
+  val propertiesBuilder = mutableMapOf<String, PropertyProto>()
+  for ((key, value) in this) {
+    val property = PropertyProto.newBuilder()
+    when (value) {
+      is String -> property.setStringValue(value)
+      is Number -> property.setNumericValue(value.toDouble())
+      else -> throw UnsupportedOperationException("Unknown type $value")
+    }
+    propertiesBuilder[key] = property.build()
+  }
+  return propertiesBuilder.toMutableMap()
+}
