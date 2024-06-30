9c69ed6e873e2b238785bd45ff369b5e410e8fb0
Shobhit Agarwal <ashobhit@google.com>
2024-06-30 13:01:34 +0530

    Move to protobuf package

--- ground/src/main/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExt.kt
+++ ground/src/main/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExt.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.ground.persistence.remote.firebase
+package com.google.android.ground.persistence.remote.firebase.protobuf
 
 import com.google.android.ground.model.AuditInfo
 import com.google.android.ground.model.geometry.Coordinates
