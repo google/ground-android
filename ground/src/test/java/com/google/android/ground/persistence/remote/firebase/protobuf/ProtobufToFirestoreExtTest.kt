e73879385593987b3f6d4ab647c7b3ef7cf3b3cb
Gino Miceli <gmiceli@google.com>
2024-07-05 13:34:30 +0200

    Test oneof conversion

--- ground/src/test/java/com/google/android/ground/persistence/remote/firebase/protobuf/ProtobufToFirestoreExtTest.kt
+++ ground/src/test/java/com/google/android/ground/persistence/remote/firebase/protobuf/ProtobufToFirestoreExtTest.kt
@@ -17,9 +17,11 @@
 package com.google.android.ground.persistence.remote.firebase.protobuf
 
 import com.google.android.ground.proto.Survey
+import com.google.android.ground.proto.Task
 import com.google.android.ground.proto.Task.DateTimeQuestion.Type.BOTH_DATE_AND_TIME
 import com.google.android.ground.proto.Task.DateTimeQuestion.Type.UNSPECIFIED_DATE_TIME_QUESTION_TYPE
 import com.google.android.ground.proto.TaskKt.dateTimeQuestion
+import com.google.android.ground.proto.TaskKt.multipleChoiceQuestion
 import com.google.android.ground.proto.survey
 import com.google.android.ground.proto.task
 import com.google.android.ground.test.deeplyNestedTestObject
@@ -93,6 +95,16 @@ class ProtobufToFirestoreExtTest(
           expected = mapOf(),
         ),
         testCase(desc = "skips unspecified enum value", input = task {}, expected = mapOf()),
+        testCase(
+          desc = "converts oneof messages",
+          input =
+            task {
+              multipleChoiceQuestion = multipleChoiceQuestion {
+                type = Task.MultipleChoiceQuestion.Type.SELECT_MULTIPLE
+              }
+            },
+          expected = mapOf("10" to mapOf("1" to 2)),
+        ),
       )
 
     /** Help to improve readability by provided named args for positional test constructor args. */
