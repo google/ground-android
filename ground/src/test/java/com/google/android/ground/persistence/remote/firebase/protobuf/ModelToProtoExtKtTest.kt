24235e81113f5b64c93210d01f3aa9b11f30b9f0
Shobhit Agarwal <ashobhit@google.com>
2024-07-06 16:24:48 +0530

    Add remaining test cases

--- ground/src/test/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExtKtTest.kt
+++ ground/src/test/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExtKtTest.kt
@@ -24,18 +24,167 @@ import com.google.android.ground.model.mutation.Mutation
 import com.google.common.truth.Truth.assertThat
 import java.time.Instant
 import java.util.Date
+import org.junit.Assert.assertThrows
 import org.junit.Test
 
 class ModelToProtoExtKtTest {
 
   @Test
-  fun test() {
+  fun `createLoiMessage() when geometry is null`() {
     val user = User("userId", "user@gmail.com", "User")
     val mutation =
       LocationOfInterestMutation(
         type = Mutation.Type.CREATE,
-        syncStatus = Mutation.SyncStatus.PENDING,
-        surveyId = "surveyId",
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = null,
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = false,
+      )
+    val proto = mutation.createLoiMessage(user)
+    val output = proto.toFirestoreMap()
+
+    assertThat(output)
+      .isEqualTo(
+        mapOf(
+          "1" to "loiId",
+          "2" to "jobId",
+          "4" to 1,
+          "5" to "userId",
+          "6" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "7" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "8" to "customId",
+          "9" to 1,
+          "10" to mapOf("name" to mapOf("1" to "loiName")),
+        )
+      )
+  }
+
+  @Test
+  fun `createLoiMessage() when isPredefined is null and type is CREATE`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.CREATE,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = Point(Coordinates(10.0, 20.0)),
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = null,
+      )
+    val proto = mutation.createLoiMessage(user)
+    val output = proto.toFirestoreMap()
+
+    assertThat(output)
+      .isEqualTo(
+        mapOf(
+          "1" to "loiId",
+          "2" to "jobId",
+          "3" to mapOf("1" to mapOf("1" to mapOf("1" to 10.0, "2" to 20.0))),
+          "4" to 1,
+          "5" to "userId",
+          "6" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "7" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "8" to "customId",
+          "10" to mapOf("name" to mapOf("1" to "loiName")),
+        )
+      )
+  }
+
+  @Test
+  fun `createLoiMessage() when isPredefined is true and type is CREATE`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.CREATE,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = Point(Coordinates(10.0, 20.0)),
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = true,
+      )
+    val proto = mutation.createLoiMessage(user)
+    val output = proto.toFirestoreMap()
+
+    assertThat(output)
+      .isEqualTo(
+        mapOf(
+          "1" to "loiId",
+          "2" to "jobId",
+          "3" to mapOf("1" to mapOf("1" to mapOf("1" to 10.0, "2" to 20.0))),
+          "4" to 1,
+          "5" to "userId",
+          "6" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "7" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "8" to "customId",
+          "9" to 2,
+          "10" to mapOf("name" to mapOf("1" to "loiName")),
+        )
+      )
+  }
+
+  @Test
+  fun `createLoiMessage() when isPredefined is false and type is CREATE`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.CREATE,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
         locationOfInterestId = "loiId",
         userId = "userId",
         jobId = "jobId",
@@ -76,5 +225,91 @@ class ModelToProtoExtKtTest {
           "10" to mapOf("name" to mapOf("1" to "loiName")),
         )
       )
+  }
+
+  @Test
+  fun `createLoiMessage() when type is UPDATE`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.UPDATE,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = Point(Coordinates(10.0, 20.0)),
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = false,
+      )
+    val proto = mutation.createLoiMessage(user)
+    val output = proto.toFirestoreMap()
+
+    assertThat(output)
+      .isEqualTo(
+        mapOf(
+          "1" to "loiId",
+          "2" to "jobId",
+          "3" to mapOf("1" to mapOf("1" to mapOf("1" to 10.0, "2" to 20.0))),
+          "4" to 1,
+          "5" to "userId",
+          "7" to
+            mapOf(
+              "1" to "userId",
+              "2" to mapOf("1" to 1000000L),
+              "3" to mapOf("1" to 1000000L),
+              "4" to "User",
+            ),
+          "8" to "customId",
+          "10" to mapOf("name" to mapOf("1" to "loiName")),
+        )
+      )
+  }
+
+  @Test
+  fun `createLoiMessage() when type is DELETE throws error`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.DELETE,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = Point(Coordinates(10.0, 20.0)),
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = false,
+      )
+
+    assertThrows(UnsupportedOperationException::class.java) { mutation.createLoiMessage(user) }
+  }
+
+  @Test
+  fun `createLoiMessage() when type is UNKNOWN throws error`() {
+    val user = User("userId", "user@gmail.com", "User")
+    val mutation =
+      LocationOfInterestMutation(
+        type = Mutation.Type.UNKNOWN,
+        syncStatus = Mutation.SyncStatus.PENDING, // this field is ignored
+        surveyId = "surveyId", // this field is ignored
+        locationOfInterestId = "loiId",
+        userId = "userId",
+        jobId = "jobId",
+        customId = "customId",
+        clientTimestamp = Date.from(Instant.ofEpochMilli(1000)),
+        geometry = Point(Coordinates(10.0, 20.0)),
+        submissionCount = 1,
+        properties = generateProperties("loiName"),
+        isPredefined = false,
+      )
+
+    assertThrows(UnsupportedOperationException::class.java) { mutation.createLoiMessage(user) }
   }
 }
