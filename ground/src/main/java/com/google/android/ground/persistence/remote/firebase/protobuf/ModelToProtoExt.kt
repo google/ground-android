fdcab06d42a162ba29c5e2f3bd1829de9f8e51d5
Sufyan Abbasi <sufy@google.com>
2024-07-24 23:36:20 -0700

    Use property name fallback for LOI customTag

--- ground/src/main/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExt.kt
+++ ground/src/main/java/com/google/android/ground/persistence/remote/firebase/protobuf/ModelToProtoExt.kt
@@ -23,6 +23,7 @@ import com.google.android.ground.model.geometry.LinearRing
 import com.google.android.ground.model.geometry.MultiPolygon
 import com.google.android.ground.model.geometry.Point
 import com.google.android.ground.model.geometry.Polygon
+import com.google.android.ground.model.locationofinterest.LOI_NAME_PROPERTY
 import com.google.android.ground.model.locationofinterest.LoiProperties
 import com.google.android.ground.model.mutation.LocationOfInterestMutation
 import com.google.android.ground.model.mutation.Mutation
@@ -102,7 +103,7 @@ fun LocationOfInterestMutation.createLoiMessage(user: User) = locationOfInterest
   jobId = me.jobId
   submissionCount = me.submissionCount
   ownerId = me.userId
-  customTag = me.customId
+  customTag = me.customId.ifEmpty { me.properties[LOI_NAME_PROPERTY]?.toString() ?: "" }
 
   properties.putAll(me.properties.toMessageMap())
 
