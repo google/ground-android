d3299a7124bb6624d5ded6685cd351288e84a215
Akshay Nandwana <akshaynandwana001@gmail.com>
2024-07-29 16:52:35 +0530

    added test cases

---
+++ ground/src/test/java/com/google/android/ground/ui/home/AboutFragmentTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.ground.ui.home
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import com.google.android.ground.BaseHiltTest
+import com.google.android.ground.R
+import com.google.android.ground.launchFragmentWithNavController
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@HiltAndroidTest
+@RunWith(RobolectricTestRunner::class)
+class AboutFragmentTest : BaseHiltTest() {
+
+  private lateinit var fragment: AboutFragment
+
+  @get:Rule override val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+  @Before
+  override fun setUp() {
+    super.setUp()
+    launchFragmentWithNavController<AboutFragment>(destId = R.id.aboutFragment) {
+      fragment = this as AboutFragment
+    }
+  }
+
+  @Test
+  fun `Toolbar is displayed`() {
+    composeTestRule.onNodeWithText("About").assertIsDisplayed()
+  }
+
+  @Test
+  fun `Back Icon click closes the screen`() {
+    composeTestRule.onNodeWithTag("logo").assertIsDisplayed()
+
+    composeTestRule
+      .onNodeWithText(composeTestRule.activity.getString(R.string.about_ground))
+      .assertIsDisplayed()
+  }
+}
