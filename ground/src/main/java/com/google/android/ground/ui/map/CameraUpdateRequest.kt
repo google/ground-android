f830cda3c23167da9a853348c3257b6127984af1
Shobhit Agarwal <ashobhit@google.com>
2024-08-11 11:08:43 +0530

    Updated kt doc

--- ground/src/main/java/com/google/android/ground/ui/map/CameraUpdateRequest.kt
+++ ground/src/main/java/com/google/android/ground/ui/map/CameraUpdateRequest.kt
@@ -19,14 +19,14 @@ import com.google.android.ground.model.geometry.Coordinates
 
 data class CameraUpdateRequest(val position: Position? = null, val shouldAnimate: Boolean = false)
 
-abstract class Position
-
-data class PositionViaBounds(val bounds: Bounds) : Position()
-
 // Prefer setting the position via coordinates as it doesn't cause the map to zoom out even if
 // nothing has changed. This is probably because the bound calculation is approximate, causing the
 // zoom levels to change slightly each time. Currently, we only set the position via bounds when
-// restoring from last saved location or when there are no saved positions (i.e. on first open).
+// there are no saved positions i.e. on first open.
+sealed class Position
+
+data class PositionViaBounds(val bounds: Bounds) : Position()
+
 data class PositionViaCoordinates(
   val coordinates: Coordinates,
   val zoomLevel: Float? = null,
