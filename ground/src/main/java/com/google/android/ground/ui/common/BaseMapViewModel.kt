07a2efbac0c079c719a016baed4a55492da02d28
Shobhit Agarwal <ashobhit@google.com>
2024-08-11 12:50:26 +0530

    Fix detekt errors

--- ground/src/main/java/com/google/android/ground/ui/common/BaseMapViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/common/BaseMapViewModel.kt
@@ -42,6 +42,7 @@ import com.google.android.ground.ui.map.NewPositionViaBounds
 import com.google.android.ground.ui.map.NewPositionViaCoordinates
 import com.google.android.ground.ui.map.gms.GmsExt.toBounds
 import com.google.android.ground.ui.map.gms.toCoordinates
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -61,7 +62,6 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import javax.inject.Inject
 
 open class BaseMapViewModel
 @Inject
