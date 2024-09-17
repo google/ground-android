e4d0d1b71e8219dac3b962d142c66e15867383ce
Akshay Nandwana <akshaynandwana001@gmail.com>
2024-09-17 14:17:48 +0530

    test fix

--- ground/src/test/java/com/google/android/ground/persistence/remote/firebase/schema/SubmissionMutationConverterTest.kt
+++ ground/src/test/java/com/google/android/ground/persistence/remote/firebase/schema/SubmissionMutationConverterTest.kt
@@ -322,12 +322,12 @@ class SubmissionMutationConverterTest {
       ),
       mapOf(
         DATE_TIME_RESPONSE_FIELD_NUMBER.toString() to
-          mapOf(DATE_TIME_FIELD_NUMBER.toString() to mapOf("1" to 1725537603066)),
+          mapOf(DATE_TIME_FIELD_NUMBER.toString() to mapOf("1" to 1725537603)),
         TASK_ID_FIELD_NUMBER.toString() to "date_task",
       ),
       mapOf(
         DATE_TIME_RESPONSE_FIELD_NUMBER.toString() to
-          mapOf(DATE_TIME_FIELD_NUMBER.toString() to mapOf("1" to 1725537603066)),
+          mapOf(DATE_TIME_FIELD_NUMBER.toString() to mapOf("1" to 1725537603)),
         TASK_ID_FIELD_NUMBER.toString() to "time_task",
       ),
     )
