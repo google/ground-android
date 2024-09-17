36422d1ee60cf93261dca266c7b32f80a566f750
Akshay Nandwana <akshaynandwana001@gmail.com>
2024-09-17 15:12:10 +0530

    test fix

--- ground/src/test/java/com/google/android/ground/persistence/local/DateTimeTaskDataTest.kt
+++ ground/src/test/java/com/google/android/ground/persistence/local/DateTimeTaskDataTest.kt
@@ -15,46 +15,15 @@
  */
 package com.google.android.ground.persistence.local
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
-import com.google.android.ground.BaseHiltTest
 import com.google.android.ground.model.submission.DateTaskData
 import com.google.common.truth.Truth.assertThat
-import dagger.hilt.android.testing.HiltAndroidTest
-import java.util.*
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@HiltAndroidTest
-@RunWith(RobolectricTestRunner::class)
-class DateTimeTaskDataTest : BaseHiltTest() {
-
-  private lateinit var context: Context
-
-  override fun setUp() {
-    super.setUp()
-    context = ApplicationProvider.getApplicationContext()
-  }
+class DateTimeTaskDataTest {
 
   @Test
   fun testTimeResponse_textDetails() {
-    val detailsText = DateTaskData.fromDate(getCalendar().time.time)
-    assertThat(detailsText).isEqualTo(DateTaskData(time = 1672549215471))
-  }
-
-  companion object {
-    private val CALENDAR = Calendar.getInstance()
-
-    fun getCalendar(): Calendar {
-      // January 1, 2023, 10:30:15 AM
-      CALENDAR.set(Calendar.YEAR, 2023)
-      CALENDAR.set(Calendar.MONTH, Calendar.JANUARY) // 0-based, so January is 0
-      CALENDAR.set(Calendar.DAY_OF_MONTH, 1)
-      CALENDAR.set(Calendar.HOUR_OF_DAY, 10) // 24-hour format
-      CALENDAR.set(Calendar.MINUTE, 30)
-      CALENDAR.set(Calendar.SECOND, 15)
-      return CALENDAR
-    }
+    val calendarTime = DateTaskData.fromDate(1672549215471)
+    assertThat(calendarTime).isEqualTo(DateTaskData(time = 1672549215471))
   }
 }
