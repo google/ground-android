8943cd962dd2eb6a36310d40573df432ef40f91f
Shobhit Agarwal <ashobhit@google.com>
2024-10-13 13:12:53 +0530

    Add helper methods to enhance readability

--- ground/src/main/java/com/google/android/ground/ui/home/HomeScreenFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/home/HomeScreenFragment.kt
@@ -173,35 +173,33 @@ class HomeScreenFragment :
     // compose.
     binding.composeView.apply {
       setContent {
-        val showUserDetailsDialog = remember { mutableStateOf(true) }
+        val showUserDetailsDialog = remember { mutableStateOf(false) }
         val showSignOutDialog = remember { mutableStateOf(false) }
 
-        // reset the state for recomposition
-        showUserDetailsDialog.value = true
-        showSignOutDialog.value = false
+        fun showUserDetailsDialog() {
+          showUserDetailsDialog.value = true
+          showSignOutDialog.value = false
+        }
+
+        fun showSignOutDialog() {
+          showUserDetailsDialog.value = false
+          showSignOutDialog.value = true
+        }
+
+        fun hideAllDialogs() {
+          showUserDetailsDialog.value = false
+          showSignOutDialog.value = false
+        }
+
+        // Init state for composition
+        showUserDetailsDialog()
 
         AppTheme {
           if (showUserDetailsDialog.value) {
-            UserDetailsDialog(
-              user,
-              {
-                showUserDetailsDialog.value = false
-                showSignOutDialog.value = true
-              },
-              {
-                showUserDetailsDialog.value = false
-                showSignOutDialog.value = false
-              },
-            )
+            UserDetailsDialog(user, { showSignOutDialog() }, { hideAllDialogs() })
           }
           if (showSignOutDialog.value) {
-            SignOutConfirmationDialog(
-              { homeScreenViewModel.signOut() },
-              {
-                showUserDetailsDialog.value = false
-                showSignOutDialog.value = false
-              },
-            )
+            SignOutConfirmationDialog({ homeScreenViewModel.signOut() }, { hideAllDialogs() })
           }
         }
       }
