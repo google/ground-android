f776297f4da13fb385fd9e89a1c2cc2ed58dcba9
Akshay Nandwana <akshaynandwana001@gmail.com>
2024-11-08 12:12:29 +0530

    added todo

--- ground/src/main/java/com/google/android/ground/ui/surveyselector/SurveySelectorFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/surveyselector/SurveySelectorFragment.kt
@@ -96,6 +96,7 @@ class SurveySelectorFragment : AbstractFragment(), BackPressListener {
     binding.recyclerView.adapter = adapter
     getAbstractActivity().setSupportActionBar(binding.toolbar)
 
+    // TODO - https://github.com/google/ground-android/issues/2692#issuecomment-2430978043
     if (parentFragmentManager.backStackEntryCount > 0) {
       getAbstractActivity().supportActionBar?.setDisplayHomeAsUpEnabled(true)
     } else {
