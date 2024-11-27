ebcb687dec53e727e8f0c3d61c4ae61c6ae7d3b5
Akshay Nandwana <akshaynandwana001@gmail.com>
2024-11-27 21:06:33 +0530

    update ui

--- ground/src/main/java/com/google/android/ground/ui/offlineareas/selector/DownloadProgressDialogFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/offlineareas/selector/DownloadProgressDialogFragment.kt
@@ -16,37 +16,59 @@
 
 package com.google.android.ground.ui.offlineareas.selector
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.android.ground.R
 
 @Composable
 fun DownloadProgressDialog(viewModel: OfflineAreaSelectorViewModel, onDismiss: () -> Unit) {
-  val progressData = viewModel.downloadProgress.observeAsState()
+  val progress by viewModel.downloadProgress.observeAsState(0)
+  val maxProgress by viewModel.downloadProgressMax.observeAsState(100)
 
   AlertDialog(
     containerColor = MaterialTheme.colorScheme.surface,
     onDismissRequest = {},
     title = {
       Text(
-        stringResource(R.string.offline_map_imagery_download_progress_dialog_title),
+        stringResource(R.string.offline_map_imagery_download_progress_dialog_title, progress),
         color = MaterialTheme.colorScheme.onSurface,
       )
     },
     text = {
       Column {
+        val animatedProgress by
+          animateFloatAsState(
+            targetValue = progress.toFloat() / maxProgress,
+            animationSpec = tween(durationMillis = 300),
+          )
+
         LinearProgressIndicator(
-          progress = { progressData.value?.toFloat() ?: 0f },
+          modifier =
+            Modifier.background(
+              shape = RoundedCornerShape(4.dp),
+              color = MaterialTheme.colorScheme.primary,
+            ),
+          progress = { animatedProgress },
           color = MaterialTheme.colorScheme.primary,
           trackColor = MaterialTheme.colorScheme.surfaceVariant,
         )
+        Spacer(Modifier.height(8.dp))
         Text(
           stringResource(R.string.offline_map_imagery_download_progress_dialog_message),
           color = MaterialTheme.colorScheme.onSurfaceVariant,
