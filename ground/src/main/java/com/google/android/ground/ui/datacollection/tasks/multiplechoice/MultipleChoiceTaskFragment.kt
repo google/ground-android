5899fb7122786c9feeb3a7bdcab78c10b0a7aee7
Shobhit Agarwal <ashobhit@google.com>
2024-12-16 23:34:17 +0530

    Use PAT to allow committing changes to protected branches

--- ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/multiplechoice/MultipleChoiceTaskFragment.kt
+++ ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/multiplechoice/MultipleChoiceTaskFragment.kt
@@ -38,17 +38,16 @@ class MultipleChoiceTaskFragment : AbstractTaskFragment<MultipleChoiceTaskViewMo
   private lateinit var multipleChoiceAdapter:
     ListAdapter<MultipleChoiceItem, RecyclerView.ViewHolder>
 
+  // TODO: Test comment
   override fun onCreateTaskView(inflater: LayoutInflater): TaskView =
     TaskViewFactory.createWithHeader(inflater)
 
   override fun onCreateTaskBody(inflater: LayoutInflater): View {
-    // TODO: Replace with compose ux
     binding = MultipleChoiceTaskFragBinding.inflate(inflater)
     setupMultipleChoice(binding.selectOptionList)
     return binding.root
   }
 
-  // TODO: Replace recycler view with compose ux
   private fun setupMultipleChoice(recyclerView: RecyclerView) {
     val multipleChoice = checkNotNull(getTask().multipleChoice)
     val canSelectMultiple = multipleChoice.cardinality == MultipleChoice.Cardinality.SELECT_MULTIPLE
