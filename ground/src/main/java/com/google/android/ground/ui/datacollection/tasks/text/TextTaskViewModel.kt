0d837b2e9e77cd5db2a45da3cced70b05d8aed41
Shobhit Agarwal <ashobhit@google.com>
2024-12-21 01:12:05 +0530

    Remove LiveData of getDetailsText() from Abstract view model

--- ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/text/TextTaskViewModel.kt
+++ ground/src/main/java/com/google/android/ground/ui/datacollection/tasks/text/TextTaskViewModel.kt
@@ -23,14 +23,14 @@ import com.google.android.ground.model.submission.TaskData
 import com.google.android.ground.model.submission.TextTaskData
 import com.google.android.ground.model.submission.TextTaskData.Companion.fromString
 import com.google.android.ground.ui.datacollection.tasks.AbstractTaskViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 class TextTaskViewModel @Inject constructor() : AbstractTaskViewModel() {
 
   /** Transcoded text to be displayed for the current [TaskData]. */
-  val textResponse: LiveData<String> =
+  val responseText: LiveData<String> =
     taskTaskData.filterIsInstance<TextTaskData>().map { it.text }.asLiveData()
 
   val textWatcher =
