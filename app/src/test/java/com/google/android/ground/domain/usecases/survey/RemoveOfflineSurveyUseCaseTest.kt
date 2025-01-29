a49d48b6596f0099f37de8dc16a3ef1542fc4975
Shobhit Agarwal <ashobhit@google.com>
2025-01-29 15:52:04 +0530

    Fix imports order

--- app/src/test/java/com/google/android/ground/domain/usecases/survey/RemoveOfflineSurveyUseCaseTest.kt
+++ app/src/test/java/com/google/android/ground/domain/usecases/survey/RemoveOfflineSurveyUseCaseTest.kt
@@ -22,12 +22,12 @@ import com.google.android.ground.persistence.local.stores.LocalSurveyStore
 import com.google.android.ground.repository.SurveyRepository
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidTest
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.advanceUntilIdle
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import javax.inject.Inject
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltAndroidTest
