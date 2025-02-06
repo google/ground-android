2d32ba4aa2a48ab60205b7a649159915754e13df
Shobhit Agarwal <ashobhit@google.com>
2025-02-06 16:51:05 +0530

    Revert changes to ComposeExt

--- app/src/main/java/com/google/android/ground/util/ComposeExt.kt
+++ app/src/main/java/com/google/android/ground/util/ComposeExt.kt
@@ -16,7 +16,6 @@
 package com.google.android.ground.util
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
@@ -31,7 +30,7 @@ import com.google.android.ground.ui.theme.AppTheme
  * @throws IllegalStateException if the Fragment's view is null or not a [ViewGroup].
  */
 fun Fragment.renderComposableDialog(composable: @Composable () -> Unit) {
-  (view as ViewGroup).renderComposable { composable() }
+  (view as ViewGroup).addView(createComposeView { composable() })
 }
 
 /**
@@ -41,16 +40,7 @@ fun Fragment.renderComposableDialog(composable: @Composable () -> Unit) {
  * @throws IllegalStateException if the Activity's content view is null or not a [ViewGroup].
  */
 fun Activity.renderComposableDialog(composable: @Composable () -> Unit) =
-  findViewById<ViewGroup>(android.R.id.content).renderComposable { composable() }
-
-/**
- * Renders a composable function within the current [ViewGroup].
- *
- * @param composable The composable function to be rendered within the [ComposeView].
- * @return A [View] instance of [ComposeView] configured to display the provided composable.
- */
-fun ViewGroup.renderComposable(composable: @Composable () -> Unit) =
-  addView(createComposeView { composable() })
+  findViewById<ViewGroup>(android.R.id.content).addView(createComposeView { composable() })
 
 /**
  * Creates a [ComposeView] within a [Fragment] that hosts the provided composable function.
@@ -59,24 +49,15 @@ fun ViewGroup.renderComposable(composable: @Composable () -> Unit) =
  * @return A [View] instance of [ComposeView] configured to display the provided composable.
  */
 fun Fragment.createComposeView(composable: @Composable () -> Unit): View =
-  requireContext().createComposeView { composable() }
+  ComposeView(requireContext()).apply { setComposableContent { composable() } }
 
 /**
- * Creates a [ComposeView] within a [ViewGroup] that hosts the provided composable function.
+ * Creates a [ComposeView] within an [Activity] that hosts the provided composable function.
  *
  * @param composable The composable function to be rendered within the [ComposeView].
  * @return A [View] instance of [ComposeView] configured to display the provided composable.
  */
-fun ViewGroup.createComposeView(composable: @Composable () -> Unit): View =
-  context.createComposeView { composable() }
-
-/**
- * Creates a [ComposeView] in the current [Context].
- *
- * @param composable The composable function to be rendered within the [ComposeView].
- * @return A [View] instance of [ComposeView] configured to display the provided composable.
- */
-fun Context.createComposeView(composable: @Composable () -> Unit): View =
+fun Activity.createComposeView(composable: @Composable () -> Unit): View =
   ComposeView(this).apply { setComposableContent { composable() } }
 
 /**
