1e36bd5893a8d8a5be14fed0899ae817264ea202
Akshay Nandwana <akshaynandwana001@gmail.com>
2025-02-26 08:09:08 +0530

    generalise

--- app/src/main/java/com/google/android/ground/ui/datacollection/tasks/polygon/DrawAreaTaskFragment.kt
+++ app/src/main/java/com/google/android/ground/ui/datacollection/tasks/polygon/DrawAreaTaskFragment.kt
@@ -15,12 +15,7 @@
  */
 package com.google.android.ground.ui.datacollection.tasks.polygon
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.os.VibratorManager
 import android.view.LayoutInflater
 import android.view.View
 import androidx.lifecycle.lifecycleScope
@@ -87,27 +82,10 @@ class DrawAreaTaskFragment @Inject constructor() : AbstractTaskFragment<DrawArea
     addPointButton =
       addButton(ButtonAction.ADD_POINT).setOnClickListener {
         viewModel.addLastVertex()
-        vibrate()
+        viewModel.triggerVibration()
       }
     completeButton =
       addButton(ButtonAction.COMPLETE).setOnClickListener { viewModel.completePolygon() }
-  }
-
-  private fun vibrate() {
-    val vibrator =
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        val vibratorManager =
-          context?.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-        vibratorManager.defaultVibrator
-      } else {
-        context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-      }
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
-    } else {
-      vibrator.vibrate(100)
-    }
   }
 
   /** Removes the last vertex from the polygon. */
