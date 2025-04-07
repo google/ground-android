725adb87a1aeb0ab3c70867d5c09012b3ee9441c
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2025-04-07 19:40:20 +0700

    Inject IoDispatcher into LocalMutationSyncWorkerTest for coroutine context

--- app/src/test/java/org/groundplatform/android/persistence/sync/LocalMutationSyncWorkerTest.kt
+++ app/src/test/java/org/groundplatform/android/persistence/sync/LocalMutationSyncWorkerTest.kt
@@ -27,10 +27,12 @@ import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
 import dagger.hilt.android.testing.HiltAndroidTest
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.groundplatform.android.BaseHiltTest
 import org.groundplatform.android.FakeData
+import org.groundplatform.android.coroutines.IoDispatcher
 import org.groundplatform.android.model.geometry.Point
 import org.groundplatform.android.model.mutation.LocationOfInterestMutation
 import org.groundplatform.android.model.mutation.Mutation
@@ -78,6 +80,8 @@ class LocalMutationSyncWorkerTest : BaseHiltTest() {
 
   @Inject lateinit var userRepository: UserRepository
 
+  @Inject @IoDispatcher lateinit var ioDispatcher: CoroutineDispatcher
+
   private val factory =
     object : WorkerFactory() {
       override fun createWorker(
@@ -92,6 +96,7 @@ class LocalMutationSyncWorkerTest : BaseHiltTest() {
           fakeRemoteDataStore,
           mockMediaUploadWorkManager,
           userRepository,
+          ioDispatcher,
         )
     }
 
