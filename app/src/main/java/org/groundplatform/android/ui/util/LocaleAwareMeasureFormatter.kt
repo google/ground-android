5c015dbfeb1fa210920d4bc2417067e00b5c5df7
Gino Miceli <gmiceli@google.com>
2025-04-08 13:31:37 -0400

    Simplify getting imperial system

--- app/src/main/java/org/groundplatform/android/ui/util/LocaleAwareMeasureFormatter.kt
+++ app/src/main/java/org/groundplatform/android/ui/util/LocaleAwareMeasureFormatter.kt
@@ -17,16 +17,13 @@ package org.groundplatform.android.ui.util
 
 import android.content.res.Resources
 import android.icu.text.MeasureFormat
-import android.icu.util.LocaleData
 import android.icu.util.Measure
 import android.icu.util.MeasureUnit
 import android.icu.util.ULocale
-import android.os.Build
 import java.util.Locale
 import javax.inject.Inject
 import kotlin.math.floor
 import kotlin.math.pow
-import timber.log.Timber
 
 private const val METERS_TO_FEET = 3.28084
 /** Distances equal to or greater than this value are rounded down to the nearest integer. */
@@ -65,23 +62,5 @@ class LocaleAwareMeasureFormatter @Inject constructor(val resources: Resources) 
   }
 
   private fun isImperialSystem(locale: Locale): Boolean =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-      runCatching {
-          LocaleData.getMeasurementSystem(ULocale.forLocale(locale)) ==
-            LocaleData.MeasurementSystem.US
-        }
-        .getOrElse { exception ->
-          Timber.w(
-            "MeasurementSystem",
-            "Failed to get measurement system from LocaleData",
-            exception,
-          )
-          isImperialSystemFallback(locale)
-        }
-    } else {
-      isImperialSystemFallback(locale)
-    }
-
-  private fun isImperialSystemFallback(locale: Locale): Boolean =
     locale.country.uppercase(Locale.ROOT) in setOf("US", "LR", "MM")
 }
