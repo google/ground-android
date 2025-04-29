2078d2fd4f326d10231371fb59f0b03751f1e89d
scolsen <scg.olsen@gmail.com>
2025-04-29 10:59:01 -0400

    Text Tasks: Update character limit UI
    Accords with Material recommendations on supporting text for text fields.

--- app/src/main/java/org/groundplatform/android/ui/datacollection/tasks/multiplechoice/MultipleChoiceItemView.kt
+++ app/src/main/java/org/groundplatform/android/ui/datacollection/tasks/multiplechoice/MultipleChoiceItemView.kt
@@ -17,12 +17,14 @@ package org.groundplatform.android.ui.datacollection.tasks.multiplechoice
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -35,6 +37,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.groundplatform.android.Config
@@ -99,8 +102,14 @@ fun MultipleChoiceItemView(
 
     if (item.isOtherOption) {
       Row(modifier = modifier.padding(horizontal = 48.dp)) {
-        TextField(
-          label = { Text(text = "${item.otherText.length} / ${Config.TEXT_DATA_CHAR_LIMIT}") },
+        OutlinedTextField(
+          supportingText = {
+            Row(modifier = modifier.fillMaxWidth()) {
+              Spacer(modifier = modifier.weight(1f))
+              Text("${item.otherText.length} / ${Config.TEXT_DATA_CHAR_LIMIT}", textAlign = TextAlign.End)
+            }
+          },
+          isError = item.otherText.length > Config.TEXT_DATA_CHAR_LIMIT,
           value = item.otherText,
           textStyle = MaterialTheme.typography.bodyLarge,
           onValueChange = { otherValueChanged(it) },
