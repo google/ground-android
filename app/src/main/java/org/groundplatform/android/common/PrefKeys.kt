7be146ba712e45d8081dc04f64b7423985e82ebd
Shobhit Agarwal <ashobhit@google.com>
2025-09-09 18:01:31 +0530

    Consolidate all preference keys into a single file and remove unused preference "feedback" (#3242)
    * Remove unused settings preference
    
    * Remove unused string translations
    
    * Refactor unit tests
    
    * Refactor settings fragment
    
    * Define all pref keys under common/PrefKeys
    
    * Fix merge conflicts

---
+++ app/src/main/java/org/groundplatform/android/common/PrefKeys.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.groundplatform.android.common
+
+/** Defines constants for names of shared preferences and settings. */
+object PrefKeys {
+
+  const val ACTIVE_SURVEY_ID_KEY = "activeSurveyId"
+  const val DATA_SHARING_CONSENT_PREFIX = "data_consent_"
+  const val DRAFT_SUBMISSION_ID = "draft_submission_id"
+  const val DRAW_AREA_INSTRUCTIONS_SHOWN = "draw_area_instructions_shown"
+  const val DROP_PIN_INSTRUCTIONS_SHOWN = "drop_pin_instructions_shown"
+  const val LANGUAGE = "language"
+  const val LAST_VIEWPORT_PREFIX = "last_viewport_"
+  const val LENGTH_UNIT = "length_unit"
+  const val LOCATION_LOCK_ENABLED = "location_lock_enabled"
+  const val MAP_TYPE = "map_type"
+  const val OFFLINE_MAP_IMAGERY = "offline_map_imagery"
+  const val TOS_ACCEPTED = "tos_accepted"
+  const val UPLOAD_MEDIA = "upload_media"
+  const val VISIT_WEBSITE = "visit_website"
+}
