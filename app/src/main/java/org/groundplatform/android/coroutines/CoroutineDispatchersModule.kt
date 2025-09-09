25b996f847492e837ef5838fd66965a5522a4be6
Shobhit Agarwal <ashobhit@google.com>
2025-09-09 16:09:06 +0530

    Removed DefaultDispatcher and MainImmediateDispatcher (#3250)

--- app/src/main/java/org/groundplatform/android/coroutines/CoroutineDispatchersModule.kt
+++ app/src/main/java/org/groundplatform/android/coroutines/CoroutineDispatchersModule.kt
@@ -26,23 +26,12 @@ import kotlinx.coroutines.Dispatchers
 @Module
 @InstallIn(SingletonComponent::class)
 object CoroutineDispatchersModule {
-  @DefaultDispatcher
-  @Provides
-  fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
   @IoDispatcher @Provides fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
   @MainDispatcher @Provides fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
-
-  @MainImmediateDispatcher
-  @Provides
-  fun provideMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
 }
-
-@Retention(AnnotationRetention.RUNTIME) @Qualifier annotation class DefaultDispatcher
 
 @Retention(AnnotationRetention.RUNTIME) @Qualifier annotation class IoDispatcher
 
 @Retention(AnnotationRetention.RUNTIME) @Qualifier annotation class MainDispatcher
-
-@Retention(AnnotationRetention.BINARY) @Qualifier annotation class MainImmediateDispatcher
