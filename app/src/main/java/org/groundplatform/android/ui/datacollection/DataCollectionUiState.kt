25d2864dfd29c31bd9fd8d53f5df1df5fad04331
Akshay Nandwana <akshaynandwana001@gmail.com>
2025-09-26 14:07:52 +0530

    added helper class

---
+++ app/src/main/java/org/groundplatform/android/ui/datacollection/DataCollectionUiState.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.groundplatform.android.ui.datacollection
+
+import org.groundplatform.android.model.job.Job
+import org.groundplatform.android.model.task.Task
+
+sealed interface DataCollectionUiState {
+  data object Loading : DataCollectionUiState
+
+  data class Ready(
+    val surveyId: String,
+    val job: Job,
+    val loiName: String,
+    val tasks: List<Task>,
+    val isAddLoiFlow: Boolean,
+    val currentTaskId: String,
+    val position: TaskPosition,
+  ) : DataCollectionUiState
+
+  data class Error(val message: String) : DataCollectionUiState
+
+  data class TaskUpdated(val position: TaskPosition) : DataCollectionUiState
+
+  data object TaskSubmitted : DataCollectionUiState
+}
