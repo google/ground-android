274a50fc352709df5957c3329caa07e72b187ad8
Akshay Nandwana <akshaynandwana001@gmail.com>
2025-12-01 10:54:04 +0530

    introduce unify_capture_location_task and a task bit

--- app/src/main/java/org/groundplatform/android/model/task/Task.kt
+++ app/src/main/java/org/groundplatform/android/model/task/Task.kt
@@ -33,6 +33,7 @@ constructor(
   val multipleChoice: MultipleChoice? = null,
   val isAddLoiTask: Boolean = false,
   val condition: Condition? = null,
+  val isUnified: Boolean = false,
 ) {
 
   // TODO: Define these in data layer!
