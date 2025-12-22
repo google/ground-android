8393b9843f1a39e1ca83bdf96520d2af5e28f6b7
andreia <andreia.mp.ferreira@gmail.com>
2025-12-22 19:17:17 +0100

    Rename test

--- e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
+++ e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
@@ -21,7 +21,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class SurveyRunnerTest {
+class CompleteAllTaskTypesTest {
 
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
