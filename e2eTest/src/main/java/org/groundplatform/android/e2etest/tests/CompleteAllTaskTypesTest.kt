b410e3141a14808504e507df146a6b755a57d6c5
andreia <andreia.mp.ferreira@gmail.com>
2026-01-13 16:01:50 +0100

    Add test rule to take screenshots on failure

--- e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
+++ e2eTest/src/main/java/org/groundplatform/android/e2etest/tests/CompleteAllTaskTypesTest.kt
@@ -1,11 +1,15 @@
 package org.groundplatform.android.e2etest.tests
 
 import android.Manifest
+import android.util.Log
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.core.app.takeScreenshot
+import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.UiDevice
+import java.io.IOException
 import org.groundplatform.android.e2etest.TestConfig
 import org.groundplatform.android.e2etest.drivers.AndroidTestDriver
 import org.groundplatform.android.e2etest.robots.DataCollectionRobot
@@ -18,12 +22,26 @@ import org.groundplatform.android.ui.map.gms.features.TEST_MARKER_TAG
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CompleteAllTaskTypesTest {
 
+  @get:Rule var nameRule = TestName()
+
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  @get:Rule
+  var screenshotRule =
+    object : TestWatcher() {
+      override fun failed(e: Throwable?, description: Description?) {
+        super.failed(e, description)
+        captureScreenshot()
+      }
+    }
 
   @get:Rule
   val runtimePermissionsRule: GrantPermissionRule =
@@ -71,6 +89,16 @@ class CompleteAllTaskTypesTest {
     with(DataCollectionRobot(testDriver)) {
       dismissInstructions()
       runTasks(TestConfig.TEST_LIST_DRAW_AREA)
+    }
+  }
+
+  private fun captureScreenshot() {
+    val screenShotName = "${javaClass.simpleName}_${nameRule.methodName}"
+    Log.d("Screenshots", "Taking screenshot of '$screenShotName'")
+    try {
+      takeScreenshot().writeToTestStorage(screenShotName)
+    } catch (ex: IOException) {
+      Log.e("Screenshots", "Could not take the screenshot", ex)
     }
   }
 }
