1a7165763142ed5fee1a833a7528e2c3b2a1ed3a
Andreia Ferreira <51242456+andreia-ferreira@users.noreply.github.com>
2026-01-16 03:44:55 +0100

    Fix conditional tasks in the room database (#3449)
    * Add migration to delete faulty surveys
    
    * Add migration test

--- app/src/androidTest/java/org/groundplatform/android/data/local/room/migration/MigrationTest.kt
+++ app/src/androidTest/java/org/groundplatform/android/data/local/room/migration/MigrationTest.kt
@@ -15,7 +15,6 @@
  */
 package org.groundplatform.android.data.local.room.migration
 
-import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import androidx.room.Room
 import androidx.room.migration.Migration
@@ -27,6 +26,11 @@ import java.io.IOException
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.groundplatform.android.data.local.room.LocalDatabase
+import org.groundplatform.android.data.local.room.migration.MigrationTestDataGenerator.getConditionContentValues
+import org.groundplatform.android.data.local.room.migration.MigrationTestDataGenerator.getExpressionContentValues
+import org.groundplatform.android.data.local.room.migration.MigrationTestDataGenerator.getJobContentValues
+import org.groundplatform.android.data.local.room.migration.MigrationTestDataGenerator.getSurveyContentValues
+import org.groundplatform.android.data.local.room.migration.MigrationTestDataGenerator.getTaskContentValues
 import org.groundplatform.android.model.Survey
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -36,6 +40,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MigrationTest {
   private val testDatabase = "test.db"
+  private val migrations = arrayOf(Migration_124_125, Migration_125_126)
 
   @get:Rule
   val helper =
@@ -45,42 +50,73 @@ class MigrationTest {
       openFactory = FrameworkSQLiteOpenHelperFactory(),
     )
 
-  private val testSurvey =
-    Survey(
-      id = "1",
-      title = "Test Survey",
-      description = "Description",
-      jobMap = mapOf(),
-      generalAccess =
-        org.groundplatform.android.proto.Survey.GeneralAccess.GENERAL_ACCESS_UNSPECIFIED,
-    )
-
   @Test
   @Throws(IOException::class)
   fun migrate124To125() = runBlocking {
+    val id = "124-125"
+    val title = "Test 124 to 125"
+    val description = "migration description"
+    val generalAccess =
+      org.groundplatform.android.proto.Survey.GeneralAccess.GENERAL_ACCESS_UNSPECIFIED.ordinal
+
     helper.createDatabase(testDatabase, 124).apply {
       insert(
-        table = "survey",
-        conflictAlgorithm = SQLiteDatabase.CONFLICT_REPLACE,
-        values =
-          ContentValues().apply {
-            put("id", testSurvey.id)
-            put("title", testSurvey.title)
-            put("description", testSurvey.description)
-            put("general_access", testSurvey.generalAccess.ordinal)
-          },
+        "survey",
+        SQLiteDatabase.CONFLICT_REPLACE,
+        getSurveyContentValues(id, title, description, generalAccess),
       )
       close()
     }
 
-    helper.runMigrationsAndValidate(testDatabase, 125, true, Migration_124_125)
+    helper.runMigrationsAndValidate(testDatabase, 125, true, *migrations)
 
-    with(getMigratedRoomDatabase(arrayOf(Migration_124_125))) {
+    with(getMigratedRoomDatabase(migrations)) {
       val migratedSurvey = surveyDao().getAll().first()[0].surveyEntity
-      assertEquals(testSurvey.id, migratedSurvey.id)
-      assertEquals(testSurvey.title, migratedSurvey.title)
-      assertEquals(testSurvey.description, migratedSurvey.description)
-      assertEquals(testSurvey.generalAccess.ordinal, migratedSurvey.generalAccess)
+      assertEquals(id, migratedSurvey.id)
+      assertEquals(title, migratedSurvey.title)
+      assertEquals(description, migratedSurvey.description)
+      assertEquals(generalAccess, migratedSurvey.generalAccess)
+      close()
+    }
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun migrate125To126() = runBlocking {
+    val correctSurvey = "survey1"
+    val faultySurvey = "survey2"
+    val jobId1 = "job1"
+    val jobId2 = "job2"
+    val taskId1 = "task1"
+    val taskId2 = "task2"
+
+    helper.createDatabase(testDatabase, 125).apply {
+      // Insert correct survey: Survey + Job + Task + Condition + Expression
+      insert("survey", SQLiteDatabase.CONFLICT_REPLACE, getSurveyContentValues(correctSurvey))
+      insert("job", SQLiteDatabase.CONFLICT_REPLACE, getJobContentValues(jobId1, correctSurvey))
+      insert("task", SQLiteDatabase.CONFLICT_REPLACE, getTaskContentValues(taskId1, jobId1))
+      insert("condition", SQLiteDatabase.CONFLICT_REPLACE, getConditionContentValues(taskId1))
+      insert("expression", SQLiteDatabase.CONFLICT_REPLACE, getExpressionContentValues(taskId1))
+
+      // Insert faulty survey: Survey + Job + Task + Condition WITHOUT Expression
+      insert("survey", SQLiteDatabase.CONFLICT_REPLACE, getSurveyContentValues(faultySurvey))
+      insert("job", SQLiteDatabase.CONFLICT_REPLACE, getJobContentValues(jobId2, faultySurvey))
+      insert("task", SQLiteDatabase.CONFLICT_REPLACE, getTaskContentValues(taskId2, jobId2))
+      insert("condition", SQLiteDatabase.CONFLICT_REPLACE, getConditionContentValues(taskId2))
+
+      close()
+    }
+
+    helper.runMigrationsAndValidate(testDatabase, 126, true, *migrations)
+
+    with(getMigratedRoomDatabase(migrations)) {
+      val surveys = surveyDao().getAll().first()
+      assertEquals(1, surveys.size)
+      assertEquals(correctSurvey, surveys[0].surveyEntity.id)
+
+      val deletedSurvey = surveyDao().findSurveyById(faultySurvey)
+      assertEquals(null, deletedSurvey)
+
       close()
     }
   }
