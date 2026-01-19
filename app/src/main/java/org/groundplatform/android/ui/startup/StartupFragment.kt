cfc45016af44b6ea6d4b6117f8e9d8c51d3481b6
Shobhit Agarwal <ashobhit@google.com>
2026-01-19 21:54:57 +0530

    Move logic from StartupFragment to compose screen and view models (#3461)

--- app/src/main/java/org/groundplatform/android/ui/startup/StartupFragment.kt
+++ app/src/main/java/org/groundplatform/android/ui/startup/StartupFragment.kt
@@ -19,28 +19,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import kotlinx.coroutines.launch
-import org.groundplatform.android.R
 import org.groundplatform.android.ui.common.AbstractFragment
 import org.groundplatform.android.ui.common.EphemeralPopups
-import timber.log.Timber
+import org.groundplatform.android.ui.theme.AppTheme
 
 @AndroidEntryPoint
 class StartupFragment : AbstractFragment() {
 
   @Inject lateinit var popups: EphemeralPopups
-
-  private val viewModel: StartupViewModel by viewModels()
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -49,29 +39,15 @@ class StartupFragment : AbstractFragment() {
   ): View =
     ComposeView(requireContext()).apply {
       setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-      setContent { Box(modifier = Modifier.fillMaxSize()) }
-    }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    viewLifecycleOwner.lifecycleScope.launch {
-      showProgressDialog(R.string.initializing)
-      try {
-        viewModel.initializeLogin()
-      } catch (t: Throwable) {
-        onInitFailed(t)
-      } finally {
-        dismissProgressDialog()
+      setContent {
+        AppTheme {
+          StartupScreen(
+            onLoadFailed = { errorMessageId ->
+              errorMessageId?.let { popups.ErrorPopup().show(it) }
+              requireActivity().finish()
+            }
+          )
+        }
       }
     }
-  }
-
-  private fun onInitFailed(t: Throwable) {
-    Timber.e(t, "Failed to launch app")
-    if (t is GooglePlayServicesNotAvailableException) {
-      popups.ErrorPopup().show(R.string.google_api_install_failed)
-    }
-    requireActivity().finish()
-  }
 }
