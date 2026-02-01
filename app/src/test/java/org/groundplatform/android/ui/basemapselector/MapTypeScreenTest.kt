893f6f597e3c899de1069b4488d74c51c86970cb
Shobhit Agarwal <ashobhit@google.com>
2026-02-01 13:40:55 +0530

    Add unit tests for MapTypeScreen

---
+++ app/src/test/java/org/groundplatform/android/ui/basemapselector/MapTypeScreenTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.groundplatform.android.ui.basemapselector
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.groundplatform.android.BaseHiltTest
+import org.groundplatform.android.model.map.MapType
+import org.groundplatform.android.repository.MapStateRepository
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(RobolectricTestRunner::class)
+class MapTypeScreenTest : BaseHiltTest() {
+
+  @Inject lateinit var mapStateRepository: MapStateRepository
+
+  private lateinit var viewModel: MapTypeViewModel
+  private var isDismissed = false
+
+  @Before
+  override fun setUp() {
+    super.setUp()
+    viewModel = MapTypeViewModel(mapStateRepository)
+    isDismissed = false
+    composeTestRule.setContent {
+      MapTypeScreen(
+        visible = true,
+        onDismissRequest = { isDismissed = true },
+        viewModel = viewModel,
+      )
+    }
+  }
+
+  @Test
+  fun `renders dialog correctly`() {
+    composeTestRule.onNodeWithText("Layers").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Base map").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Road map").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Terrain").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Satellite").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Offline map imagery").assertIsDisplayed()
+  }
+
+  @Test
+  fun `displays default map type correctly`() {
+    assertThat(mapStateRepository.mapType).isEqualTo(MapType.TERRAIN)
+  }
+
+  @Test
+  fun `changes map type when clicked`() {
+    composeTestRule.onNodeWithText("Road map").performClick()
+
+    assertThat(mapStateRepository.mapType).isEqualTo(MapType.ROAD)
+  }
+
+  @Test
+  fun `dismiss dialog after map type selection`() {
+    composeTestRule.onNodeWithText("Terrain").performClick()
+
+    assertThat(isDismissed).isTrue()
+  }
+}
