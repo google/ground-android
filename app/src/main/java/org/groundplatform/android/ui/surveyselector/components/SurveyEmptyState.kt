80345daaa830245c3f4b69185a215c272759ced1
Shobhit Agarwal <ashobhit@google.com>
2026-02-09 17:26:44 +0530

    Add preview for EmptyState compose

--- app/src/main/java/org/groundplatform/android/ui/surveyselector/components/SurveyEmptyState.kt
+++ app/src/main/java/org/groundplatform/android/ui/surveyselector/components/SurveyEmptyState.kt
@@ -26,8 +26,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.groundplatform.android.R
+import org.groundplatform.android.ui.theme.AppTheme
 
 @Composable
 fun SurveyEmptyState(onSignOut: () -> Unit) {
@@ -45,4 +47,10 @@ fun SurveyEmptyState(onSignOut: () -> Unit) {
       Text(stringResource(R.string.sign_out))
     }
   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SurveyEmptyStatePreview() {
+  AppTheme { SurveyEmptyState(onSignOut = {}) }
 }
