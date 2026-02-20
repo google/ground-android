908583827fb32b9cc795e654b2460637977f3e12
Shobhit Agarwal <ashobhit@google.com>
2026-02-20 17:18:08 +0530

    Migrate SettingsActivity to compose

--- app/src/main/java/org/groundplatform/android/ui/settings/SettingsActivity.kt
+++ app/src/main/java/org/groundplatform/android/ui/settings/SettingsActivity.kt
@@ -16,21 +16,49 @@
 package org.groundplatform.android.ui.settings
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.fragment.compose.AndroidFragment
 import dagger.hilt.android.AndroidEntryPoint
-import org.groundplatform.android.databinding.SettingsActivityBinding
+import org.groundplatform.android.R
 import org.groundplatform.android.ui.common.AbstractActivity
+import org.groundplatform.android.ui.theme.AppTheme
 
 @AndroidEntryPoint
 class SettingsActivity : AbstractActivity() {
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    val binding = SettingsActivityBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-
-    with(binding.settingsToolbar) {
-      setSupportActionBar(this)
-      setNavigationOnClickListener { finish() }
+    setContent {
+      AppTheme {
+        Scaffold(
+          topBar = {
+            TopAppBar(
+              title = { Text(text = stringResource(R.string.settings)) },
+              navigationIcon = {
+                IconButton(onClick = { finish() }) {
+                  Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                }
+              },
+            )
+          }
+        ) { innerPadding ->
+          AndroidFragment<SettingsFragment>(
+            modifier = Modifier.padding(innerPadding).fillMaxSize()
+          )
+        }
+      }
     }
   }
 }
