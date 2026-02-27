83c9db0f0adbbf7232cd8177f7b1a36b693b47e3
andreia <andreia.mp.ferreira@gmail.com>
2026-02-27 18:10:00 +0100

    create :share:ui and :share:domain kmp modules

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+  // We do not add an Android target here because this is a pure domain module.
+  // Adding an Android target would require the Android Gradle Plugin (AGP) and SDK configuration,
+  // which would couple the domain layer to platform-specific infrastructure.
+  // iOS targets are included because Kotlin can compile to iOS without any platform plugin.
+  jvm()
+  jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+
+  iosX64()
+  iosArm64()
+  iosSimulatorArm64()
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        implementation(libs.kotlin.stdlib)
+        implementation(libs.kotlinx.serialization.json)
+      }
+    }
+
+    commonTest { dependencies { implementation(libs.kotlin.test) } }
+  }
+}
