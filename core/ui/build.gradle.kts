aeb9e8453d96b38376dfb9ed10a12b69936c431a
andreia <andreia.mp.ferreira@gmail.com>
2026-03-05 12:42:11 +0100

    add ui kmp module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.android.kotlin.multiplatform.library)
+  alias(libs.plugins.android.lint)
+  alias(libs.plugins.compose.multiplatform)
+  alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+  jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+  androidLibrary {
+    namespace = "org.groundplatform.core.ui"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    minSdk = libs.versions.androidMinSdk.get().toInt()
+  }
+
+  val xcfName = "GroundUiKit"
+  val xcf = XCFramework(xcfName)
+
+  listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
+    it.binaries.framework {
+      baseName = xcfName
+      xcf.add(this)
+    }
+  }
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        implementation(libs.compose.runtime)
+        implementation(libs.compose.foundation)
+        implementation(libs.compose.material3)
+        implementation(libs.compose.ui)
+        implementation(libs.compose.ui.tooling.preview)
+        implementation(libs.compose.components.resources)
+        implementation(libs.androidx.lifecycle.runtime.compose)
+        implementation(libs.qrose)
+      }
+    }
+
+    commonTest { dependencies { implementation(libs.kotlin.test) } }
+
+    androidMain { dependencies {} }
+
+    iosMain { dependencies {} }
+  }
+}
+
+dependencies { androidRuntimeClasspath(libs.compose.ui.tooling) }
