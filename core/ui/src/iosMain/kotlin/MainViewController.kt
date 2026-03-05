1598006d91629cbe2f6264724fddea30d7ba4e76
Andreia Ferreira <andreia.mp.ferreira@gmail.com>
2026-03-05 17:00:56 +0100

    add iosApp to test running the current shareable kmp modules

---
+++ core/ui/src/iosMain/kotlin/MainViewController.kt
@@ -0,0 +1,21 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.ComposeUIViewController
+import org.groundplatform.ui.components.GroundQrCode
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController {
+  Box(modifier = Modifier.fillMaxSize()) {
+    GroundQrCode(
+      modifier = Modifier.align(Alignment.Center),
+      content = "https://www.google.com",
+      contentDescription = "Google",
+      centerLogoPainter = null,
+    )
+  }
+}
