a373cff39d2ba37ddd200fb987a7c5c910e4b8c6
andreia <andreia.mp.ferreira@gmail.com>
2026-03-10 16:09:50 +0100

    move LoiReport to sepparate package

--- core/domain/src/commonMain/kotlin/org/groundplatform/domain/model/locationofinterest/LoiReport.kt
+++ core/domain/src/commonMain/kotlin/org/groundplatform/domain/model/locationofinterest/LoiReport.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.groundplatform.domain.model
+package org.groundplatform.domain.model.locationofinterest
 
 import kotlinx.serialization.json.JsonObject
 
