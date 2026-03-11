d95a06b151b8651520590a9f86ea779929a9ba50
andreia <andreia.mp.ferreira@gmail.com>
2026-03-11 16:43:05 +0100

    update DataSubmissionConfirmationScreen with new design with QR code

--- core/domain/src/commonMain/kotlin/org/groundplatform/domain/usecases/GetLoiReportUseCase.kt
+++ core/domain/src/commonMain/kotlin/org/groundplatform/domain/usecases/GetLoiReportUseCase.kt
@@ -45,9 +45,9 @@ class GetLoiReportUseCase(
    * @param surveyId the identifier of the survey the LOI belongs to.
    * @throws IllegalStateException if the LOI geometry is a bare [LinearRing].
    */
-  suspend operator fun invoke(loiId: String, surveyId: String): LoiReport? {
+  suspend operator fun invoke(loiName: String, loiId: String, surveyId: String): LoiReport? {
     val loi = locationOfInterestRepository.getOfflineLoi(surveyId, loiId)
-    return loi?.let { LoiReport(it.geometry.toGeoJson(it.properties)) }
+    return loi?.let { LoiReport(loiName, it.geometry.toGeoJson(it.properties)) }
   }
 
   /**
