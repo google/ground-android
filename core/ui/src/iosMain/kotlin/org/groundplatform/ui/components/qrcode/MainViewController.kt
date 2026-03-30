d406c3780294e8f3d6a9fd49f8475d6560c9aa0d
Andreia Ferreira <andreia.mp.ferreira@gmail.com>
2026-03-30 16:42:03 +0200

    move files

--- core/ui/src/iosMain/kotlin/org/groundplatform/ui/components/qrcode/MainViewController.kt
+++ core/ui/src/iosMain/kotlin/org/groundplatform/ui/components/qrcode/MainViewController.kt
@@ -1,12 +1,10 @@
+package org.groundplatform.ui.components.qrcode
+
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import org.groundplatform.ui.components.GroundQrCode
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = ComposeUIViewController {
