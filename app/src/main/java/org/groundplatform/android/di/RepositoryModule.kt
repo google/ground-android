ff67a265842d97945d75b9fc7f71fcfa98611383
andreia <andreia.mp.ferreira@gmail.com>
2026-04-20 17:10:06 +0200

    add MapStateRepository bindings to RepositoryModule

--- app/src/main/java/org/groundplatform/android/di/RepositoryModule.kt
+++ app/src/main/java/org/groundplatform/android/di/RepositoryModule.kt
@@ -21,10 +21,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import jakarta.inject.Singleton
 import org.groundplatform.android.repository.LocationOfInterestRepository
+import org.groundplatform.android.repository.MapStateRepository
 import org.groundplatform.android.repository.SubmissionRepository
 import org.groundplatform.android.repository.SurveyRepository
 import org.groundplatform.android.repository.UserRepository
 import org.groundplatform.domain.repository.LocationOfInterestRepositoryInterface
+import org.groundplatform.domain.repository.MapStateRepositoryInterface
 import org.groundplatform.domain.repository.SubmissionRepositoryInterface
 import org.groundplatform.domain.repository.SurveyRepositoryInterface
 import org.groundplatform.domain.repository.UserRepositoryInterface
@@ -59,4 +61,12 @@ abstract class SubmissionRepositoryModule {
   @Binds
   @Singleton
   abstract fun bindSubmissionRepository(impl: SubmissionRepository): SubmissionRepositoryInterface
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class MapStateRepositoryModule {
+  @Binds
+  @Singleton
+  abstract fun bindMapStateRepository(impl: MapStateRepository): MapStateRepositoryInterface
 }
